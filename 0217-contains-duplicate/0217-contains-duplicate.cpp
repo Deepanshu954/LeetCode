2286dc9228355f99e60b186d698fc42b084d46c8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-04-14 12:12:31 +0530

    Accepted: Contains Duplicate

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for(int num : nums) {
-            if(seen.count(num)) return true;
-            seen.insert(num);
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_map<int,int> map;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            map[nums[i]]++;
+            if(map[nums[i]] > 1) return true;
         }
+
         return false;
     }
 };
