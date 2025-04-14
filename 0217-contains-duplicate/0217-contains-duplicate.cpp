95b3a7bb31651af02f28d336cb578232b713268c
Deepanshu954 <deepanshu95488@email.com>
2025-04-14 06:41:13 +0000

    ✅ 217. Contains Duplicate | Accepted | sub:1606308701

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -7,20 +7,8 @@ public:
         for(int i = 0; i < nums.size(); i++)
         {
             map[nums[i]]++;
+            if(map[nums[i]] > 1) return true;
         }
-
-        for(auto & i : map)
-        {
-            if(i.second > 1) return true;
-        }
-
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     for(int j = i+1; j < nums.size(); j++)
-        //     {
-        //         if(nums[i] == nums[j]) return true;
-        //     }
-        // }
 
         return false;
     }
