1ee27b55734abd4b7eae4a8760404ff07394f50d
Deepanshu954 <deepanshu95488@email.com>
2025-04-14 07:48:40 +0000

    ✅ 1. Two Sum | Accepted | sub:1606362373

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashMap;
-        for(int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if(hashMap.find(complement) != hashMap.end()) {
-                return {hashMap[complement], i};
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int,int> map;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int c = target - nums[i];
+            if(map.find(c) != map.end())
+            {
+                return {map[c], i};
             }
-            hashMap[nums[i]] = i;
+            map[nums[i]] = i;
         }
 
         return {};
