89dbb9a6d58b2dcb26dc2f41dad6fef20e1dce01
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-07-18 13:40:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (92.03%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,9 +8,6 @@ public:
         while(left < right)
         {
             mid = left + (right-left)/2;
-
-            
-
             if(nums[mid] < nums[mid+1])
             {
                 left = mid + 1;
