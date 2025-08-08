3db23c10f814bcaafc42bfba094117f2560b49d2
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-08-08 21:31:45 +0530

    Wrong Answer: Reverse Integer

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,22 +2,13 @@ class Solution {
     public int reverse(int x)
     {
         long num = 0,dig;
-        boolean togle = false;
 
-        if(x < 0)
-        {
-            x = x *(-1);
-            togle = true;
-        }
-
-        while(x > 0)
+        while(x != 0)
         {
             dig = x % 10;
             x /= 10;
             num = (num * 10) + dig;
         }
-
-        if(togle) num = num *(-1);
 
         int ans = (int) num;
         return ans;
