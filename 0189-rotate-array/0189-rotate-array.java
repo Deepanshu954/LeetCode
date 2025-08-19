65dd8de500844f233bf1eaa55d40a32577d2d4b8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-08-19 18:30:33 +0530

    Runtime Error: Rotate Array

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -13,8 +13,11 @@ class Solution {
 
     public void rotate(int[] nums, int k){
 
+        int n = nums.length;
+        if(n == 0) return;
+        if(k == 0) return;
         int i = 0;
-        int j = nums.length - 1;
+        int j = n - 1;
 
         reverseArr(nums,i, j);
         reverseArr(nums,i,k - 1);
