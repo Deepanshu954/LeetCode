7a0a9d66b6c0fe55748e60fced5fb037fd3406c6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-08-20 18:34:55 +0530

    Accepted: Single Number

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
-        for(int num : nums) {
-            ans ^= num;
-        }
-
+        for(int num : nums) ans ^= num;
         return ans;
     }
 }
