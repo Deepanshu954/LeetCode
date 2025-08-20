6350a876ab125396049be258fb7a8cdf72da55ce
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-08-20 07:01:51 +0530

    Accepted: Missing Number

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,15 +3,17 @@ class Solution {
     {
         int n = arr.length;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n - 1; j++) {
-                if(arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < n - 1; j++) {
+        //         if(arr[j] > arr[j+1]) {
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+
+        Arrays.sort(arr);
 
         for(int i = 0; i < n; i++) {
             if(arr[i] != i) return i;
