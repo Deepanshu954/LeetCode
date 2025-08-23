84642c8c0489fe38483a4bc1982bf6194a9ec419
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-08-23 09:22:44 +0530

    Accepted: Valid Anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,16 +3,15 @@ class Solution {
         if(s.length() != t.length()) return false;
         int n = s.length();
 
-
         int[] arr = new int[26];
 
         for(int i = 0; i < n; i++) {
-            arr[s.charAt(i) - 'a'] += 1;
-            arr[t.charAt(i) - 'a'] -= 1;
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        for(int i = 0; i < 26; i++) {
-            if(arr[i] != 0) return false;
+        for(int a : arr) {
+            if(a != 0) return false;
         }
 
         return true;
