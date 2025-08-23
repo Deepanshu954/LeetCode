d77300e3d8e8cc0587c1112e0853533648c1302e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-08-23 09:30:05 +0530

    Accepted: Two Sum

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,14 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target)
-    {
+    public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        Map<Integer,Integer> map = new HashMap<>();
 
-        for(int i = 0; i < n; i++)
-        {
-            int comp = target - nums[i];
-
-            if(map.containsKey(comp))
-            {
-                return new int[] {map.get(comp), i};
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i,j};
+                }
             }
-
-            map.put(nums[i], i);
         }
-
         return new int[] {0,0};
-
     }
 }
