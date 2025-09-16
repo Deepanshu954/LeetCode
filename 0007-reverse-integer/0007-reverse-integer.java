448da01bc33ffde5b5ff13ab3b10e033b20df590
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-09-16 22:36:23 +0530

    Wrong Answer: Reverse Integer

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -14,7 +14,7 @@ class Solution {
             num /= 10;
 
             if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && dig >  7)) return 0;
-            if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && dig < -8)) return 0;
+            //if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && dig < -8)) return 0;
 
             rev = rev*10 + dig;
         }
