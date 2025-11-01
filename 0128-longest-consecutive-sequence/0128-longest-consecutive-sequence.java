af36cd939038f614c46083423dc2a32813962235
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-01 10:46:10 +0530

    Wrong Answer: Longest Consecutive Sequence

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -9,6 +9,7 @@ class Solution {
         for(int num : nums) s.add(num);
 
         for(int it : s) {
+            if(it == Integer.MIN_VALUE )
             if(!s.contains(it - 1)) {
                 int cnt = 1;
                 int x = it;
