4eb5d29fda0b808b5e43f75f6100308b5266a5ce
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-01 11:47:14 +0530

    Wrong Answer: First Missing Positive

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -6,7 +6,7 @@ class Solution {
         
         for(int num : nums) set.add(num);
 
-        for(int i = 1; i < nums.length; i++)
+        for(int i = 1; i <= nums.length; i++)
         {
             if(!set.contains(i)) return i;
         }
