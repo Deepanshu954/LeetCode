537049b9dbf6f6932e1684040a446d6df3f764bb
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-01 10:18:27 +0530

    Accepted: Verifying an Alien Dictionary

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,9 +1,54 @@
+// class Solution {
+//     public boolean isAlienSorted(String[] words, String order) {
+//         Map<Character, Integer> map = new HashMap<>();
+
+//         for(int i = 0; i < 26; i++){
+//             map.put(order.charAt(i), i);
+//         }
+
+        
+
+//         for(int j = 0; j < words.length - 1; j++){
+//             String word1 = words[j];
+//             String word2 = words[j+1];
+
+//             //if(word1.equals("kuvp") && word2.equals("q") return true;
+
+//             if(word1.equals(word2)) continue;
+
+//             int len = Math.min(word1.length(), word2.length());
+//             boolean differenceFound = false;
+
+//             for(int i = 0; i < len; i++){
+//                 char ch1 = word1.charAt(i);
+//                 char ch2 = word2.charAt(i);
+                
+//                 if(ch1 != ch2){
+//                     if(map.get(ch1) > map.get(ch2)) return false;
+//                     differenceFound = true;
+//                     break;
+//                 }
+                
+//             }
+//             if(!differenceFound && word1.length() > word2.length()) return false;
+//         }
+
+//         return true;
+//     }
+// }
+
+
+
+
+
+
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        Map<Character, Integer> map = new HashMap<>();
+        int arr[] = new int[26];
 
         for(int i = 0; i < 26; i++){
-            map.put(order.charAt(i), i);
+            //map.put(order.charAt(i), i);
+            arr[order.charAt(i) - 'a'] = i;
         }
 
         
@@ -24,7 +69,7 @@ class Solution {
                 char ch2 = word2.charAt(i);
                 
                 if(ch1 != ch2){
-                    if(map.get(ch1) > map.get(ch2)) return false;
+                    if(arr[ch1 - 'a'] > arr[ch2 - 'a']) return false;
                     differenceFound = true;
                     break;
                 }
