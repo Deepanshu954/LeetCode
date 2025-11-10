18fea542ea7bd787c4b034037af4c64c0c975080
Deepanshu954 <deepanshu95488@email.com>
2025-11-10 22:03:00 +0000

    ✅ 125. Valid Palindrome | Accepted | sub:1826389476

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase();
+        int i = 0, j = str.length() - 1;
+
+        if (s.equals(" ") || s.length() == 0) return true;
+
+        while (i < j) {
+            char left = str.charAt(i);
+            char right = str.charAt(j);
+
+            if (!Character.isLetterOrDigit(left)) {
+                i++;
+            } else if (!Character.isLetterOrDigit(right)) {
+                j--;
+            } else {
+                if (left != right) return false;
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
