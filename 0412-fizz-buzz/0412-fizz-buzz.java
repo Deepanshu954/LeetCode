4cb50e33f44ec9537e89f40db809a3de40e01197
Deepanshu954 <deepanshu95488@email.com>
2025-11-10 22:23:50 +0000

    ✅ 412. Fizz Buzz | Accepted | sub:1826398505

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> arr = new ArrayList<>(n);
+        List<String> list = new ArrayList<>();
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 15 == 0) arr.add("FizzBuzz");
-            else if (i % 3 == 0) arr.add("Fizz");
-            else if (i % 5 == 0) arr.add("Buzz");
-            else arr.add(String.valueOf(i));
+        for(int i = 1; i <= n; i++){
+            //String ans = "" + 1;
+            if(i % 15 == 0) list.add("FizzBuzz");
+            else if(i % 3 == 0) list.add("Fizz");
+            else if(i % 5 == 0) list.add("Buzz");
+            else list.add(""+i);
         }
-
-        return arr;
+        return list;
     }
 }
