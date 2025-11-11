fa4220fa1d723543308b88c3ed58276a56e23efb
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-11 20:40:20 +0530

    Accepted: Minimum Window Substring

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,32 +1,39 @@
+import java.util.*;
+
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
+        if (t.length() > s.length()) return "";
 
-        int[] freq = new int[128]; // ASCII support
-        for (char c : t.toCharArray()) freq[c]++;
+        Map<Character, Integer> mapT = new HashMap<>();
+        for (char ch : t.toCharArray())
+            mapT.put(ch, mapT.getOrDefault(ch, 0) + 1);
 
-        int left = 0, right = 0, required = t.length();
-        int minLen = Integer.MAX_VALUE, start = 0;
+        Map<Character, Integer> mapS = new HashMap<>();
+        int have = 0, need = mapT.size();
+        int left = 0, minLen = Integer.MAX_VALUE;
+        int start = 0; // starting index of min window
 
-        while (right < s.length()) {
-            // Expand window
-            char c = s.charAt(right);
-            if (freq[c] > 0) required--;
-            freq[c]--;
-            right++;
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            mapS.put(ch, mapS.getOrDefault(ch, 0) + 1);
 
-            // When all required characters are covered
-            while (required == 0) {
-                // update result
-                if (right - left < minLen) {
-                    minLen = right - left;
+            if (mapT.containsKey(ch) && mapS.get(ch).intValue() == mapT.get(ch).intValue()) {
+                have++;
+            }
+
+            while (have == need) {
+                // update min window
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
                     start = left;
                 }
 
-                // Shrink window from left
-                char lc = s.charAt(left);
-                freq[lc]++;
-                if (freq[lc] > 0) required++;
+                // shrink from left
+                char leftChar = s.charAt(left);
+                mapS.put(leftChar, mapS.get(leftChar) - 1);
+                if (mapT.containsKey(leftChar) && mapS.get(leftChar) < mapT.get(leftChar)) {
+                    have--;
+                }
                 left++;
             }
         }
