711a9faf8c274421b567e0d608b5841334a194ff
Deepanshu954 <deepanshu95488@email.com>
2025-11-11 14:34:18 +0000

    ✅ 14. Longest Common Prefix | Accepted | sub:1826965235

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,3 +1,25 @@
+/*
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String first = strs[0];
+        int prefixLen = first.length();
+
+        for (int i = 1; i < strs.length; i++) {
+            String word = strs[i];
+            int j = 0;
+            int min = Math.min(prefixLen, word.length());
+
+            while (j < min && first.charAt(j) == word.charAt(j)) j++;
+
+            prefixLen = j;
+            if (prefixLen == 0) return "";
+        }
+
+        return first.substring(0, prefixLen);
+    }
+}
+*/
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length == 0) return "";
