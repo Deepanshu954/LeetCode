72b4fe954190b57a25808f1372deab00ad639f4a
Deepanshu954 <deepanshu95488@email.com>
2025-11-12 05:53:17 +0000

    ✅ 42. Trapping Rain Water | Accepted | sub:1827549520

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,6 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
+        int left = 0, right = height.length - 1;
         int leftMax = 0;
         int rightMax = 0;
         int ans = 0;
