291b3b1d92d74fd2276338c5c384a848a2f175d8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-12 18:41:52 +0530

    Wrong Answer: Maximum Product Subarray

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,14 +1,23 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int ans = Integer.MIN_VALUE;
+        int prod = 1;
         
+        // for(int i = 0; i < nums.length; i++){
+        //     prod = 1;
+        //     for(int j = i; j < nums.length; j++){
+        //         prod *= nums[j];
+        //         ans = Math.max(prod, ans);
+        //     }
+        // }
+
         for(int i = 0; i < nums.length; i++){
-            int prod = 1;
-            for(int j = i; j < nums.length; j++){
-                prod *= nums[j];
-                ans = Math.max(prod, ans);
-            }
+            prod *= nums[i];
+
+            ans = Math.max(prod, ans);
+            if(prod <= 0) prod = 1;
         }
+        
         return ans;
     }
 }
