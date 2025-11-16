95dcc508d313615341fb18c13869e2f1d3592567
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-16 22:16:17 +0530

    Accepted: Reverse Integer

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,28 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        int num = x;
-        boolean turn = false;
-        if(num < 0){
-            turn = true;
-            num = -1 * num;
+        int ans = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (ans > Integer.MAX_VALUE/10 || ans < Integer.MIN_VALUE/10) return 0;
+            ans = ans * 10 + digit;
         }
-
-        int rev = 0;
-
-        while(num != 0) {
-            int dig = num % 10;
-            num /= 10;
-
-            if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && dig >  7)) return 0;
-            if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && dig < -8)) return 0;
-
-            rev = rev*10 + dig;
-        }
-
-        if(turn){
-            return -1 * rev;
-        }else{
-            return rev;
-        }
+        return ans;
     }
 }
