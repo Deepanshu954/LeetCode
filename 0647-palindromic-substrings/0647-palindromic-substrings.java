32454551f0ec07ea41a6c27482d5253abd598813
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-11-23 13:42:11 +0530

    Accepted: Palindromic Substrings

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,51 +1,43 @@
+// class Solution {
+
+//     boolean isPalindrome(String str){
+//         int left = 0, right = str.length() - 1;
+
+//         while(left <= right){
+//             if(str.charAt(left) != str.charAt(right))return false;
+//             left++; right--;
+//         }
+//         return true;
+//     }
+
+//     public int countSubstrings(String s) {
+//         int n = s.length();
+//         int cnt = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i + 1; j <= n; j++) {
+//                 String sub = s.substring(i,j);
+//                 if(isPalindrome(sub)) cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
 class Solution {
     public int countSubstrings(String s) {
-        // Step 1: Transform: "abba" -> "^#a#b#b#a#$"
-        char[] t = preprocess(s);
-        int n = t.length;
+        int n = s.length();
+        int cnt = 0;
+
+        for(int center = 0;  center < 2*n - 1; center++){
+            int left = center/2;
+            int right = left + center % 2;
+
+            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                cnt++; left--; right++;
+            }
+        }
         
-        int[] p = new int[n];  // palindrome radius array
-        int center = 0, right = 0;
-        int count = 0;
-
-        // Step 2: Core Manacher loop
-        for (int i = 1; i < n - 1; i++) {
-            int mirror = 2 * center - i;      // mirror index wrt center
-
-            if (i < right)
-                p[i] = Math.min(right - i, p[mirror]);
-
-            // try expanding around i
-            while (t[i + 1 + p[i]] == t[i - 1 - p[i]]) {
-                p[i]++;
-            }
-
-            // update center & right boundary
-            if (i + p[i] > right) {
-                center = i;
-                right = i + p[i];
-            }
-
-            // Every increase in radius contributes (p[i] + 1) / 2 palindromes
-            count += (p[i] + 1) / 2;
-        }
-
-        return count;
-    }
-
-    // add #'s to convert even & odd palindromes into one form
-    private char[] preprocess(String s) {
-        char[] t = new char[s.length() * 2 + 3];
-        t[0] = '^';
-        t[t.length - 1] = '$';
-
-        int idx = 1;
-        for (char c : s.toCharArray()) {
-            t[idx++] = '#';
-            t[idx++] = c;
-        }
-        t[idx] = '#';
-
-        return t;
+        return cnt;
     }
 }
