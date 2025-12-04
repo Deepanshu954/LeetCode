d8a848e2c750045ad1b6a5e7ba815d0517702fdf
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-04 12:40:08 +0530

    Wrong Answer: Longest Palindromic Substring

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String ans = "";
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                String subString = s.substring(i,j);
+
+                if(isPalindrome(subString)){
+                    if(subString.length() > ans.length()){
+                        ans = subString;
+                    }
+                }
+
+            }
+        }
+        return ans;
+    }
+
+    public static boolean isPalindrome(String str){
+        if(str.length() == 0) return true;
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
