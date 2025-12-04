5761bcffdb6ba1062c9efcc6c48fcb8d627a1e87
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-04 20:10:34 +0530

    Wrong Answer: Repeated DNA Sequences

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,35 +1,18 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> res = new ArrayList<>();
-        if (s == null || s.length() < 10) return res;
+        List<String> list = new ArrayList<>();
+        if(s.length() < 10) return list;
 
-        int n = s.length();
-        int mask = (1 << 20) - 1; // keep 20 bits
-        int hash = 0;
+        Set<String> seen = new HashSet<>();
+        Set<String> repeat = new HashSet<>();
 
-        // map chars to 2-bit values
-        int[] map = new int[128];
-        map['A'] = 0;
-        map['C'] = 1;
-        map['G'] = 2;
-        map['T'] = 3;
+        for(int i = 0; i+10 < s.length(); i++){
+            String sub = s.substring(i, i+10);
 
-        Set<Integer> seen = new HashSet<>();
-        Set<Integer> added = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            hash = ((hash << 2) | map[s.charAt(i)]) & mask; // roll and keep 20 bits
-
-            if (i < 9) continue; // wait until first full window (i = 9)
-
-            if (!seen.add(hash)) {
-                // already seen: add to result set if not added before
-                if (added.add(hash)) {
-                    res.add(s.substring(i - 9, i + 1));
-                }
-            }
+            if(!seen.add(sub)) repeat.add(sub);
         }
 
-        return res;
+        list.addAll(repeat);
+        return list;
     }
 }
