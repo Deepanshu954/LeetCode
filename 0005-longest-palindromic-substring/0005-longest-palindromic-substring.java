159af905d186aaeb8d89659f3870cc7c5fd2187d
Deepanshu954 <deepanshu95488@email.com>
2025-12-04 07:10:53 +0000

    🛠️ 5. Longest Palindromic Substring | Compile Error | sub:1846561138

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(str.length() == 0 || str.length() == 1) return s;
+        int n = s.length();
+        String ans = "";
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                String subString = s.substring(i,j);
+
+                if(isPalindrome(subString)){
+                    if(subString.length() > ans.length()){
+                        ans = subString;
+                    }
+                }
+
+            }
+        }
+        return ans;
+    }
+
+    public static boolean isPalindrome(String str){
+        if(str.length() == 0) return true;
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
