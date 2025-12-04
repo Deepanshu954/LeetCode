980e82e7b3e03601eac1ce522424475b94ac6166
Deepanshu954 <deepanshu95488@email.com>
2025-12-04 13:23:34 +0000

    ✅ 966. Binary Subarrays With Sum | Accepted | sub:1846790388

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,54 +1,18 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length;
-        int count = 0;
-        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
 
-        // for(int i = 0; i < n; i++){
-        //     int sum = 0;
-        //     for(int j = i; j < n; j++){
-        //         sum += nums[j];
-        //         if(sum == goal) count++;
-        //     }
-        // }
+        int cnt = 0, prefixSum = 0;
 
+        for(int num : nums){
+            prefixSum += num;
 
+            if(map.containsKey(prefixSum - goal)) cnt += map.get(prefixSum - goal);
 
-
-
-        int left = 0, sum = 0;
-        int index = 0;
-        for(int right = 0; right < n; right++){
-            index++;
-            sum += nums[right];
-            if(sum == goal){
-                count++;
-                break;
-            }
-
-            if(right == n - 1) return 0;
-            
-            // if(count > 0 && nums[right] == 0) count++;
-
-            // while(sum > goal){
-            //     sum -= nums[left];
-            //     left++;
-            // }
-
-            // if(sum == goal) count++;
-
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
 
-        int ones = 0;
-        int zeros = 0;
-        for(int i = index; i < n; i++){
-            if(nums[i] == 0) zeros++;
-            else ones++;
-        }
-
-        int z = ((zeros+1)*(zeros+2))/2;
-        ans = ones + z;
-
-        return ans;
+        return cnt;
     }
 }
