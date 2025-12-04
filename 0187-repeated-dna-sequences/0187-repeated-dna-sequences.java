75873cbb8488a2e239299562268706a36c00d012
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-04 20:11:58 +0530

    Accepted: Repeated DNA Sequences

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> list = new ArrayList<>();
-        if(s.length() < 10) return list;
+        if(s == null || s.length() < 10) return list;
 
         Set<String> seen = new HashSet<>();
         Set<String> repeat = new HashSet<>();
 
-        for(int i = 0; i+10 < s.length(); i++){
+        for(int i = 0; i+10 <= s.length(); i++){
             String sub = s.substring(i, i+10);
 
             if(!seen.add(sub)) repeat.add(sub);
