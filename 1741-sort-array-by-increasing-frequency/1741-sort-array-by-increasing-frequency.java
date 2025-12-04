a44a5ea0f235d0f8976fe5b131e27cd4c3237a71
Deepanshu954 <deepanshu95488@email.com>
2025-12-04 12:24:21 +0000

    ✅ 1741. Sort Array by Increasing Frequency | Accepted | sub:1846756941

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -5,6 +5,39 @@ class Solution {
         HashMap<Integer, Integer> freq = new HashMap<>();
         for(int num : nums) freq.put(num, freq.getOrDefault(num,0)+1);
 
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<>(){
+            public int compare(Integer a, Integer b){
+                if(freq.get(a) == freq.get(b)) return b - a;
+                return freq.get(a) - freq.get(b);
+            }
+        });
+
+        for(Integer key : freq.keySet()) queue.offer(key);
+
+        int index = 0;
+        
+        while(!queue.isEmpty()){
+            int key = queue.poll();
+            int f = freq.get(key);
+
+            for(int i = 0; i < f; i++){
+                nums[index++] = key;
+            }
+        }
+
+        return nums;
+    }
+}
+
+
+/*
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        int n = nums.length;
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int num : nums) freq.put(num, freq.getOrDefault(num,0)+1);
+
         Integer[] temp = new Integer[n];
         for(int i = 0; i < n; i++) temp[i] = nums[i];
 
@@ -19,3 +52,4 @@ class Solution {
         return nums;
     }
 }
+*/
