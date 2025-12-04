9c35707982072404e1a87a5a028f4716988c2a25
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-04 09:16:06 +0530

    Accepted: Product of Array Except Self

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,6 +3,35 @@ class Solution {
         int n = nums.length;
 
         int[] suffix = new int[nums.length];int prod = 1;
+
+        for(int i = n - 1; i >= 0; i--){
+            prod *= nums[i];
+            suffix[i] = prod;
+        }
+
+        int[] prefix = new int[nums.length];
+        prod = 1;
+        for(int i = 0; i < n; i++){
+            prod *= nums[i];
+            prefix[i] = prod;
+        }
+
+        nums[0] = suffix[1];
+        nums[n-1] = prefix[n-2];
+
+        for(int i = 1; i < n - 1; i++){
+            nums[i] = prefix[i-1] * suffix[i+1]; 
+        }
+        return nums;
+    }
+}
+
+/*
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        int[] suffix = new int[nums.length];int prod = 1;
         
         for(int i = n - 1; i >= 0; i--){
             prod *= nums[i];
@@ -25,3 +54,4 @@ class Solution {
         return nums;
     }
 }
+*/
