08081a1b69791501c081690f786ec2dc4e7a671c
Deepanshu954 <deepanshu95488@email.com>
2025-12-24 13:23:28 +0000

    ✅ 217. Contains Duplicate | Accepted | sub:1864205915

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,21 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int n = nums.length;
-        Set<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        // for(int num : nums) {
-        //     if(set.contains(num)) return true;
-
-        //     set.add(num);
-        // }
-
-        for(int i = 0; i < 500; i++) {
-            if(set.contains(nums[i])) return true;
-            if(i == n- 1) return false;
-
-            set.add(nums[i]);
+        for(int num : nums){
+            if(set.contains(num)) return true;
+            set.add(num);
         }
-
         return false;
     }
 }
