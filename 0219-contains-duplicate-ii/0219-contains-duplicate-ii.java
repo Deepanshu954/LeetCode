608363ed8d9784ecfbd68619e66f7d5104bb3fdd
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-25 14:35:41 +0530

    Accepted: Contains Duplicate II

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,11 +1,24 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int i = 0; i < k; i++) set.add(nums[i]);
+//         for(int i = k; i < nums.length; i++){
+//             if(!set.add(nums[i])) return true;
+//             set.remove(nums[i-k]);
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < k; i++) set.add(nums[i]);
-        for(int i = k; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++){
             if(!set.add(nums[i])) return true;
-            set.remove(nums[i-k]);
+            if(i - k >= 0) set.remove(nums[i-k]);
         }
         return false;
     }
