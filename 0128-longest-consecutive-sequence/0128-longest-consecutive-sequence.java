9b1da6a5c3be2fe27c56b20be8eaafa7cdac1944
Deepanshu954 <deepanshu95488@email.com>
2025-12-26 07:41:49 +0000

    ✅ 128. Longest Consecutive Sequence | Accepted | sub:1865610547

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,40 +1,27 @@
-class Solution {
+public class Solution {
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        int longest = 1;
-        int curr = 1;
+int n=nums.length;
+int curr=1;
+int longest=1;
+Arrays.sort(nums);
 
-        Arrays.sort(nums);
+if(n==0)
+return 0;
 
-        if(n == 0) return 0;
+for(int i=1;i<nums.length;i++)
+{
+    if(nums[i]==nums[i-1]) continue;
+    if(nums[i]==nums[i-1]+1)
+    {
+        longest++;
+    }
+    else
+    {
+        longest=1;
+    }
+    curr=Math.max(curr,longest);
 
-        for(int i = 1; i < nums.length ; i++){
-            if(nums[i] == nums[i-1]) continue;
-            if(nums[i] == nums[i-1]+1) curr++;
-            else curr = 1;
-
-            longest = Math.max(curr, longest);
-        }
-        return longest;
+}
+return curr;
     }
 }
-
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-//         int maxCount = 0;
-
-//         HashSet<Integer> set = new HashSet<>();
-//         for(int num : nums) set.add(num);
-
-//         for(int s : set){
-//             if(set.contains(s - 1)) continue;
-
-//             int count = 0;
-//             while(set.contains(s++)) count++;
-
-//             maxCount = Math.max(count, maxCount);
-//         }
-//         return maxCount;
-        
-//     }
-// }
