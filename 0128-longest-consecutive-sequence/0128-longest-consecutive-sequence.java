ca4dd4bb9a9788e5fa01d9083a511067735b3e47
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-26 13:11:19 +0530

    Accepted: Longest Consecutive Sequence

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -2,16 +2,18 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         int n = nums.length;
         int longest = 1;
-        int current = 1;
+        int curr = 1;
 
         Arrays.sort(nums);
 
         if(n == 0) return 0;
 
-        for(int i = 0; i < nums.length ; i++){
-            //if(nums[i] == nums[i-1]);
-            if(nums[i] == nums[i-1]+1) longest++;
-            else longest = 1;
+        for(int i = 1; i < nums.length ; i++){
+            if(nums[i] == nums[i-1]) continue;
+            if(nums[i] == nums[i-1]+1) curr++;
+            else curr = 1;
+
+            longest = Math.max(curr, longest);
         }
         return longest;
     }
