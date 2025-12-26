f6f8d43f7d937992ddc13c78a87492ff3cde5262
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-26 13:09:41 +0530

    Runtime Error: Longest Consecutive Sequence

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,5 +1,38 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        
+        int n = nums.length;
+        int longest = 1;
+        int current = 1;
+
+        Arrays.sort(nums);
+
+        if(n == 0) return 0;
+
+        for(int i = 0; i < nums.length ; i++){
+            //if(nums[i] == nums[i-1]);
+            if(nums[i] == nums[i-1]+1) longest++;
+            else longest = 1;
+        }
+        return longest;
     }
 }
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         int maxCount = 0;
+
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int num : nums) set.add(num);
+
+//         for(int s : set){
+//             if(set.contains(s - 1)) continue;
+
+//             int count = 0;
+//             while(set.contains(s++)) count++;
+
+//             maxCount = Math.max(count, maxCount);
+//         }
+//         return maxCount;
+        
+//     }
+// }
