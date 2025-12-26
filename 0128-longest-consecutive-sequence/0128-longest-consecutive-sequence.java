e705a5017eceebafa9cf6ef0e155f0a82072680d
Deepanshu954 <deepanshu95488@email.com>
2025-12-26 07:29:51 +0000

    ⏱️ 128. Longest Consecutive Sequence | Time Limit Exceeded | sub:1865602750

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,19 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-        int longest = 0;
+        int maxCount = 0;
 
         HashSet<Integer> set = new HashSet<>();
         for(int num : nums) set.add(num);
-        
-        for(int s : set){
-            if(!set.contains(s - 1)){
-                int cnt = 1;
-                int x = s;
 
-                while(set.contains(x+1)){
-                    cnt++;
-                    x++;
-                }
-                longest = Math.max(longest, cnt);
-            }
-            
+        for(int num : nums){
+            if(set.contains(num - 1)) continue;
+
+            int count = 0;
+            while(set.contains(num++)) count++;
+
+            maxCount = Math.max(count, maxCount);
         }
-
-        return longest;
+        return maxCount;
+        
     }
 }
