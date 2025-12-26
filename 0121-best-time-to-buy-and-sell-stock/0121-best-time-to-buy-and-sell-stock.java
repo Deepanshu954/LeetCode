1c9b60a23b420bea6be5642a8ec255fd449cac66
Deepanshu954 <deepanshu95488@email.com>
2025-12-26 13:34:17 +0000

    ✅ 121. Best Time to Buy and Sell Stock | Accepted | sub:1865841526

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxProfit(int[] nums) {
+        int prof = 0, maxProf = 0, min = nums[0];
 
-        int prof = 0;
-        int maxProf = 0;
-        int min = nums[0];
-
-        for(int num : nums) {
-            if(num < min) {prof = 0; min = num;};
+        for(int num : nums){
+            if(min > num){
+                min = num;
+                prof = 0;
+            }
             prof = num - min;
             maxProf = Math.max(prof, maxProf);
         }
-
         return maxProf;
-        
     }
 }
