188167387e43c12cd9b9eb0acf1cbd10df31ff85
Deepanshu954 <deepanshu95488@email.com>
2025-12-26 07:34:52 +0000

    🛠️ 128. Longest Consecutive Sequence | Compile Error | sub:1865606011

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,19 +1,5 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int maxCount = 0;
-
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums) set.add(num);
-
-        for(int s : set){
-            if(set.contains(s - 1)) continue;
-
-            int count = 0;
-            while(set.contains(s++)) count++;
-
-            maxCount = Math.max(count, maxCount);
-        }
-        return maxCount;
         
     }
 }
