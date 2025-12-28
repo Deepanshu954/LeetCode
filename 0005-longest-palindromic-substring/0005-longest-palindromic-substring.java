2de79d58693e0a1cc053dc6ecc895c5844c50d12
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-28 14:56:17 +0530

    Runtime Error: Longest Palindromic Substring

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,49 +1,43 @@
 class Solution {
 
-    // Node represents one unique palindrome
     static class Node {
-        int len;            // length of palindrome
-        int link;           // suffix link
-        int[] next = new int[26]; // transitions
+        int len;                     // palindrome length
+        int link;                    // suffix link
+        java.util.HashMap<Character, Integer> next = new java.util.HashMap<>();
         Node(int l) { len = l; }
     }
 
     Node[] tree;
-    int size;              // number of nodes
-    int last;              // node of longest suffix palindrome
+    int size, last;
     String s;
-    int maxLen = 0;        // longest palindrome length
-    int endPos = 0;        // ending position of longest palindrome
+    int maxLen = 0, endPos = 0;
 
     public String longestPalindrome(String str) {
-        initTree();
+        init();
         s = "";
 
         for (int i = 0; i < str.length(); i++) {
             addChar(str.charAt(i), i);
         }
 
-        // extract answer
         return str.substring(endPos - maxLen + 1, endPos + 1);
     }
 
-    // Initialize tree with 2 roots
-    private void initTree() {
-        tree = new Node[1005];
-        tree[0] = new Node(-1); // imaginary root
-        tree[1] = new Node(0);  // empty string root
+    private void init() {
+        tree = new Node[1000];
+        tree[0] = new Node(-1);   // imaginary root
+        tree[1] = new Node(0);    // empty string root
         tree[0].link = 0;
         tree[1].link = 0;
         size = 2;
         last = 1;
     }
 
-    // Add one character to the tree
     private void addChar(char ch, int pos) {
         s += ch;
         int cur = last;
 
-        // Find longest suffix palindrome that can be extended
+        // Find extendable suffix palindrome
         while (true) {
             int curLen = tree[cur].len;
             if (pos - 1 - curLen >= 0 && s.charAt(pos - 1 - curLen) == ch)
@@ -52,25 +46,25 @@ class Solution {
         }
 
         // If palindrome already exists
-        if (tree[cur].next[ch - 'a'] != 0) {
-            last = tree[cur].next[ch - 'a'];
+        if (tree[cur].next.containsKey(ch)) {
+            last = tree[cur].next.get(ch);
             return;
         }
 
-        // Create new palindrome node
-        Node newNode = new Node(tree[cur].len + 2);
-        tree[size] = newNode;
-        tree[cur].next[ch - 'a'] = size;
+        // Create new node
+        Node nn = new Node(tree[cur].len + 2);
+        tree[size] = nn;
+        tree[cur].next.put(ch, size);
 
         // Set suffix link
-        if (newNode.len == 1) {
-            newNode.link = 1;
+        if (nn.len == 1) {
+            nn.link = 1;
         } else {
             while (true) {
                 cur = tree[cur].link;
                 int curLen = tree[cur].len;
                 if (pos - 1 - curLen >= 0 && s.charAt(pos - 1 - curLen) == ch) {
-                    newNode.link = tree[cur].next[ch - 'a'];
+                    nn.link = tree[cur].next.get(ch);
                     break;
                 }
             }
@@ -78,9 +72,9 @@ class Solution {
 
         last = size++;
 
-        // Update longest palindrome
-        if (newNode.len > maxLen) {
-            maxLen = newNode.len;
+        // Track longest palindrome
+        if (nn.len > maxLen) {
+            maxLen = nn.len;
             endPos = pos;
         }
     }
