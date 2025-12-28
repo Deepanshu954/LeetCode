178b7f036b40bf96b5fcfcc493b8791c9b431bff
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-28 14:32:36 +0530

    Wrong Answer: Longest Palindromic Substring

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,27 @@
 class Solution {
-    private int start = 0;
-    private int maxLength = 0;
-
     public String longestPalindrome(String s) {
-        int n = s.length();
-        if (n < 2) return s;
+        String ans = "";
 
-        for (int i = 0; i < n; i++) {
-            // Case 1: Odd length (Center is s[i])
-            expandAroundCenter(s, i, i);
-
-            // Case 2: Even length (Center is s[i] and s[i+1])
-            expandAroundCenter(s, i, i + 1);
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i+1; j < s.length(); j++){
+                String sub = s.substring(i,j);
+                if(isPalindrome(sub)){
+                    if(ans.length() < sub.length()) ans = sub;
+                }
+            }
         }
-
-        return s.substring(start, start + maxLength);
+        return ans;
     }
 
-    private void expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
         }
-
-        // The current palindrome found is s.substring(left + 1, right)
-        // Its length is (right - 1) - (left + 1) + 1 = right - left - 1
-
-        int currentLength = right - left - 1;
-
-        if (currentLength > maxLength) {
-            maxLength = currentLength;
-            start = left + 1; // start index of the new longest palindrome
-        }
+        return true;
     }
 }
