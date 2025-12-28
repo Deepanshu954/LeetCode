d5df100572ce21a66b2cf864fdd450c9e43301f2
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-28 14:37:36 +0530

    Accepted: Longest Palindromic Substring

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -5,24 +5,17 @@ class Solution {
 
         for(int i = 0; i < s.length(); i++){
             for(int j = i; j < s.length(); j++){
-                String sub = s.substring(i,j);
-                if(isPalindrome(sub)){
-                    if(ans.length() < sub.length()) ans = sub;
-                }
+                String sub = s.substring(i,j+1);
+                if(isPalindrome(sub) && ans.length() < sub.length()) ans = sub;
             }
         }
         return ans;
     }
 
     public static boolean isPalindrome(String s){
-        int left = 0;
-        int right = s.length() - 1;
+        int left = 0, right = s.length() - 1;
 
-        while(left < right){
-            if(s.charAt(left) != s.charAt(right)) return false;
-            left++;
-            right--;
-        }
+        while(left < right) if(s.charAt(left++) != s.charAt(right--)) return false;
         return true;
     }
 }
