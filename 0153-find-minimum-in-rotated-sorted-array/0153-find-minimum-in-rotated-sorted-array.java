f35ed73f756c9a7abe75c30f832109c2bb126813
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-29 18:34:38 +0530

    Compile Error: Find Minimum in Rotated Sorted Array

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int minVal = nums[minVal];
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+
+            // if(nums[left] < nums[mid] && nums[mid] > nums[right])
+            // {
+            //     if(nums[mid] < minVal) minVal = nums[mid];
+            //     left = mid + 1;
+            // }
+            // else
+            // {
+            //     if(nums[left] < nums[mid]){
+            //         if(nums[mid] < minVal) minVal = nums[mid];
+            //         left = mid + 1;
+            //     }
+            //     right = mid - 1;
+            // }
+
+            if(nums[left] < nums[mid]){
+                if(nums[mid] < minVal) minVal = nums[mid];
+                left = mid + 1;
+            }
+                right = mid - 1;
+
+
+        }
+
+        return midVal;
+    }
+}
