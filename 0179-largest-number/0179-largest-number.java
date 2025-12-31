3ce7961de4ced50c4ced85cebc41c2f4956831f3
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2025-12-31 18:27:41 +0530

    Accepted: Largest Number

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < nums.length; i++) arr[i] = String.valueOf(nums[i]);
 
         Arrays.sort(arr, (a,b) -> (b + a).compareTo(a + b));
-        if(arr[0] == "0") return "0";
+        if(arr[0].equals("0")) return "0";
 
         StringBuilder sb = new StringBuilder();
         for(String s : arr) sb.append(s);
