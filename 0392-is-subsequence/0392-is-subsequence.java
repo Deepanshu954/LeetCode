8ed4892826fd7a76221e231e776a35e2dbbd522c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-01 19:03:55 +0530

    Wrong Answer: Is Subsequence

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -2,12 +2,13 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         if(s.length() > t.length()) return false;
         if(s.equals(t)) return true;
+        if(s.equals("")) return true;
 
         int index = 0;
 
         for(int i = 0; i < t.length(); i++){
             if(index == s.length() - 1) return true;
-            
+
             if(s.charAt(index) == t.charAt(i)) index++;
             else continue;
         }
