df2e0336a235ea9e7fe8179a2c6a4867269ef24c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-01 22:05:52 +0530

    Accepted: Remove Duplicates from Sorted Array II

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,27 +1,18 @@
 class Solution {
-    public int removeDuplicates(int[] nums) 
-    {
+    public int removeDuplicates(int[] nums) {
         int index = 1;
         int enc = 1;
         int prev = nums[0];
 
-        for(int i = 1; i < nums.length; i++)
-        {
-            if(enc == 2 && nums[i] == prev) continue;
-            else 
-            {
-                if(nums[i] == prev)
-                {
-                    enc++;
-                    nums[index++] = nums[i];
-                }
-                else
-                {
-                    enc = 1;
-                    nums[index++] = nums[i];
-                    prev = nums[i];
-                }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == prev) {
+                if (enc == 2) continue; // FIX
+                enc++;
+            } else {
+                prev = nums[i];
+                enc = 1;
             }
+            nums[index++] = nums[i];
         }
         return index;
     }
