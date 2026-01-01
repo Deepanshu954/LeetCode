c6435a5c84039fa2df185ca1ea165b38b90d2485
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-01 21:32:08 +0530

    Accepted: Container With Most Water

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -8,7 +8,6 @@ class Solution {
             int hLeft = height[left];
             int hRight = height[right];
             
-            // Manual calculation of area to avoid extra method calls
             int h = (hLeft < hRight) ? hLeft : hRight;
             int currentArea = h * (right - left);
             
@@ -16,13 +15,25 @@ class Solution {
                 max = currentArea;
             }
 
-            // Move the pointer pointing to the shorter bar
-            if (hLeft < hRight) {
-                left++;
-            } else {
-                right--;
-            }
+            if(hLeft < hRight) left++;
+            else right--;
         }
         return max;
     }
 }
+
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int left = 0;
+//         int right = height.length - 1;
+//         int max = 0;
+
+//         while(left < right){
+//             max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+
+//             if(height[left] <= height[right]) left++;
+//             else right--;
+//         }
+//         return max;
+//     }
+// }
