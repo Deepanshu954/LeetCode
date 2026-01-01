4e1e0c77d521629e554552d5b1745d2add5889bd
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-01 19:03:04 +0530

    Runtime Error: Is Subsequence

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() > t.length()) return false;
+        if(s.equals(t)) return true;
+
+        int index = 0;
+
+        for(int i = 0; i < t.length(); i++){
+            if(index == s.length() - 1) return true;
+            
+            if(s.charAt(index) == t.charAt(i)) index++;
+            else continue;
+        }
+
+        if(index == s.length() - 1) return true;
+        
+        return false;
+    }
+}
