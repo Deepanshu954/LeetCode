ee3264db5558819ec5e41cca5f5ed49612b1ddca
Deepanshu954 <deepanshu95488@email.com>
2026-01-02 12:37:29 +0000

    ❌ 30. Substring with Concatenation of All Words | Wrong Answer | sub:1872020307

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -4,39 +4,24 @@ class Solution {
         if (words.length == 0) return res;
 
         int wordLen = words[0].length();
-        int totalLen = wordLen * words.length;
+        int totalWords = words.length;
+        int totalLen = wordLen * totalWords;
 
-        Map<String, Integer> freq = new HashMap<>();
-        for (String w : words)
-            freq.put(w, freq.getOrDefault(w, 0) + 1);
+        Set<String> dict = new HashSet<>(Arrays.asList(words));
 
-        for (int i = 0; i < wordLen; i++) {
-            Map<String, Integer> seen = new HashMap<>();
-            int left = i, count = 0;
+        for (int start = 0; start + totalLen <= s.length(); start++) {
+            Set<String> seen = new HashSet<>();
+            int i = start;
 
-            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
-                String word = s.substring(right, right + wordLen);
-
-                if (!freq.containsKey(word)) {
-                    seen.clear();
-                    count = 0;
-                    left = right + wordLen;
-                    continue;
-                }
-
-                seen.put(word, seen.getOrDefault(word, 0) + 1);
-                count++;
-
-                while (seen.get(word) > freq.get(word)) {
-                    String remove = s.substring(left, left + wordLen);
-                    seen.put(remove, seen.get(remove) - 1);
-                    count--;
-                    left += wordLen;
-                }
-
-                if (count == words.length)
-                    res.add(left);
+            while (i < start + totalLen) {
+                String w = s.substring(i, i + wordLen);
+                if (!dict.contains(w) || !seen.add(w))
+                    break;
+                i += wordLen;
             }
+
+            if (seen.size() == totalWords)
+                res.add(start);
         }
         return res;
     }
