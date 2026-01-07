c1c72ea22516ecedb823f7176f42cfa25fa2e341
Deepanshu954 <deepanshu95488@email.com>
2026-01-07 13:12:31 +0000

    ✅ 48. Rotate Image | Accepted | sub:1877602691

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,25 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+        int temp = 0;
 
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i; j < n; j++)
-            {
-                int temp = matrix[i][j];
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n/2; j++)
-            {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
             }
         }
+
+        return;
     }
 }
