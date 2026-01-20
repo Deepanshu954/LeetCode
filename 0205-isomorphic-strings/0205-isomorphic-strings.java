183d1745863ebe64ad1cc16b4f259afd1ee06c68
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-20 11:26:57 +0530

    Accepted: Isomorphic Strings

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] mapST = new int[26];
-        int[] mapTS = new int[26];
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
 
         for (int i = 0; i < s.length(); i++) {
-            int a = s.charAt(i) - 'a';
-            int b = t.charAt(i) - 'a';
+            char a = s.charAt(i);
+            char b = t.charAt(i);
 
             if (mapST[a] == 0 && mapTS[b] == 0) {
-                mapST[a] = b + 1;
-                mapTS[b] = a + 1;
-            } else if (mapST[a] != b + 1 || mapTS[b] != a + 1) {
+                mapST[a] = b;
+                mapTS[b] = a;
+            } else if (mapST[a] != b || mapTS[b] != a) {
                 return false;
             }
         }
