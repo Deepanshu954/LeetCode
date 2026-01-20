108fc8b95a4c478a194641731a6705ef9599d46e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-20 11:05:51 +0530

    Accepted: Remove Outermost Parentheses

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,25 +1,16 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-
         int count = 0;
-
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if(count == 0 && ch == '(') {
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (count > 0) sb.append(ch);
                 count++;
-                continue;
-            } else if(count == 1 && ch == ')'){
-                count--;
-                continue;
             } else {
-                if(ch == '(') count++;
-                else count--;
+                count--;
+                if (count > 0) sb.append(ch);
             }
-
-            sb.append(ch);
         }
         return sb.toString();
     }
