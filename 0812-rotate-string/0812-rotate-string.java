52f52bab4e00f7da4b74a825b7451ca9274a87b1
Deepanshu954 <deepanshu95488@email.com>
2026-01-20 14:11:55 +0000

    ✅ 812. Rotate String | Accepted | sub:1891077545

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++) {
+            String str = s.substring(i, s.length()) + s.substring(0,i);
+
+            if(str.equals(goal)) return true;
+        }
+        return false;
+    }
+}
