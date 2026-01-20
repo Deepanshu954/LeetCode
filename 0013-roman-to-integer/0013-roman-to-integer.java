c266942a334d86b027a9b5fda8803bf2216f0cd1
Deepanshu954 <deepanshu95488@email.com>
2026-01-20 14:36:58 +0000

    ✅ 13. Roman to Integer | Accepted | sub:1891104127

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,5 +1,35 @@
 class Solution {
     public int romanToInt(String s) {
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            int val = value(s.charAt(i));
+
+            if(i + 1 < s.length() && val < value(s.charAt(i+1))) total -= val;
+            else total += val;
+        }
+
+        return total;
+    }
+
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
+
+/*
+
+class Solution {
+    public int romanToInt(String s) {
         Map<Character, Integer> map = new HashMap<>();
 
         map.put('I', 1);
@@ -21,3 +51,5 @@ class Solution {
         return total;
     }
 }
+
+*/
