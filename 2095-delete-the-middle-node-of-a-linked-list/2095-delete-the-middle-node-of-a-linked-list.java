2394b8893efa86c2e6fc2b83b526f96f672b05b1
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-25 10:21:01 +0530

    Accepted: Delete the Middle Node of a Linked List

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         if(head == null || head.next == null) return null;
@@ -27,6 +29,32 @@ class Solution {
 
         // 2. Copy value of mid + 1 into mid
         slow.val = slow.next.val;
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
+*/
+
+
+ 
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        if(head.next.next == null) {
+            head.next = null;
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
+        // 1. Find Mid - 1
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
         slow.next = slow.next.next;
 
         return head;
