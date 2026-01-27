2b1584896a5462f529469ea9081e86d415780ef8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-27 19:09:13 +0530

    Wrong Answer: Find Peak Element

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        if(nums.length == 0) return 0;
 
         for(int i = 0; i < nums.length - 1; i++) {
             if(nums[i] > nums[i+1]) return i;
