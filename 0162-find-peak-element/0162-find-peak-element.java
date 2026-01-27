4ff292f46908825789e5e3189c213c46edff501b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-27 19:16:34 +0530

    Accepted: Find Peak Element

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,7 +6,7 @@ class Solution {
         while(left < right) {
             int mid = left + (right - left)/2;
 
-            if(nums[mid] > nums[mid + 1]) return mid;
+            if(nums[mid] > nums[mid + 1]) right = mid;
             else left = mid + 1;
         }
         return left;
