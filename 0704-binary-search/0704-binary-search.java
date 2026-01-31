5309d175c7d3218f9190784311377de6b09db4ec
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-01-31 15:06:30 +0530

    Accepted: Binary Search

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -4,13 +4,14 @@ class Solution {
     }
 
     public int binarySearch(int[] nums, int target, int left, int right) {
-        if(left > right) return -1;
+        if (left > right) return -1;
 
-        int mid = left + (right - left)/2;
+        int mid = left + (right - left) / 2;
 
-        if(nums[mid] == target) return mid;
-        else if(nums[mid] < target) return binarySearch(nums, target, mid + 1, nums.length);
-        else return binarySearch(nums, target, left, mid - 1);
-
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target)
+            return binarySearch(nums, target, mid + 1, right);
+        else
+            return binarySearch(nums, target, left, mid - 1);
     }
 }
