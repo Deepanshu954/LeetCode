088f1f6a9dd7dd1e271d47d7bc3b5447d4ff7bef
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-03 20:03:10 +0530

    Accepted: Sort List

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,57 +1,44 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) return head;
 
-        // 1. Split the list
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        // 1. split list
         ListNode mid = getMid(head);
         ListNode right = mid.next;
         mid.next = null;
 
-        // 2. Sort Halves
-        ListNode leftSort = sortList(head);
-        ListNode rightSort = sortList(right);
+        // 2. sort halves
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
 
-        // 3. Merge
-        return merge(leftSort, rightSort);
+        // 3. merge
+        return merge(leftSorted, rightSorted);
     }
 
     private ListNode getMid(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while(fast != null && fast.next != null) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow;
+        return slow; // mid-left
     }
 
     private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode();
         ListNode tail = dummy;
 
-        while(l1 != null && l2 != null) {
-            if(l1.val <= l2.val) {
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
                 tail.next = l1;
                 l1 = l1.next;
             } else {
                 tail.next = l2;
                 l2 = l2.next;
             }
-
             tail = tail.next;
         }
-
         tail.next = (l1 != null) ? l1 : l2;
         return dummy.next;
     }
