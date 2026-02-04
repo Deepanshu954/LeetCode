8ee03d9b2cc8ac98a7a5bbf1b3bb7885fe7f8a55
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-04 13:57:43 +0530

    Accepted: Reverse Linked List

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,17 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
 
-        // base case
-        if (head == null || head.next == null)
-            return head;
-
-        // reverse rest
         ListNode newHead = reverseList(head.next);
 
-        // fix current node
         head.next.next = head;
         head.next = null;
-
         return newHead;
+        
     }
 }
