00da519e8c2709b8888b1d20699a28c8a21162be
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-05 10:13:05 +0530

    Time Limit Exceeded: Container With Most Water

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,39 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int max = 0;
+        
+        int max = -1;
+        int min = 0;
 
-        while (left < right) {
-            int hLeft = height[left];
-            int hRight = height[right];
-            
-            int h = (hLeft < hRight) ? hLeft : hRight;
-            int currentArea = h * (right - left);
-            
-            if (currentArea > max) {
-                max = currentArea;
+        for(int i = 0; i < height.length; i++) {
+
+            int localMax = 0;
+
+            for(int j = i + 1; j < height.length; j++) {
+
+                if(height[i] < height[j]) min = height[i];
+                else min = height[j];
+
+                localMax = min * (j - i);
+
+                if(localMax > max) max = localMax;
             }
-
-            if(hLeft < hRight) left++;
-            else right--;
         }
+
         return max;
     }
 }
-
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int left = 0;
-//         int right = height.length - 1;
-//         int max = 0;
-
-//         while(left < right){
-//             max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
-
-//             if(height[left] <= height[right]) left++;
-//             else right--;
-//         }
-//         return max;
-//     }
-// }
