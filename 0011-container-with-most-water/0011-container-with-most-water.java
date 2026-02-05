b825cd105fd5c28cc347c8e262b2a19d0683a736
Deepanshu954 <deepanshu95488@email.com>
2026-02-05 04:43:05 +0000

    ⏱️ 11. Container With Most Water | Time Limit Exceeded | sub:1908674032

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left=0;
-        int right = height.length -1;
-        int max = 0;
+        
+        int max = -1;
+        int min = 0;
 
-        while(left<right){
-            int min = Math.min(height[left],height[right]) ;
-            max = Math.max(min * (right - left),max);
+        for(int i = 0; i < height.length; i++) {
 
-            while(left<right && height[left]<=min)
-                left++;
-            while(left<right && height[right]<=min)
-                right--;
+            int localMax = 0;
+
+            for(int j = i + 1; j < height.length; j++) {
+
+                if(height[i] < height[j]) min = height[i];
+                else min = height[j];
+
+                localMax = min * (j - i);
+
+                if(localMax > max) max = localMax;
+            }
         }
 
         return max;
