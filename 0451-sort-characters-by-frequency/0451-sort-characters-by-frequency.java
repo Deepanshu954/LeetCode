8a66512e7da94fd336dc13f7e64d2ed4b0a66fec
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-07 08:41:34 +0530

    Accepted: Sort Characters By Frequency

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,1 +1,31 @@
-                buckets[freq[c]].add((char) c);
+class Solution {
+    public String frequencySort(String s) {
+        int[] freq = new int[128];
+
+        // 1. Count frequency
+        for (char c : s.toCharArray()) {
+            freq[c]++;
+        }
+
+        // 2. Store characters that appear
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < 128; i++) {
+            if (freq[i] > 0) {
+                list.add((char) i);
+            }
+        }
+
+        // 3. Sort by frequency (desc)
+        list.sort((a, b) -> freq[b] - freq[a]);
+
+        // 4. Build result
+        StringBuilder sb = new StringBuilder();
+        for (char c : list) {
+            for (int i = 0; i < freq[c]; i++) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
