b61e709cee175a64accb75dc655bc25a3ba03ab3
Deepanshu954 <deepanshu95488@email.com>
2026-02-07 09:31:09 +0000

    ✅ 152. Maximum Product Subarray | Accepted | sub:1911119443

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,26 +1,40 @@
 class Solution {
-  
     public int maxProduct(int[] nums) {
-     int maxi=Integer.MIN_VALUE;
-     int prod=1;
-     for(int i=0;i<nums.length;i++){
-        prod*=nums[i];
-        maxi=Math.max(prod,maxi);
-        if(prod==0){
-            prod=1;
-        }
-      
-     }
-    prod=1;
-     for(int i=nums.length-1;i>=0;i--){
-        prod*=nums[i];
-        maxi=Math.max(prod,maxi);
-        if(prod==0){
-            prod=1;
-        }
+        int minProd = nums[0];
+        int maxProd = nums[0];
+        int ans = nums[0];
 
-       
-     }
-        return maxi; 
+
+        // int prod = 1;
+        // for(int i = 0; i < nums.length; i++){
+        //     prod = 1;
+        //     for(int j = i; j < nums.length; j++){
+        //         prod *= nums[j];
+        //         ans = Math.max(prod, ans);
+        //     }
+        // }
+
+        for(int i = 1; i < nums.length; i++){
+            int curr = nums[i];
+
+            if(curr < 0){
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+
+            int temp = curr;
+
+            maxProd = maxProd * curr;
+            if(maxProd < curr) maxProd = curr;
+
+            minProd = minProd * temp;
+            if(minProd > temp) minProd = temp;
+
+
+            if(ans < maxProd) ans = maxProd;
+        }
+        
+        return ans;
     }
 }
