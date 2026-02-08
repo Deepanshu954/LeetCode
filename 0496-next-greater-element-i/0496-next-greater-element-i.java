8430fc3cd62778aa3743d9a01b8d88d2e9528bbf
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-09 01:35:42 +0530

    Accepted: Next Greater Element I

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
+
         int n2 = nums2.length;
         Deque<Integer> dq = new ArrayDeque<>();
-        int[] res = new int[n2];
-        
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = n2 - 1; i >= 0; i--) {
-            while(!dq.isEmpty() && dq.peekFirst() <= nums2[i]) dq.removeFirst();
+        // Step 1: Next Greater for nums2
+        for (int i = n2 - 1; i >= 0; i--) {
 
-            res[i] = dq.isEmpty() ? -1 : dq.peekFirst();
+            while (!dq.isEmpty() && dq.peekFirst() <= nums2[i]) {
+                dq.removeFirst();
+            }
 
+            map.put(nums2[i], dq.isEmpty() ? -1 : dq.peekFirst());
             dq.addFirst(nums2[i]);
         }
 
-        int[] ans = new int[n1];
-
-        for(int i = 0; i < n1; i++) {
-            for(int j = 0; j < n2; j++) {
-                if(nums1[i] == nums2[j]) ans[i] = res[j];
-            }
+        // Step 2: Build answer for nums1
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
         }
-        
+
         return ans;
     }
 }
