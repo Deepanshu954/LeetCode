2eec5e0ed475a76b3e82162ba498bcc15c3ecb53
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-08 13:26:30 +0530

    Accepted: Single Number III

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,18 +1,28 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        if(nums.length == 2) return nums;
         Arrays.sort(nums);
 
         int[] arr = new int[2];
         int index = 0;
+        int n = nums.length;
 
-        for(int i = 1; i < nums.length - 1; i++){
-            if( (nums[i - 1] == nums[i]) || (nums[i] == nums[i+1] ) ) continue;
-
-            arr[index++] = nums[i];
+        // Check first element
+        if (nums[0] != nums[1]) {
+            arr[index++] = nums[0];
         }
 
-        if(nums[nums.length - 1] != nums[nums.length - 2]) arr[1] = nums[nums.length - 1];
+        // Check middle elements
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                arr[index++] = nums[i];
+            }
+        }
+
+        // Check last element
+        if (nums[n - 1] != nums[n - 2]) {
+            arr[index++] = nums[n - 1];
+        }
+
         return arr;
     }
 }
