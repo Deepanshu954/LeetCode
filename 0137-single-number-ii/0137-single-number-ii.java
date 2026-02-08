8e10fca14c166db966d2aeee651b414ba0f17641
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-08 12:32:10 +0530

    Accepted: Single Number II

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,6 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0, twos = 0;
+        int ones = 0;
+        int twos = 0;
+        
         for (int n : nums) {
             ones = (ones ^ n) & ~twos;
             twos = (twos ^ n) & ~ones;
