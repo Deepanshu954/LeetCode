352bac853eb7c8eb549aef8f73a49e2983cc97ca
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-08 16:10:04 +0530

    Compile Error: Minimum Bit Flips to Convert Number

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,22 +1,17 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        // return Integer.bitCount((start ^ goal));
+        /return Integer.bitCount((start ^ goal));
 
-        int num = start ^ goal;
+        // int num = start ^ goal;
 
-        int count = 0;
+        // int count = 0;
 
-        while(num > 0) {
-            // num = num & (num - 1);
-            // count++;
+        // while(num > 0) {
+        //     num = num & (num - 1);
+        //     count++;
+        // }
 
-            int bit = num & 1;
-            if(bit == 1) count++;
-
-            num >>= 1;
-        }
-
-        return count;
+        // return count;
         
     }
 }
