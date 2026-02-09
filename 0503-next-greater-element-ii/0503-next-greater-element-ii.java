58efeb3a64d3118f7ddba453e15041edb54b06ff
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-09 08:50:42 +0530

    Accepted: Next Greater Element II

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         Arrays.fill(res, -1);
 
-        Deque<Integer> dq = new ArrayDeque<>(); // store indices
+        Deque<Integer> dq = new ArrayDeque<>();
 
-        for (int i = 0; i < 2 * n; i++) {
-            int idx = i % n;
+        for(int i = 0; i < 2 * n; i++) {
+            int index = i % n;
 
-            // resolve next greater
-            while (!dq.isEmpty() && nums[dq.peekFirst()] < nums[idx]) {
-                res[dq.removeFirst()] = nums[idx];
+            while(!dq.isEmpty() && nums[dq.peekFirst()] < nums[index]) {
+                res[dq.removeFirst()] = nums[index];
             }
 
-            // push index only in first round
-            if (i < n) {
-                dq.addFirst(idx);
-            }
+            if(i < n) dq.addFirst(index);
         }
-
         return res;
     }
 }
