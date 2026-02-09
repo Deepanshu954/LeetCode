94cddf04a7a491ea73ca52943053088133c09b38
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-09 07:20:04 +0530

    Accepted: Next Greater Element I

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,29 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
+        int n1 = nums1.length;
         int n2 = nums2.length;
+
         Deque<Integer> dq = new ArrayDeque<>();
         Map<Integer, Integer> map = new HashMap<>();
 
-        // Step 1: Next Greater for nums2
-        for (int i = n2 - 1; i >= 0; i--) {
-
-            while (!dq.isEmpty() && dq.peekFirst() <= nums2[i]) {
-                dq.removeFirst();
-            }
+        for(int i = n2 - 1; i >= 0; i--) {
+            while(!dq.isEmpty() && dq.peekFirst() <= nums2[i]) dq.removeFirst();
 
             map.put(nums2[i], dq.isEmpty() ? -1 : dq.peekFirst());
+
             dq.addFirst(nums2[i]);
         }
 
-        // Step 2: Build answer for nums1
-        int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
+        int[] ans = new int[n1];
+
+        for(int i = 0; i < n1; i++) {
             ans[i] = map.get(nums1[i]);
         }
-
         return ans;
     }
 }
