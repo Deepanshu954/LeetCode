76a6d65d9c99fa52fa0f17d5636237d1b88cb4ed
Deepanshu954 <deepanshu95488@email.com>
2026-02-09 05:34:10 +0000

    ✅ 20. Valid Parentheses | Accepted | sub:1913109753

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,35 +1,14 @@
 class Solution {
     public boolean isValid(String s) {
-
-        int n = s.length();
-        if (n % 2 == 1) return false;
-
-        // manual stack
-        char[] stack = new char[n];
-        int top = -1;
-
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-
-            // push opening brackets
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack[++top] = ch;
-            }
-            // closing brackets
-            else {
-                if (top == -1) return false; // underflow
-
-                char open = stack[top--]; // pop
-
-                if ((ch == ')' && open != '(') ||
-                    (ch == '}' && open != '{') ||
-                    (ch == ']' && open != '[')) {
-                    return false;
-                }
+        char[] arr = s.toCharArray();
+        int i=0;
+        for(char a: arr){
+            if((a&3)==1){
+                if(i==0 || (char)(a-arr[--i])>2) return false;
+            }else{
+                arr[i++]=a;
             }
         }
-
-        // stack must be empty
-        return top == -1;
+        return i==0;
     }
 }
