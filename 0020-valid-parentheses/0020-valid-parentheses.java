9e24158ae68754868e294133a40254b41bb98ffb
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-09 11:00:52 +0530

    Accepted: Valid Parentheses

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() % 2 == 1) return false;
+        if (s.length() % 2 == 1) return false;
+
         Deque<Character> dq = new ArrayDeque<>();
 
-        for(char ch : s.toCharArray()) {
-            if((ch == '(') || (ch == '{') || (ch == '[')) dq.addFirst(ch);
-            else if((ch == ')') && (dq.peekFirst() == '(')) dq.removeFirst();
-            else if((ch == '}') && (dq.peekFirst() == '{')) dq.removeFirst();
-            else if((ch == ']') && (dq.peekFirst() == '[')) dq.removeFirst();
-            else return false;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                dq.addFirst(ch);
+            } else {
+                if (dq.isEmpty()) return false;
+
+                if (ch == ')' && dq.peekFirst() == '(') dq.removeFirst();
+                else if (ch == '}' && dq.peekFirst() == '{') dq.removeFirst();
+                else if (ch == ']' && dq.peekFirst() == '[') dq.removeFirst();
+                else return false;
+            }
         }
-        return dq.isEmpty() ? true : false;
+        return dq.isEmpty();
     }
 }
