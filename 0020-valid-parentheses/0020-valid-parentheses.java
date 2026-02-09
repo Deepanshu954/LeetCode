35cb7f819a65a461d3a0c156e0c8fd70386c5af2
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-09 10:56:40 +0530

    Wrong Answer: Valid Parentheses

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> dq = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray()) {
+            if((ch == '(') || (ch == '{') || (ch == '[')) {
+                dq.addFirst(ch);
+                continue;
+            }
+
+            if((ch == ')') && (dq.peekFirst() == '(')) dq.removeFirst();
+            else if((ch == '}') && (dq.peekFirst() == '{')) dq.removeFirst();
+            else if((ch == ']') && (dq.peekFirst() == '[')) dq.removeFirst();
+            else return false;
+        }
+        return true;
+    }
+}
