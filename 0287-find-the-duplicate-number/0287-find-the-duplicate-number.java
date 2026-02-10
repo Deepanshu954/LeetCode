41ef0345357f760972d577de3b1718844dd765c5
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-10 08:07:12 +0530

    Accepted: Find the Duplicate Number

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,22 +1,35 @@
 class Solution {
     public int findDuplicate(int[] nums) {
 
-        int slow = nums[0];
-        int fast = nums[0];
+        int n = nums.length - 1;
+        int ans = 0;
 
-        // Phase 1: detect cycle
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
+        // Check each bit position (0 to 31)
+        for (int bit = 0; bit < 32; bit++) {
 
-        // Phase 2: find entry point
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+            int countNums = 0;
+            int countRange = 0;
+
+            // Count bit in nums[]
+            for (int num : nums) {
+                if ((num & (1 << bit)) != 0) {
+                    countNums++;
+                }
+            }
+
+            // Count bit in range [1..n]
+            for (int i = 1; i <= n; i++) {
+                if ((i & (1 << bit)) != 0) {
+                    countRange++;
+                }
+            }
+
+            // Extra count means duplicate has this bit
+            if (countNums > countRange) {
+                ans |= (1 << bit);
+            }
         }
 
-        return slow;
+        return ans;
     }
 }
