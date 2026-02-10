e31f8eac59831044045f56ed08bc69ff7b641e79
Deepanshu954 <deepanshu95488@email.com>
2026-02-10 02:33:50 +0000

    🛠️ 287. Find the Duplicate Number | Compile Error | sub:1914108658

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,16 +1,20 @@
+i did't understand it
+
 class Solution {
     public int findDuplicate(int[] nums) {
+
         int slow = nums[0];
         int fast = nums[0];
 
+        // Phase 1: detect cycle
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }
-        while(slow != fast);
+        } while (slow != fast);
 
+        // Phase 2: find entry point
         slow = nums[0];
-        while(slow != fast) {
+        while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
