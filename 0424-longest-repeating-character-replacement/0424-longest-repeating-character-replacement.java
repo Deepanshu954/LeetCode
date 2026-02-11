62af5cb9433b5af64cc2c7029229390c0325dee1
Deepanshu954 <deepanshu95488@email.com>
2026-02-11 14:10:39 +0000

    🛠️ 424. Longest Repeating Character Replacement | Compile Error | sub:1915872939

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -7,15 +7,18 @@ class Solution {
 
         for (int right = 0; right < s.length(); right++) {
 
-            freq[s.charAt(right) - 'A']++;
+            int freq[s.charAt(right) - 'A']++;
             maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+
+
 
             while ((right - left + 1) - maxFreq > k) {
                 freq[s.charAt(left) - 'A']--;
                 left++;
             }
 
-            maxLen = Math.max(maxLen, right - left + 1);
+            int len = right - left + 1;
+            if(maxLen < len) maxLen = len;
         }
 
         return maxLen;
