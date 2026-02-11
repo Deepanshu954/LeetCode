b9fc4a4493d00de74fc58dcac11702e900a45507
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-11 19:36:56 +0530

    Accepted: Longest Repeating Character Replacement

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,41 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int freq[] = new int[26];
+        if (s.length() == 0 || s.length() <= k) return s.length();
+
+        int[] freq = new int[26];
+
+        // initialize first k window
+        for (int i = 0; i < k && i < s.length(); i++) {
+            freq[s.charAt(i) - 'A']++;
+        }
+
         int left = 0;
-        int maxLen = 0;
-        int maxFreq = 0;
+        int max = k;   // at least k length possible
 
-        for(int right = 0; right < s.length(); right++){
+        for (int right = k; right < s.length(); right++) {
+
             freq[s.charAt(right) - 'A']++;
-            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
 
-            while((right - left + 1) - maxFreq > k){
+            while (!check(freq, right - left + 1, k)) {
                 freq[s.charAt(left) - 'A']--;
                 left++;
             }
 
-
-            maxLen = Math.max(maxLen, right - left + 1);
-
+            int localMax = right - left + 1;
+            if (localMax > max) max = localMax;
         }
 
-        return maxLen;
-        
+        return max;
+    }
+
+    private boolean check(int[] nums, int windowSize, int k) {
+
+        int maxFreq = 0;
+
+        for (int num : nums) {
+            maxFreq = Math.max(maxFreq, num);
+        }
+
+        return (windowSize - maxFreq) <= k;
     }
 }
