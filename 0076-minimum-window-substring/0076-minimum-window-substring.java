4f2e313f7ddd8e2137fc5ca53da312df9dec8303
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 10:04:52 +0530

    Accepted: Minimum Window Substring

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,44 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
 
-        HashMap<Character, Integer> mapt = new HashMap<>();
-        for(int i = 0; i < t.length(); i++){
-            char ch = t.charAt(i);
-            mapt.put(ch, mapt.getOrDefault(ch, 0)+1);
-        }
+        String ans = "";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : t.toCharArray()) map.put(ch, map.getOrDefault(ch, 0) + 1);
 
-        int have = 0;
+        int[] freq = new int[128];
         int left = 0;
-        int start = 0;
-        int need = mapt.size();
-        int minLen = Integer.MIN_VALUE;
+        int minLen = Integer.MAX_VALUE;
 
-        HashMap<Character, Integer> maps = new HashMap<>();
+        for(int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right)]++;
 
-        for(int right = 0; right < s.length(); right++){
-            char ch = s.charAt(right);
-            maps.put(ch, maps.getOrDefault(ch, 0)+1);
-
-            if(mapt.containsKey(ch) && mapt.get(ch).intValue() == maps.get(ch).intValue()) have++;
-
-            
-            while(have == need){
-                if(right - left + 1 < minLen){
-                    minLen = right - left + 1;
-                    start = left;
+            // Valid Window -> Try to Shrink it
+            while(check(freq, map)) {
+                int len = right - left + 1;
+                
+                if(len < minLen) {
+                    minLen = len;
+                    ans = s.substring(left, right + 1);
                 }
 
-                char leftChar = s.charAt(left);
-                maps.put(leftChar, maps.get(leftChar));
-
-                if(mapt.containsKey(leftChar) && maps.get(leftChar) < mapt.get(leftChar) ) have--;
+                freq[s.charAt(left)]--;
+                left++;
             }
-            left++;
-
         }
+        return ans;
+    }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start + start + minLen);
-
-        
+    private boolean check(int[] nums, HashMap<Character, Integer> map) {
+        for(Character key : map.keySet()) {
+            if(map.get(key) > nums[key]) return false;
+        }
+        return true;
     }
 }
