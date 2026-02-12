d253ad741ff7a90ae823d3de70dfa335e4a6c03b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 11:17:45 +0530

    Accepted: Shortest Subarray with Sum at Least K

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
@@ -1,37 +1,59 @@
-use std::collections::VecDeque;
-
 impl Solution {
     pub fn shortest_subarray(nums: Vec<i32>, k: i32) -> i32 {
         let n = nums.len();
         let k = k as i64;
-        let mut sum = 0i64;
-        let mut ans = n + 1;
-        let mut deque = VecDeque::new();
-        deque.push_back((0i64, 0));
+        let mut min_len = (n + 1) as i32;
 
-        for i in 0..n {
-            sum += nums[i] as i64;
-            let cur_idx = i + 1;
+        let mut q_vals = Vec::<i64>::with_capacity(n + 1);
+        let mut q_idxs = Vec::<i32>::with_capacity(n + 1);
 
-            // Unified Front Pruning
-            while let Some(&(s, idx)) = deque.front() {
-                if sum - s >= k {
-                    ans = ans.min(cur_idx - idx);
-                    deque.pop_front();
-                } else if cur_idx - idx >= ans {
-                    deque.pop_front();
-                } else {
-                    break;
+        unsafe {
+            let q_vals_ptr = q_vals.as_mut_ptr();
+            let q_idxs_ptr = q_idxs.as_mut_ptr();
+            
+            let mut head = 0;
+            let mut tail = 0;
+
+            *q_vals_ptr = 0;
+            *q_idxs_ptr = 0; 
+            tail += 1;
+
+            let mut curr_sum: i64 = 0;
+
+            for i in 0..n {
+
+                curr_sum += *nums.get_unchecked(i) as i64;
+                let curr_idx = (i + 1) as i32;
+
+                while head < tail {
+                    let val = *q_vals_ptr.add(head);
+                    if curr_sum - val >= k {
+                        let idx = *q_idxs_ptr.add(head);
+                        let len = curr_idx - idx;
+                        if len < min_len {
+                            min_len = len;
+                        }
+                        head += 1;
+                    } else {
+                        break;
+                    }
                 }
-            }
 
-            // Back Pruning (Monotonicity)
-            while deque.back().map_or(false, |&(s, _)| sum <= s) {
-                deque.pop_back();
+                while head < tail {
+                    let val = *q_vals_ptr.add(tail - 1);
+                    if curr_sum <= val {
+                        tail -= 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                *q_vals_ptr.add(tail) = curr_sum;
+                *q_idxs_ptr.add(tail) = curr_idx;
+                tail += 1;
             }
-            deque.push_back((sum, cur_idx));
         }
 
-        if ans > n { -1 } else { ans as i32 }
+        if min_len > n as i32 { -1 } else { min_len }
     }
 }
