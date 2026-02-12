7095030bb5bafc265638e0e511f422deaa659771
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 09:14:33 +0530

    Accepted: Subarrays with K Different Integers

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,39 +1,59 @@
-//o(n), o(n)
-
 class Solution {
-
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+        return atMost(nums, k) - atMost(nums, k-1);
     }
 
-    private int countAtMost(int[] nums, int k) {
+    private int atMostK(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int cnt = 0;
         int left = 0;
-        int result = 0;
+        for(int right = 0; right < nums.length; right++) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+
+            while(map.size() > k) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+
+                if(map.get(nums[left]) == 0) map.remove(nums[left]);
+                left++;
+            }
+            
+            cnt += right - left + 1;
+        }
+
+        return cnt;
+    }
+
+    private int atMost(int[] nums, int k) {
+
+        int[] freq = new int[nums.length + 1];
+
+        int left = 0;
+        int count = 0;
         int unique = 0;
 
-        int[] freq = new int[nums.length+1];
+        for(int right = 0; right < nums.length; right++) {
 
-        for (int right = 0; right < nums.length; right++) {
+            // Unique Case
+            if(freq[nums[right]] == 0) unique++;
 
-            // Add nums[right] into window
-            if (freq[nums[right]] == 0) {
-                unique++;
-            }
+            // Expand Window
             freq[nums[right]]++;
 
-            // Shrink window if distinct > k
-            while (unique > k) {
+            // Srink Window
+            while(unique > k) {
+                
                 freq[nums[left]]--;
-                if (freq[nums[left]] == 0) {
-                    unique--;
-                }
+
+                if(freq[nums[left]] == 0) unique--;
+
                 left++;
             }
 
-            // Count valid subarrays
-            result += right - left + 1;
+            count += right - left + 1;
         }
 
-        return result;
+        return count;
     }
 }
