3660ea85b01e60838cbf8e6427ec53dc13f16966
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 11:13:49 +0530

    Accepted: Shortest Subarray with Sum at Least K

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
@@ -1,40 +1,81 @@
-use std::collections::VecDeque;
-
 impl Solution {
     pub fn shortest_subarray(nums: Vec<i32>, k: i32) -> i32 {
         let n = nums.len();
         let k = k as i64;
+        let mut min_len = (n + 1) as i32;
 
-        let (_, _, min_len) = nums.iter().enumerate().fold(
-            (0i64, VecDeque::from([(0i64, 0)]), n + 1),
-            |(mut sum, mut deque, mut ans), (i, &num)| {
-                sum += num as i64;
-                let idx = i + 1;
+        // Allocate memory without initialization (Fastest possible allocation)
+        // We need:
+        // P: Prefix sums (i64)
+        // Q: Deque indices (i32) - sufficient since N <= 10^5
+        let mut p = Vec::<i64>::with_capacity(n + 1);
+        let mut q = Vec::<i32>::with_capacity(n + 1);
 
-                while let Some(&(_, start_idx)) = deque.front()
-                    && idx - start_idx >= ans
-                {
-                    deque.pop_front();
+        unsafe {
+            // Get raw pointers to the allocated memory buffers
+            let p_ptr = p.as_mut_ptr();
+            let q_ptr = q.as_mut_ptr();
+
+            // Initialize the first element: P[0] = 0
+            *p_ptr = 0;
+
+            // Initialize Deque with index 0
+            let mut head = 0;
+            let mut tail = 0;
+            *q_ptr.add(tail) = 0;
+            tail += 1;
+
+            let mut curr_sum: i64 = 0;
+
+            // Raw loop over the input array
+            for i in 0..n {
+                // Read nums[i] without bounds check
+                // 'i' here represents the array index
+                // Corresponding prefix sum index will be 'i + 1'
+                curr_sum += *nums.get_unchecked(i) as i64;
+                
+                // Write P[i+1]
+                *p_ptr.add(i + 1) = curr_sum;
+
+                // 1. SHRINK FROM HEAD (Find valid shortest)
+                // While (Current Sum - Oldest Sum >= K)
+                while head < tail {
+                    let front_idx = *q_ptr.add(head) as usize;
+                    let prefix_front = *p_ptr.add(front_idx);
+                    
+                    if curr_sum - prefix_front >= k {
+                        let len = (i + 1) as i32 - front_idx as i32;
+                        // Branchless min update is sometimes slower, sticking to if
+                        if len < min_len { 
+                            min_len = len; 
+                        }
+                        head += 1; // Pop front
+                    } else {
+                        break;
+                    }
                 }
 
-                while let Some(&(s, start_idx)) = deque.front()
-                    && sum - s >= k
-                {
-                    ans = idx - start_idx;
-                    deque.pop_front();
+                // 2. MAINTAIN MONOTONICITY (Shrink from Tail)
+                // Keep P[indices] increasing in the deque.
+                // If Current Sum <= P[Newest Index], the newest index is useless.
+                while head < tail {
+                    // Peek back
+                    let back_idx = *q_ptr.add(tail - 1) as usize;
+                    let prefix_back = *p_ptr.add(back_idx);
+                    
+                    if curr_sum <= prefix_back {
+                        tail -= 1; // Pop back
+                    } else {
+                        break;
+                    }
                 }
 
-                while let Some(&(s, _)) = deque.back()
-                    && sum <= s
-                {
-                    deque.pop_back();
-                }
+                // 3. PUSH BACK
+                *q_ptr.add(tail) = (i + 1) as i32;
+                tail += 1;
+            }
+        }
 
-                deque.push_back((sum, idx));
-                (sum, deque, ans)
-            },
-        );
-
-        if min_len > n { -1 } else { min_len as i32 }
+        if min_len > n as i32 { -1 } else { min_len }
     }
 }
