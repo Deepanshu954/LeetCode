8a92a63042c99df25c9e2ee8eb1147bb7e4c0594
Deepanshu954 <deepanshu95488@email.com>
2026-02-12 02:07:41 +0000

    ✅ 1046. Max Consecutive Ones III | Accepted | sub:1916421458

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,21 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int n = nums.length;
-        int cnt = 0, maxLen = 0, left = 0;
+        int max = 0;
+        int cnt = 0;
+        int left = 0;
 
-        for(int right = 0; right < n; right++){
-
+        for(int right = 0; right < nums.length; right++) {
             if(nums[right] == 0) cnt++;
 
-            while(cnt > k){
+            while(cnt > k) {
                 if(nums[left] == 0) cnt--;
+
                 left++;
             }
 
-            maxLen = Math.max(maxLen, right - left + 1);
+            max = Math.max(max, right - left + 1);
         }
-
-        return maxLen;
-        
+        return max;
     }
 }
