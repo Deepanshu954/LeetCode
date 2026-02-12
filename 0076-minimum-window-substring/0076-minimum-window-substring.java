3c6e1b1f093c1bfda958678581d77331f4265961
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 09:33:28 +0530

    Time Limit Exceeded: Minimum Window Substring

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,43 +1,44 @@
-import java.util.*;
-
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() > s.length()) return "";
 
-        Map<Character, Integer> mapT = new HashMap<>();
-        for (char ch : t.toCharArray())
-            mapT.put(ch, mapT.getOrDefault(ch, 0) + 1);
+        HashMap<Character, Integer> mapt = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            mapt.put(ch, mapt.getOrDefault(ch, 0)+1);
+        }
 
-        Map<Character, Integer> mapS = new HashMap<>();
-        int have = 0, need = mapT.size();
-        int left = 0, minLen = Integer.MAX_VALUE;
-        int start = 0; // starting index of min window
+        int have = 0;
+        int left = 0;
+        int start = 0;
+        int need = mapt.size();
+        int minLen = Integer.MIN_VALUE;
 
-        for (int right = 0; right < s.length(); right++) {
+        HashMap<Character, Integer> maps = new HashMap<>();
+
+        for(int right = 0; right < s.length(); right++){
             char ch = s.charAt(right);
-            mapS.put(ch, mapS.getOrDefault(ch, 0) + 1);
+            maps.put(ch, maps.getOrDefault(ch, 0)+1);
 
-            if (mapT.containsKey(ch) && mapS.get(ch).intValue() == mapT.get(ch).intValue()) {
-                have++;
-            }
+            if(mapt.containsKey(ch) && mapt.get(ch).intValue() == maps.get(ch).intValue()) have++;
 
-            while (have == need) {
-                // update min window
-                if (right - left + 1 < minLen) {
+            
+            while(have == need){
+                if(right - left + 1 < minLen){
                     minLen = right - left + 1;
                     start = left;
                 }
 
-                // shrink from left
                 char leftChar = s.charAt(left);
-                mapS.put(leftChar, mapS.get(leftChar) - 1);
-                if (mapT.containsKey(leftChar) && mapS.get(leftChar) < mapT.get(leftChar)) {
-                    have--;
-                }
-                left++;
+                maps.put(leftChar, maps.get(leftChar));
+
+                if(mapt.containsKey(leftChar) && maps.get(leftChar) < mapt.get(leftChar) ) have--;
             }
+            left++;
+
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start + start + minLen);
+
+        
     }
 }
