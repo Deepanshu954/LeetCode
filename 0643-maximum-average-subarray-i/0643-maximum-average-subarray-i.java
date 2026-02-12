8fa836848b87379baec88482965b6d091807661b
Deepanshu954 <deepanshu95488@email.com>
2026-02-12 01:35:30 +0000

    ❌ 643. Maximum Average Subarray I | Wrong Answer | sub:1916407258

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        
+        int sum = 0;
+        double maxSum = 0;
+        int left = 0;
+
+        for(int right = 0; right < nums.length; right++) {
+            if(right < k - 1) {
+                sum += nums[right];
+                continue;
+            }
+
+            sum += nums[right];
+
+            if(sum > maxSum) maxSum = sum;
+
+            sum -= nums[left++];
+        }
+
+        return maxSum/k;
+        
+    }
+}
