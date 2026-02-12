71edfce796e3ee3d309e650a4cefb2529cc014fb
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 08:53:40 +0530

    Accepted: Subarrays with K Different Integers

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,27 +1,39 @@
+//o(n), o(n)
+
 class Solution {
+
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMost(nums, k) - atMost(nums, k-1);
+        return countAtMost(nums, k) - countAtMost(nums, k - 1);
     }
 
-    private int atMost(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        int cnt = 0;
+    private int countAtMost(int[] nums, int k) {
         int left = 0;
-        for(int right = 0; right < nums.length; right++) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+        int result = 0;
+        int unique = 0;
 
+        int[] freq = new int[nums.length+1];
 
-            while(map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
+        for (int right = 0; right < nums.length; right++) {
 
-                if(map.get(nums[left]) == 0) map.remove(nums[left]);
+            // Add nums[right] into window
+            if (freq[nums[right]] == 0) {
+                unique++;
+            }
+            freq[nums[right]]++;
+
+            // Shrink window if distinct > k
+            while (unique > k) {
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0) {
+                    unique--;
+                }
                 left++;
             }
-            
-            cnt += right - left + 1;
+
+            // Count valid subarrays
+            result += right - left + 1;
         }
 
-        return cnt;
+        return result;
     }
 }
