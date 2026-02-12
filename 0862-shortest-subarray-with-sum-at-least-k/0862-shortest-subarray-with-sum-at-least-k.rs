dfd4c48cafe41095fb8e0618caf417230804a706
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-12 11:22:27 +0530

    Accepted: Shortest Subarray with Sum at Least K

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
@@ -4,32 +4,37 @@ impl Solution {
         let k = k as i64;
         let mut min_len = (n + 1) as i32;
 
-        let mut q_vals = Vec::<i64>::with_capacity(n + 1);
-        let mut q_idxs = Vec::<i32>::with_capacity(n + 1);
-
         unsafe {
-            let q_vals_ptr = q_vals.as_mut_ptr();
-            let q_idxs_ptr = q_idxs.as_mut_ptr();
-            
-            let mut head = 0;
-            let mut tail = 0;
+            // Preallocate prefix + deque in one go
+            let mut prefix = Vec::<i64>::with_capacity(n + 1);
+            prefix.set_len(n + 1);
 
-            *q_vals_ptr = 0;
-            *q_idxs_ptr = 0; 
-            tail += 1;
-
-            let mut curr_sum: i64 = 0;
+            *prefix.get_unchecked_mut(0) = 0;
 
             for i in 0..n {
+                let prev = *prefix.get_unchecked(i);
+                *prefix.get_unchecked_mut(i + 1) =
+                    prev + *nums.get_unchecked(i) as i64;
+            }
 
-                curr_sum += *nums.get_unchecked(i) as i64;
-                let curr_idx = (i + 1) as i32;
+            // Deque as raw index buffer
+            let mut dq = Vec::<usize>::with_capacity(n + 1);
+            dq.set_len(n + 1);
 
+            let dq_ptr = dq.as_mut_ptr();
+            let pref_ptr = prefix.as_ptr();
+
+            let mut head = 0usize;
+            let mut tail = 0usize;
+
+            for i in 0..=n {
+                let curr = *pref_ptr.add(i);
+
+                // Pop from front if valid
                 while head < tail {
-                    let val = *q_vals_ptr.add(head);
-                    if curr_sum - val >= k {
-                        let idx = *q_idxs_ptr.add(head);
-                        let len = curr_idx - idx;
+                    let idx = *dq_ptr.add(head);
+                    if curr - *pref_ptr.add(idx) >= k {
+                        let len = (i - idx) as i32;
                         if len < min_len {
                             min_len = len;
                         }
@@ -39,17 +44,17 @@ impl Solution {
                     }
                 }
 
+                // Maintain monotonic increasing prefix
                 while head < tail {
-                    let val = *q_vals_ptr.add(tail - 1);
-                    if curr_sum <= val {
+                    let last = *dq_ptr.add(tail - 1);
+                    if curr <= *pref_ptr.add(last) {
                         tail -= 1;
                     } else {
                         break;
                     }
                 }
 
-                *q_vals_ptr.add(tail) = curr_sum;
-                *q_idxs_ptr.add(tail) = curr_idx;
+                *dq_ptr.add(tail) = i;
                 tail += 1;
             }
         }
