e6421fb519ca838bbfe02c76ee9f6053d5865ae8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-13 07:33:06 +0530

    Wrong Answer: String to Integer (atoi)

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,28 +1,40 @@
 class Solution {
+
     public int myAtoi(String s) {
         int index = removeSpace(s, 0);
 
-        if(index >= s.length()) return 0;
+        if (index >= s.length()) return 0;
 
         int sign = 1;
-        
-        if(s.charAt(index) == '-') {sign = -1; index++;}
-        else if(s.charAt(index) == '+') {index++;}
+
+        if (s.charAt(index) == '-') {
+            sign = -1;
+            index++;
+        } else if (s.charAt(index) == '+') {
+            index++;
+        }
 
         return sign * s2n(s, index, 0);
-        
     }
 
     private int removeSpace(String s, int index) {
-        if(index >= s.length() || s.charAt(index) != ' ') return index;
+        if (index >= s.length() || s.charAt(index) != ' ')
+            return index;
+
         return removeSpace(s, index + 1);
     }
 
-    private int s2n(String s, int index,int res) {
-        
-        if(index >= s.length() || !Character.isDigit(s.charAt(index))) return res;
+    private int s2n(String s, int index, int res) {
+
+        if (index >= s.length() || !Character.isDigit(s.charAt(index)))
+            return res;
 
         int digit = s.charAt(index) - '0';
+
+        // Overflow check
+        if (res > (Integer.MAX_VALUE - digit) / 10)
+            return Integer.MAX_VALUE;
+
         res = res * 10 + digit;
 
         return s2n(s, index + 1, res);
