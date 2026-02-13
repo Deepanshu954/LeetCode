6576f7702f931845ad815691725d8e17360c050f
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-13 21:56:00 +0530

    Wrong Answer: Maximum Points You Can Obtain from Cards

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -2,18 +2,22 @@ class Solution {
     public int maxScore(int[] nums, int t) {
         int left = 0;
         int right = nums.length - 1;
-        int sum = 0;
+        int leftSum = 0;
+        int rightSum = 0;
 
-        while(t--> 0) {
-            if(nums[left] >= nums[right]) sum += nums[left];
-            else sum += nums[right];
+        // while(t--> 0) {
+        //     if(nums[left] >= nums[right]) sum += nums[left];
+        //     else sum += nums[right];
 
-            left++;
-            right--;
-        }
+        //     left++;
+        //     right--;
+        // }
+
+        for(int i = 0; i < t; i++) leftSum += nums[i];
+        while(t--> 0) rightSum += nums[right--];
 
         
 
-        return sum;
+        return Math.max(leftSum, rightSum);
     }
 }
