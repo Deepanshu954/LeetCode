558b81b12d37dc2ade8679dd7a9ccf3c9928be96
Deepanshu954 <deepanshu95488@email.com>
2026-02-15 14:56:45 +0000

    ✅ 110. Balanced Binary Tree | Accepted | sub:1920155844

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,6 +13,29 @@
  *     }
  * }
  */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        return check(root) != -1;
+    }
+
+    private int check(TreeNode root) {
+        if(root == null) return 0;
+
+        int l = check(root.left);
+        if(l == -1) return -1;
+
+        int r = check(root.right);
+        if(r == -1) return -1;
+
+        if(Math.abs(l - r) > 1) return -1;
+
+        return Math.max(l,r) + 1;
+    }
+}
+
+/*
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
@@ -36,3 +59,4 @@ class Solution {
         return Math.max(l,r) + 1;
     }
 }
+*/
