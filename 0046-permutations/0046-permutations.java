2d7ef013d49fad3e278aaab6535136f4b1e53984
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-15 14:06:45 +0530

    Accepted: Permutations

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,15 +2,16 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         helper(nums, 0, nums.length - 1, res);
-
         return res;
     }
 
     private void helper(int[] nums, int left, int right, List<List<Integer>> res) {
         if(left == right) {
+
             List<Integer> list = new ArrayList<>();
             for(int num : nums) list.add(num);
             res.add(list);
+            
         }
         else {
             for(int i = left; i <= right; i++) {
