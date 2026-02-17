25fe647ada18b5c8c129b9b12bb0704fc4a92e32
Deepanshu954 <deepanshu95488@email.com>
2026-02-17 16:43:07 +0000

    ✅ 1013. Fibonacci Number | Accepted | sub:1922368981

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -13,7 +13,7 @@ class Solution {
     private Map<Integer, Integer> map = new HashMap<>();
 
     public int fib(int n) {
-        if(n <= 1) return n;
+        if(n < 2) return n;
         
         if(map.containsKey(n)) return map.get(n);
 
