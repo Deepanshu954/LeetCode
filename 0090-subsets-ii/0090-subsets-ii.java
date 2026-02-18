01daf07168f25613e94b4a4a575bc32e0af89ed9
Deepanshu954 <deepanshu95488@email.com>
2026-02-18 14:24:01 +0000

    ✅ 90. Subsets II | Accepted | sub:1923350116

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-
         List<List<Integer>> result = new ArrayList<>();
+
         helper(nums, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void helper(int[] nums, int index, List<Integer> curr, List<List<Integer>> result) {
-        result.add(new ArrayList<>(curr));
+    private void helper(int[] nums, int index, List<Integer> unp, List<List<Integer>> result) {
+        result.add(new ArrayList<>(unp));
 
-        for (int i = index; i < nums.length; i++) {
-            
-            // Skip duplicates at same level
-            if (i > index && nums[i] == nums[i - 1]) continue;
+        for(int i = index; i < nums.length; i++) {
+            if(i > index && nums[i] == nums[i-1]) continue;
 
-            curr.add(nums[i]);
-            helper(nums, i + 1, curr, result);
-            curr.remove(curr.size() - 1);
+            unp.add(nums[i]);
+            helper(nums, i + 1, unp, result);
+            unp.remove(unp.size() - 1);
         }
     }
 }
