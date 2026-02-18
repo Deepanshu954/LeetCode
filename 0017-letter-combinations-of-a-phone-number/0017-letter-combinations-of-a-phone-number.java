06258b1c61d0c13839dbfaefd1d751f5e534d574
Deepanshu954 <deepanshu95488@email.com>
2026-02-18 07:35:30 +0000

    🛠️ 17. Letter Combinations of a Phone Number | Compile Error | sub:1923009057

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        HashMap<Character, String> map = new HashMap<>();
+        fillMap(map);
+    }
+
+    private void fillMap(HashMap<Character, String> map) {
+        map.put('2', "abc");
+        map.put('2', "abc");
+        map.put('2', "abc");
+        map.put('2', "abc");
+        map.put('2', "abc");
+    }
+
+    
+}
