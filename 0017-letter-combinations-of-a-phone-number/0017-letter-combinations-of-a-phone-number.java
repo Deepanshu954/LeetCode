a4a65fbc083f5dacc1b1babe6dc958a2175f3b4c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-18 13:19:19 +0530

    Accepted: Letter Combinations of a Phone Number

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,16 +1,38 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return new ArrayList<>();
+
         HashMap<Character, String> map = new HashMap<>();
-        fillMap(map);
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
+        List<String> result = new ArrayList<>();
+        helper(digits, 0, "", map, result);
+        return result;
     }
 
-    private void fillMap(HashMap<Character, String> map) {
-        map.put('2', "abc");
-        map.put('2', "abc");
-        map.put('2', "abc");
-        map.put('2', "abc");
-        map.put('2', "abc");
-    }
+    private void helper(String digits, int index, String temp,
+                        HashMap<Character, String> map,
+                        List<String> result) {
 
-    
+        if (index == digits.length()) {
+            result.add(temp);
+            return;
+        }
+
+        char ch = digits.charAt(index);
+        String letters = map.get(ch);
+
+        for (int i = 0; i < letters.length(); i++) {
+            helper(digits, index + 1,
+                   temp + letters.charAt(i),
+                   map, result);
+        }
+    }
 }
