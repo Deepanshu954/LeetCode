d729ae4e61a4077d0294e03b131caf7e0e3a3342
Deepanshu954 <deepanshu95488@email.com>
2026-02-19 04:13:39 +0000

    ✅ 78. Subsets | Accepted | sub:1923915787

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,18 +2,19 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
-        helper(nums, 0, new ArrayList<>(),result);
-
+        helper(nums, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void helper(int[] nums, int index, List<Integer> unp, List<List<Integer>> result) {
-        result.add(new ArrayList<>(unp));
+    private void helper(int[] nums, int index, List<Integer> curr, List<List<Integer>> result) {
+
+        result.add(new ArrayList<>(curr));
 
         for(int i = index; i < nums.length; i++) {
-            unp.add(nums[i]);
-            helper(nums, i + 1, unp, result);
-            unp.remove(unp.size() - 1);
+            curr.add(nums[i]);
+            helper(nums, i + 1, curr, result);
+            curr.remove(curr.size() - 1);
         }
+
     }
 }
