f399c110c4ce9d05a7564c8d48af77c6e92b6a11
Deepanshu954 <deepanshu95488@email.com>
2026-02-19 05:47:52 +0000

    ✅ 47. Permutations II | Accepted | sub:1924007513

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
         helper(nums, 0, result);
-
         return result;
     }
 
     private void helper(int[] nums, int index, List<List<Integer>> result) {
 
         if(index == nums.length) {
-            List list = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
             for(int num : nums) list.add(num);
             result.add(list);
             return;
         }
 
+        Set<Integer> used = new HashSet<>();
 
         for(int i = index; i < nums.length; i++) {
-            if(i > index && nums[i] == nums[i-1]) continue;
+
+            if(used.contains(nums[i])) continue;
+
+            used.add(nums[i]);
 
             swap(nums, i, index);
             helper(nums, index + 1, result);
