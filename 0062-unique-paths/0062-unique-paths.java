9a5dee3777302726aef8f323d355c3090bb5c496
Deepanshu954 <deepanshu95488@email.com>
2026-02-20 11:23:24 +0000

    ✅ 62. Unique Paths | Accepted | sub:1925314938

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return helper(m,n);
+        int[][] dp = new int[m +1][n +1];
+        return helper(m,n, dp);
     }
 
-    private int helper(int m, int n) {
+    private int helper(int m, int n, int[][] dp) {
         if(m == 1 || n == 1) return 1;
 
-        int left = helper(m - 1, n);
-        int right = helper(m, n - 1);
+        if(dp[m][n] != 0) return dp[m][n];
 
-        return left + right;
+        dp[m][n] = helper(m - 1, n, dp) + helper(m, n - 1, dp);
+
+        return dp[m][n];
     }
 }
