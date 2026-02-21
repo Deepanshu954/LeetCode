64d6fb3cfb8dff5fc5c4461b045eb8df54a266af
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-21 13:07:02 +0530

    Accepted: Prime Number of Set Bits in Binary Representation

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,15 +1,12 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
-        boolean[] prime = new boolean[33];
-        prime[2] = prime[3] = prime[5] = prime[7] =
-        prime[11] = prime[13] = prime[17] = prime[19] = true;
-
         int count = 0;
+        Set<Integer> set = new HashSet<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19));
 
         for(int i = left; i <= right; i++) {
-            if(prime[Integer.bitCount(i)]) count++;
+            int cnt = Integer.bitCount(i);
+            if(set.contains(cnt)) count++;
         }
-
         return count;
     }
 }
