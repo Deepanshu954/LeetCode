41a14c5769511035dd2a61f16aac38494432423d
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-22 11:59:03 +0530

    Accepted: Minimum Add to Make Parentheses Valid

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         if(s.length() == 0) return 0;
-        int a = 0;
-        int b = 0;
+        int cnt = 0;
+
+        Deque<Character> dq = new ArrayDeque<>();
 
         for(char ch : s.toCharArray()) {
-            if(ch == '(') a++;
-            else b++;
+            if(ch == '(') dq.addFirst(ch);
+            else {
+                if(!dq.isEmpty() && dq.peekFirst() == '(') dq.removeFirst();
+                else cnt++;
+            }
         }
 
-        if(a > b) return a - b;
-
-        return b - a;
+        return dq.size() + cnt;
         
     }
 }
