2e56ca69247304797b9a870d8a58ccbab9c1c59f
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-24 15:31:08 +0530

    Accepted: Binary Tree Level Order Traversal

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,12 +17,14 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if(root == null) return result;
+
         Deque<TreeNode> dq = new LinkedList<>();
 
         dq.addFirst(root);
 
         while(!dq.isEmpty()) {
             int size = dq.size();
+
             List<Integer> list = new ArrayList<>();
 
             for(int i = 0; i < size; i++) {
@@ -32,9 +34,9 @@ class Solution {
                 if(node.left != null) dq.addFirst(node.left);
                 if(node.right != null) dq.addFirst(node.right);
             }
+
             result.add(list);
         }
-
 
         return result;
     }
