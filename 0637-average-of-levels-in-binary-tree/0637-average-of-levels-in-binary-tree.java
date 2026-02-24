ce1672e03caa504bed79929d66c1cfb5153be7f0
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-24 16:34:30 +0530

    Wrong Answer: Average of Levels in Binary Tree

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        dq.offerFirst(root);
+
+        while(!dq.isEmpty()) {
+            int size = dq.size();
+            double ans = 0;
+
+
+            for(int i = 0; i < size; i++) {
+                TreeNode node = dq.removeLast();
+
+                ans = ans + (double)node.val/size;
+
+                if(node.left != null) dq.addFirst(node.left);
+                if(node.right != null) dq.addFirst(node.right);
+            }
+
+            result.add(ans);
+        }
+
+        return result;
+    }
+}
