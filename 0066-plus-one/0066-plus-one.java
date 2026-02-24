60807485835cb49c2367b1af6d13ab23c4577f97
Deepanshu954 <deepanshu95488@email.com>
2026-02-24 03:19:09 +0000

    ✅ 66. Plus One | Accepted | sub:1929143018

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,18 +1,25 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        int index = -1;
+
         for(int i = digits.length - 1; i >= 0; i--) {
-            
-            if(digits[i] < 9) {
-                digits[i]++;
-                return digits;
+            if(digits[i] != 9) {
+                index = i;
+                break;
             }
-            
-            digits[i] = 0; // carry
         }
-        
-        // If we reach here → all digits were 9
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
+
+        // If all digits are 9
+        if(index == -1) {
+            int[] arr = new int[digits.length + 1];
+            arr[0] = 1;
+            return arr;
+        }
+
+        digits[index] += 1;
+
+        Arrays.fill(digits, index + 1, digits.length, 0);
+
+        return digits;
     }
 }
