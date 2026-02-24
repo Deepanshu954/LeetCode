bd1895144d0472c0e4753c4579254dfd1764f038
Deepanshu954 <deepanshu95488@email.com>
2026-02-24 10:18:09 +0000

    ✅ 103. Binary Tree Zigzag Level Order Traversal | Accepted | sub:1929520259

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,72 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root == null) return result;
-
-        Deque<TreeNode> dq = new LinkedList<>();
-
-        dq.addFirst(root);
-
-        while(!dq.isEmpty()) {
-            int size = dq.size();
-
-            List<Integer> l = new ArrayList<>();
-
-            for(int i = 0; i < size; i++) {
-                TreeNode node = dq.removeLast();
-                l.add(node.val);
-
-                if(node.right != null) dq.addFirst(node.right);
-                if(node.left != null) dq.addFirst(node.left);
-                
-            }
-
-            result.add(l);
-
-            if(dq.isEmpty()) return result;
-
-
-
-
-
-            size = dq.size();
-
-
-            List<Integer> list = new ArrayList<>();
-
-            for(int i = 0; i < size; i++) {
-                TreeNode node = dq.removeLast();
-                list.add(node.val);
-
-                if(node.left != null) dq.addFirst(node.left);
-                if(node.right != null) dq.addFirst(node.right);
-            }
-
-            result.add(list);
-        }
-
-        return result;
-    }
-}
-
-
-/*
-
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
@@ -100,5 +31,3 @@ class Solution {
         return result;
     }
 }
-
-*/
