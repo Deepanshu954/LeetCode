3c89181f37f64257276c15f4d481975cd2c45f53
Deepanshu954 <deepanshu95488@email.com>
2026-02-24 03:18:02 +0000

    ✅ 66. Plus One | Accepted | sub:1929142310

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,12 +1,8 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int index = digits.length - 1;
+        int index = -1;
 
-        if(digits[index] != 9) {
-            digits[index] += 1;
-            return digits;
-        }
-
+        // Find first non-9 digit from right
         for(int i = digits.length - 1; i >= 0; i--) {
             if(digits[i] != 9) {
                 index = i;
@@ -14,16 +10,18 @@ class Solution {
             }
         }
 
-        if(index == digits.length - 1) {
+        // If all digits are 9
+        if(index == -1) {
             int[] arr = new int[digits.length + 1];
-            Arrays.fill(arr, 0);
             arr[0] = 1;
-
             return arr;
         }
 
+        // Increase that digit
         digits[index] += 1;
-        Arrays.fill(digits, 0 ,index + 1, digits.length);
+
+        // Make all digits after it zero
+        Arrays.fill(digits, index + 1, digits.length, 0);
 
         return digits;
     }
