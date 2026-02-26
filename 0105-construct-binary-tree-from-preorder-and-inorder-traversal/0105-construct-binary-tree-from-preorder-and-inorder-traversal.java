d8af406483b520210dfddcc4514b2e5207969b28
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-02-26 14:21:49 +0530

    Accepted: Construct Binary Tree from Preorder and Inorder Traversal

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,36 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0) return null;
 
-        int r = preorder[0];
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) return null;
+
+        int rootVal = preorder[0];
         int index = 0;
 
-        for(int i = 0; i < inorder.length; i++) {
-            if(r == inorder[i]) index = i;
+        // Find root in inorder
+        for (int i = 0; i < inorder.length; i++) {
+            if (rootVal == inorder[i]) {
+                index = i;
+                break;
+            }
         }
 
-        TreeNode node = new TreeNode(r);
+        TreeNode root = new TreeNode(rootVal);
 
-        node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
+        // Build left subtree
+        root.left = buildTree(
+                Arrays.copyOfRange(preorder, 1, index + 1),
+                Arrays.copyOfRange(inorder, 0, index)
+        );
 
-        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
+        // Build right subtree
+        root.right = buildTree(
+                Arrays.copyOfRange(preorder, index + 1, preorder.length),
+                Arrays.copyOfRange(inorder, index + 1, inorder.length)
+        );
 
-        return node;
-
+        return root;
     }
 }
