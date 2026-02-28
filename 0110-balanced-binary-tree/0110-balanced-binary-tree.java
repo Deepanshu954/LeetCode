2a9ab7b5444f712f40a3c8ebfedd96f87742e43b
Deepanshu954 <deepanshu95488@email.com>
2026-02-28 11:47:56 +0000

    ✅ 110. Balanced Binary Tree | Accepted | sub:1933585382

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,22 +13,50 @@
  *     }
  * }
  */
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return helper(root) != -1;
+        if(root == null) return true;
+        return check(root) != -1;
     }
 
-    private int helper(TreeNode root) {
+    private int check(TreeNode root) {
         if(root == null) return 0;
 
-        int left = helper(root.left);
-        if(left == -1) return -1;
+        int l = check(root.left);
+        if(l == -1) return -1;
 
-        int right = helper(root.right);
-        if(right == -1) return -1;
+        int r = check(root.right);
+        if(r == -1) return -1;
 
-        if(Math.abs(left - right) > 1) return -1;
+        if(Math.abs(l - r) > 1) return -1;
 
-        return Math.max(left, right) + 1;
+        return Math.max(l,r) + 1;
     }
 }
+
+/*
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+
+        int l = check(root.left);
+        int r = check(root.right);
+
+        if(Math.abs(l - r) > 1) return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
+
+        
+    }
+
+    private int check(TreeNode root) {
+        if(root == null) return 0;
+
+        int l = check(root.left);
+        int r = check(root.right);
+
+        return Math.max(l,r) + 1;
+    }
+}
+*/
