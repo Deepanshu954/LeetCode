7a518b72addaec46cae865e724598bdceed08cdd
Deepanshu954 <deepanshu95488@email.com>
2026-02-28 15:28:49 +0000

    ✅ 101. Symmetric Tree | Accepted | sub:1933809199

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,16 +15,15 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-
-        return check(root.left, root.right);
+        return helper(root.left, root.right);
     }
 
-    private boolean check(TreeNode node1, TreeNode node2) {
+    private boolean helper(TreeNode node1, TreeNode node2) {
         if(node1 == null && node2 == null) return true;
         if(node1 == null || node2 == null) return false;
-        if(node1.val == node2.val) 
-            return check(node1.left, node2.right) && check(node1.right, node2.left);
-        else return false;
+        if(node1.val != node2.val) return false;
+            
+            
+        return helper(node1.left, node2.right) && helper(node1.right, node2.left);
     }
 }
