b5cf8825558ff4aff088f414da419638c328b5d9
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-01 09:27:56 +0530

    Accepted: Validate Binary Search Tree

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,17 +15,15 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private boolean helper(TreeNode root, Long min, Long max) {
+    boolean validate(TreeNode root, Long min, Long max) {
         if(root == null) return true;
 
         if(root.val >= max || root.val <= min) return false;
 
-
-        return helper(root.left, min, (long)root.val) && 
-                helper(root.right, (long)root.val, max);
+        return validate(root.left, min, (long)root.val) &&
+            validate(root.right, (long)root.val, max);
     }
 }
