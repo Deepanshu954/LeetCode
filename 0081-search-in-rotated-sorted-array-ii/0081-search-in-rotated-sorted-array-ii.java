7965554f8ec732516dce5e221e1d976c0e06a7bc
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-03 15:39:15 +0530

    Wrong Answer: Search in Rotated Sorted Array II

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -10,11 +10,11 @@ class Solution {
             if(nums[mid] == target) return true;
             
             if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target <= nums[mid]) right = mid - 1;
+                if(nums[left] < target && target < nums[mid]) right = mid - 1;
                 else left = mid + 1;
             }
             else{
-                if(nums[mid] < target && target <= nums[right]) left = mid + 1;
+                if(nums[mid] < target && target < nums[right]) left = mid + 1;
                 else right = mid - 1;
             }
         }
