bcb33c6ae01730cbc1616ccd4cc09e6e22ecdf68
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-05 06:49:09 +0530

    Accepted: Integer to Roman

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,15 +1,40 @@
 class Solution {
     public String intToRoman(int num) {
-  int[] values =    {1000, 900, 500, 400, 100, 90,  50, 40,  10, 9,  5, 4, 1};
-        String[] romans = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        String[][] roman = {
+            {"I","V","X"},   // ones
+            {"X","L","C"},   // tens
+            {"C","D","M"},   // hundreds
+            {"M","",""}      // thousands
+        };
+
+        int len = (int)Math.log10(num) + 1;
+        int place = (int)Math.pow(10, len - 1);
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                sb.append(romans[i]);
+        while(place > 0) {
+
+            int digit = num / place;
+            num %= place;
+
+            int pos = (int)Math.log10(place);
+
+            if(digit <= 3) {
+                sb.append(roman[pos][0].repeat(digit));
             }
+            else if(digit == 4) {
+                sb.append(roman[pos][0]).append(roman[pos][1]);
+            }
+            else if(digit <= 8) {
+                sb.append(roman[pos][1]);
+                sb.append(roman[pos][0].repeat(digit - 5));
+            }
+            else {
+                sb.append(roman[pos][0]).append(roman[pos][2]);
+            }
+
+            place /= 10;
         }
 
         return sb.toString();
