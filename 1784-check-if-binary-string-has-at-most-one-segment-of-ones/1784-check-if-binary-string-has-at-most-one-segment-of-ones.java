2972bef2e7382f36c75268316fde584d071c67d8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-06 08:00:45 +0530

    Wrong Answer: Check if Binary String Has at Most One Segment of Ones

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
+        if(s.length() == 1 && s.charAt(0) == '1') return true;
         int cnt = 0;
 
         for(char ch : s.toCharArray()) {
