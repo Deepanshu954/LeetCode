3f02017b15777e9277901d69b41f89d572c8245c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-07 07:16:50 +0530

    Accepted: Subarray Sum Equals K

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,20 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int sum = 0;
+        //int n = nums.length;
         int count = 0;
+        int prefixSum = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
         map.put(0,1);
 
         for(int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if(map.containsKey(sum - nums[i])) {
-                count += map.get(sum - nums[i]);
+            prefixSum += nums[i];
+
+            if(map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
             }
 
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
     }
