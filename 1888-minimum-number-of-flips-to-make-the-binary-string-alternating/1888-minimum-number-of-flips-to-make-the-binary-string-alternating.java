babc9466c1c959d378b0ae4b2c6b93a5350bf2e3
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-07 06:00:35 +0530

    Accepted: Minimum Number of Flips to Make the Binary String Alternating

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,15 +1,29 @@
 class Solution {
     public int minFlips(String s) {
-        int flip1 = 0;
-        int flip2 = 0;
+        int n = s.length();
+        String str = s + s;
 
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        int alt1 = 0, alt2 = 0;
+        int res = Integer.MAX_VALUE;
 
-            if(c != (i % 2 == 0 ? '0' : '1')) flip1++;
-            if(c != (i % 2 == 0 ? '1' : '0')) flip2++;
+        int l = 0;
+
+        for(int r = 0; r < str.length(); r++) {
+
+            if(str.charAt(r) != (r % 2 == 0 ? '0' : '1')) alt1++;
+            if(str.charAt(r) != (r % 2 == 0 ? '1' : '0')) alt2++;
+
+            if(r - l + 1 > n) {
+                if(str.charAt(l) != (l % 2 == 0 ? '0' : '1')) alt1--;
+                if(str.charAt(l) != (l % 2 == 0 ? '1' : '0')) alt2--;
+                l++;
+            }
+
+            if(r - l + 1 == n) {
+                res = Math.min(res, Math.min(alt1, alt2));
+            }
         }
 
-        return Math.min(flip1, flip2);
+        return res;
     }
 }
