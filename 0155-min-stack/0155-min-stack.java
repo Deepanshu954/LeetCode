2063f060ed7565fc91253e55e67578abf5088476
Deepanshu954 <deepanshu95488@email.com>
2026-03-08 10:57:04 +0000

    ✅ 155. Min Stack | Accepted | sub:1941868297

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,26 +1,26 @@
 class MinStack {
-	        private Deque<Integer> stack = new ArrayDeque<>();
-	        private Deque<Integer> minStack = new ArrayDeque<>();
-	        
-	        public MinStack() {
-            }
-            
-            public void push(int val) {
-                if (minStack.isEmpty() || val <= minStack.peek()) 
-                    minStack.push(val);
-                stack.push(val);
-            }
-            
-            public void pop() {
-                int v = stack.pop();
-                if (minStack.peek() == v) minStack.pop();
-            }
-            
-            public int top() {
-                return stack.peek();
-            }
-            
-            public int getMin() {
-                return minStack.peek();
-            }
+    private Deque<Integer> stack = new ArrayDeque<>();
+    private Deque<Integer> minStack = new ArrayDeque<>();
+    
+    public MinStack() {
+    }
+    
+    public void push(int val) {
+        if (minStack.isEmpty() || val <= getMin()) 
+            minStack.push(val);
+        stack.push(val);
+    }
+    
+    public void pop() {
+        int v = stack.pop();
+        if (getMin() == v) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
 }
