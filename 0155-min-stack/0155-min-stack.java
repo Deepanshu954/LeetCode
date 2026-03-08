a46335ecbd643aa1af557e160f507adf75873187
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-08 16:24:09 +0530

    Accepted: Min Stack

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,32 +1,35 @@
 class MinStack {
-
-    private int[] stack = new int[30000];
-    private int[] minStack = new int[30000];
-    private int index = -1;
-
-    public MinStack() {}
-
-    public void push(int val) {
-        index++;
-        stack[index] = val;
-
-        if (index == 0) {
-            minStack[index] = val;
-        } else {
-            minStack[index] = Math.min(val, minStack[index - 1]);
-        }
-    }
-
-    public void pop() {
-        if (index == -1) return;
-        index--;
-    }
-
-    public int top() {
-        return stack[index];
-    }
-
-    public int getMin() {
-        return minStack[index];
-    }
+	        private Deque<Integer> stack = new ArrayDeque<>();
+	        private Deque<Integer> minStack = new ArrayDeque<>();
+	        
+	        public MinStack() {
+            }
+            
+            public void push(int val) {
+                if (minStack.isEmpty() || val <= minStack.peek()) 
+                    minStack.push(val);
+                stack.push(val);
+            }
+            
+            public void pop() {
+                int v = stack.pop();
+                if (minStack.peek() == v) minStack.pop();
+            }
+            
+            public int top() {
+                return stack.peek();
+            }
+            
+            public int getMin() {
+                return minStack.peek();
+            }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
