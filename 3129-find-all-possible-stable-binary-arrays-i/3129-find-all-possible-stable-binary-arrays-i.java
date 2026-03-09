73faafa8e27f58147bc3aeba3a54a404c314838e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-09 12:09:38 +0530

    Accepted: Find All Possible Stable Binary Arrays I

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -1,43 +1,27 @@
 class Solution {
-
-    int MOD = 1000000007;
-    long[][][][] dp;
-
     public int numberOfStableArrays(int zero, int one, int limit) {
+        final int mod = 1_000_000_007;
+        int L = limit + 1;
 
-        dp = new long[zero + 1][one + 1][3][limit + 1];
+        int[][] dp0 = new int[zero + 1][one + 1]; // i 0s + j 1s ending with 0
+        int[][] dp1 = new int[zero + 1][one + 1]; // i 0s + j 1s ending with 1
 
-        for(int i=0;i<=zero;i++)
-            for(int j=0;j<=one;j++)
-                for(int k=0;k<3;k++)
-                    Arrays.fill(dp[i][j][k], -1);
+        // Base cases: only zeros or only ones => only one string if len <= min(limit, zero/one)
+        for (int i = 1; i <= Math.min(zero, limit); ++i) dp0[i][0] = 1;
+        for (int j = 1; j <= Math.min(one, limit); ++j) dp1[0][j] = 1;
 
-        return (int) dfs(zero, one, 2, 0, limit);
-    }
+        // DP iterations
+        for (int i = 1; i <= zero; ++i) {
+            for (int j = 1; j <= one; ++j) {
+                dp0[i][j] = (dp0[i - 1][j] + dp1[i - 1][j] - (i >= L ? dp1[i - L][j] : 0)) % mod;
+                dp1[i][j] = (dp1[i][j - 1] + dp0[i][j - 1] - (j >= L ? dp0[i][j - L] : 0)) % mod;
 
-    long dfs(int z, int o, int last, int streak, int limit) {
-
-        if (z == 0 && o == 0) return 1;
-
-        if (dp[z][o][last][streak] != -1)
-            return dp[z][o][last][streak];
-
-        long res = 0;
-
-        if (z > 0) {
-            if (last != 0)
-                res += dfs(z - 1, o, 0, 1, limit);
-            else if (streak < limit)
-                res += dfs(z - 1, o, 0, streak + 1, limit);
+                // Fix negatives
+                dp0[i][j] = (dp0[i][j] + mod) % mod;
+                dp1[i][j] = (dp1[i][j] + mod) % mod;
+            }
         }
 
-        if (o > 0) {
-            if (last != 1)
-                res += dfs(z, o - 1, 1, 1, limit);
-            else if (streak < limit)
-                res += dfs(z, o - 1, 1, streak + 1, limit);
-        }
-
-        return dp[z][o][last][streak] = res % MOD;
+        return (dp0[zero][one] + dp1[zero][one]) % mod;
     }
 }
