62e1d0d078a2d598a38e80ba361927ba5155d707
Deepanshu954 <deepanshu95488@email.com>
2026-03-09 06:39:18 +0000

    ✅ 3406. Find All Possible Stable Binary Arrays I | Accepted | sub:1942573835

--- 3406-find-all-possible-stable-binary-arrays-i/3406-find-all-possible-stable-binary-arrays-i.java
+++ 3406-find-all-possible-stable-binary-arrays-i/3406-find-all-possible-stable-binary-arrays-i.java
@@ -1,17 +1,25 @@
 class Solution {
 
     int MOD = 1000000007;
-    Long[][][][] dp;
+    long[][][][] dp;
 
     public int numberOfStableArrays(int zero, int one, int limit) {
-        dp = new Long[zero + 1][one + 1][3][limit + 1];
+
+        dp = new long[zero + 1][one + 1][3][limit + 1];
+
+        for(int i=0;i<=zero;i++)
+            for(int j=0;j<=one;j++)
+                for(int k=0;k<3;k++)
+                    Arrays.fill(dp[i][j][k], -1);
+
         return (int) dfs(zero, one, 2, 0, limit);
     }
 
     long dfs(int z, int o, int last, int streak, int limit) {
+
         if (z == 0 && o == 0) return 1;
 
-        if (dp[z][o][last][streak] != null)
+        if (dp[z][o][last][streak] != -1)
             return dp[z][o][last][streak];
 
         long res = 0;
