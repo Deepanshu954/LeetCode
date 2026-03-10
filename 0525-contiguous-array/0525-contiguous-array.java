2ae17f9ed61de1485e177f7f83b06c7775563a2a
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-10 08:39:03 +0530

    Accepted: Contiguous Array

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,20 +1,40 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int max = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int sum = 0;
+        int maxLen = 0;
 
         for(int i = 0; i < nums.length; i++) {
-            int cnt0 = 0;
-            int cnt1 = 0;
-            for(int j = i; j < nums.length; j++) {
-                if(nums[j] == 0) cnt0++;
-                else cnt1++;
+            sum += nums[i] == 0 ? -1 : 1;
 
-                if(cnt0 == cnt1) {
-                    max = Math.max(cnt0 * 2, max);
-                }
-            }
+            if(map.containsKey(sum)) maxLen = Math.max(maxLen, i - map.get(sum));
+            else map.put(sum, i);
         }
 
-        return max;
+        return maxLen;
     }
 }
+
+
+// class Solution {
+//     public int findMaxLength(int[] nums) {
+//         int max = 0;
+
+//         for(int i = 0; i < nums.length; i++) {
+//             int cnt0 = 0;
+//             int cnt1 = 0;
+//             for(int j = i; j < nums.length; j++) {
+//                 if(nums[j] == 0) cnt0++;
+//                 else cnt1++;
+
+//                 if(cnt0 == cnt1) {
+//                     max = Math.max(cnt0 * 2, max);
+//                 }
+//             }
+//         }
+
+//         return max;
+//     }
+// }
