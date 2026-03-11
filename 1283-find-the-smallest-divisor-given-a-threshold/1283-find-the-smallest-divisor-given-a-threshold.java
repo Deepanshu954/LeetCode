fdf8150ee78eddb1b19a9034c30747a8df9fda52
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-11 13:24:00 +0530

    Wrong Answer: Find the Smallest Divisor Given a Threshold

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int th) {
+        int low = 0;
+        int high = 0;
+
+        for(int num : nums) high += num;
+
+        int ans = high;
+        while(low < high) {
+            int mid = low + (high - low)/2;
+
+            if(isValid(nums, mid, th)) {
+                ans = mid;
+                high = mid - 1;
+            } else low = mid + 1;
+        }
+
+        return ans;
+    }
+
+    private boolean isValid(int[] nums, int a, int th) {
+        int cnt = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            cnt += (nums[i] + a - 1) / a;
+        }
+
+        return cnt <= th;
+    }
+}
