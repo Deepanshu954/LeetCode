91505325e91cb5e6836474071b224f462bfb9bb6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-11 09:47:48 +0530

    Accepted: Complement of Base 10 Integer

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -3,7 +3,6 @@ class Solution {
         if(n == 0) return 1;
         int bits = 32 - Integer.numberOfLeadingZeros(n);
 
-        int res = (1 << bits) - n - 1;
-        return res;
+        return (1 << bits) - n - 1;
     }
 }
