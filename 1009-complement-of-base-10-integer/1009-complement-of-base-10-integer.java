c10ee0853afbd2bd085a8ec9e209cc51c86f1e01
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-11 09:46:48 +0530

    Wrong Answer: Complement of Base 10 Integer

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,5 +1,6 @@
 class Solution {
     public int bitwiseComplement(int n) {
+        if(n == 0) return 0;
         int bits = 32 - Integer.numberOfLeadingZeros(n);
 
         int res = (1 << bits) - n - 1;
