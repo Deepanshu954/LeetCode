2ae075e91524d305018d8c19a4406f9a7580aeb7
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-12 15:27:48 +0530

    Accepted: Insert Interval

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int[][] insert(int[][] inv, int[] new) {
+    public int[][] insert(int[][] inv, int[] newI) {
         List<int[]> res = new ArrayList<>();
 
         int n = inv.length;
         int idx = 0;
 
-        while(i < n && inv[idx][1] < new[0]) res.add(inv[idx++]);
+        while(idx < n && inv[idx][1] < newI[0]) res.add(inv[idx++]);
 
-        while(i < n && inv[idx][0] < new[1]) {
-            new[0] = Math.min(new[0], inv[idx][0]);
-            new[1] = Math.max(new[1], inv[idx][1]);
+        while(idx < n && inv[idx][0] <= newI[1]) {
+            newI[0] = Math.min(newI[0], inv[idx][0]);
+            newI[1] = Math.max(newI[1], inv[idx][1]);
             idx++;
         }
 
-        while(i < n) res.add(inv[idx++]);
+        res.add(newI);
+
+        while(idx < n) res.add(inv[idx++]);
 
         return res.toArray(new int[0][]);
     }
