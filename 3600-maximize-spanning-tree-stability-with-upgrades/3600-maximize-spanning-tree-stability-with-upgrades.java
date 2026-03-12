b5d8dbf1e7bf84dce635232c67a151bd902bd4c8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-12 07:17:42 +0530

    Accepted: Maximize Spanning Tree Stability with Upgrades

--- 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.java
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.java
@@ -1,68 +1,87 @@
-public class Solution {
+class Solution {
+    public int Comp(int [] a, int [] b) {
+        if(a[2] >= b[2]) return -1;
+        else if(a[2] < b[2]) return 1;
+        return 0;
+    }
     public int maxStability(int n, int[][] edges, int k) {
-        UnionFind initial_uf = new UnionFind(n);
-        // Observation 1: Find minimum s_i for must-have edges.
-        int min_s = Integer.MAX_VALUE;
-        for (int[] e : edges) {
-            int u = e[0], v = e[1], s = e[2], must = e[3];
-            if (must == 0) continue;
-            min_s = Math.min(min_s, s);
-            // Observation 2: The must-have edges form a cycle.
-            if (!initial_uf.union(u, v)) return -1;
-        }
-        int[] baseRank = initial_uf.rank.clone();
-        int[] baseRoot = initial_uf.root.clone();
+        // If Not single component then no MST -> -1
+        // If No of edges with must=1 > n-1 leads to cycle Hence -> -1
+        PriorityQueue<int []> pq2 = new PriorityQueue<>((x,y)->Comp(x, y));
 
-        java.util.function.IntPredicate check = (min_stability) -> {
-            // Observation 1
-            if (min_stability > min_s) return false;
-
-            // This copies the initial state, where all `must-have edges`
-            // are connected.
-            UnionFind uf = new UnionFind(n);
-            uf.rank = baseRank.clone();
-            uf.root = baseRoot.clone();
-
-            // Observation 3: List of edges to consider an upgrade
-            List<int[]> upgrade = new ArrayList<>();
-            for (int[] e : edges) {
-                int u = e[0], v = e[1], s = e[2], must = e[3];
-                if (must == 1) continue;
-                // Observation 3
-                if (s >= min_stability) {
-                    uf.union(u, v);
-                } else if (s * 2 >= min_stability) {
-                    upgrade.add(new int[]{u, v});
-                }
+        boolean poss = true;
+        Disjoint dsu = new Disjoint(n);
+        int min = Integer.MAX_VALUE;
+        for(int e [] : edges) {
+            if(e[3] == 1) {
+                poss &= dsu.Join(e[0], e[1]);
+                if(!poss) return -1;
+                min = Math.min(min, e[2]);
+            } else {
+                pq2.add(Arrays.copyOf(e, e.length));
             }
-
-            int r = k;  // number of upgrades remaining
-            for (int[] p : upgrade) {
-                int u = p[0], v = p[1];
-                if (uf.isConnected(u, v)) continue;
-                if (r == 0) return false;
-                uf.union(u, v);
-                --r;
-            }
-
-            // Observation 3
-            int root0 = uf.find(0);
-            for (int i = 1; i < n; ++i)
-                if (uf.find(i) != root0) return false;
-            return true;
-        };
-
-        int right = 0;
-        for (int[] e : edges) right = Math.max(right, e[2]);
-        right *= 2;
-        int left = -1;
-        while (left < right) {
-            int mid = left + (right - left + 1) / 2;
-            if (check.test(mid))
-                left = mid;
-            else
-                right = mid - 1;
         }
-        return left;
+        // if(pq1.size() >= n) return -1;
+        List<Integer> list2 = new ArrayList<>();
+        while(!pq2.isEmpty()) {
+            int [] curr = pq2.poll();
+            boolean flag = dsu.Join(curr[0], curr[1]);
+            if(!flag) continue;
+            list2.add(curr[2]);
+        }
+        int p = -1;
+        for(int i=0; i<n; i++) {
+            if(p == -1) p = dsu.GiveP(i);
+            else if(p != dsu.GiveP(i)) return -1;
+        }
+        
+        PriorityQueue<int []> pq = new PriorityQueue<>((x, y) -> Integer.compare(x[0], y[0]));
+        for(int ele : list2) pq.add(new int [] {ele, 0});
+        while(!pq.isEmpty()) {
+            int [] curr = pq.poll();
+            if(k > 0 && curr[1]!=1) {
+                pq.add(new int [] {curr[0]*2, 1});
+                k--;
+            } else {
+                // System.out.println("Here");
+                min = Math.min(curr[0], min);
+            }
+        }
+
+        return min;
+    }
+}
+
+class Disjoint {
+    int parent [];
+    int size [];
+
+    public Disjoint(int n) {
+        parent = new int [n+1];
+        size = new int [n+1];
+        for(int i=0; i<=n; i++) {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    public int GiveP(int x) {
+        if(parent[x] == x) return x;
+        parent[x] = GiveP(parent[x]);
+        return parent[x];
+    }
+
+    public boolean Join(int a, int b) {
+        int pa = GiveP(a);
+        int pb = GiveP(b);
+        if(pa == pb) return false;
+        if(size[pa] >= size[pb]) {
+            size[pa] += size[pb];
+            parent[pb] = pa;
+        } else {
+            size[pb] += size[pa];
+            parent[pa] = pb;
+        }
+        return true;
     }
 }
