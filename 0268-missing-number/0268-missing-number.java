7450d3d249d32b5b919356b65821763b0c76fe18
Deepanshu954 <deepanshu95488@email.com>
2026-03-13 19:15:58 +0000

    ✅ 268. Missing Number | Accepted | sub:1947344170

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,50 +1,19 @@
 class Solution {
-    public int missingNumber(int[] nums) 
-    {
-        int n = nums.length, sum = 0;
-        int reqSum = n * (n+1)/2;
-        for(int num : nums) sum += num;
+    public int missingNumber(int[] nums) {
+        int index = 0;
 
-        return reqSum - sum;
+        while(index < nums.length) {
+            if(nums[index] < nums.length && nums[index] != nums[nums[index]]) {
+                int temp = nums[index];
+                nums[index] = nums[temp];
+                nums[temp] = temp;
+            } else index++;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != i) return i;
+        }
+
+        return nums.length;
     }
 }
-
-// class Solution {
-//     public int missingNumber(int[] nums) 
-//     {
-//         int n = nums.length;
-//         // Set<Integer> s = new HashSet<>();
-        
-//         // for(int a : arr) {
-//         //     s.add(a);
-//         // }
-
-//         // //// Using Bubble Sort
-//         // // for(int i = 0; i < n; i++) {
-//         // //     for(int j = 0; j < n - 1; j++) {
-//         // //         if(arr[j] > arr[j+1]) {
-//         // //             int temp = arr[j];
-//         // //             arr[j] = arr[j+1];
-//         // //             arr[j+1] = temp;
-//         // //         }
-//         // //     }
-//         // // }
-
-//         // //// Using sort function of Arrays
-//         // // Arrays.sort(arr);
-
-//         // for(int i = 0; i < n; i++) {
-//         //     // if(arr[i] != i) return i;
-//         //     if(s.contains(i) == false) return i;
-//         // }
-
-//         int sum = 0;
-//         int reqSum = n * (n+1)/2;
-//         for(int num : nums) {
-//             sum += num;
-//         }
-
-
-//         return reqSum - sum;
-//     }
-// }
