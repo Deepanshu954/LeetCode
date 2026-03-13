7615b3d5f70ac2111f2ba4bc043273c6022877b1
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-14 01:21:16 +0530

    Accepted: Find All Duplicates in an Array

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,15 +1,39 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> s = new HashSet<>();
+        List<Integer> res = new ArrayList<>();
 
-        for(int num : nums) {
-            if(!set.add(num)) s.add(num);
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if(nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else i++;
         }
 
-        List<Integer> list = new ArrayList<>();
+        for(int j = 0; j < nums.length; j++) {
+            if(nums[j] != j + 1) res.add(nums[j]);
+        }
 
-        for(int i : s) list.add(i);
-        return list;
+        return res;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         Set<Integer> s = new HashSet<>();
+
+//         for(int num : nums) {
+//             if(!set.add(num)) s.add(num);
+//         }
+
+//         List<Integer> list = new ArrayList<>();
+
+//         for(int i : s) list.add(i);
+//         return list;
+//     }
+// }
