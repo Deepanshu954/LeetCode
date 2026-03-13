ee34165f7c7e5b2eb4ca08ffe8e4df3e12050850
Deepanshu954 <deepanshu95488@email.com>
2026-03-13 20:30:58 +0000

    ✅ 41. First Missing Positive | Accepted | sub:1947384973

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,30 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int i = 0;
-        int n = nums.length;
-
-        for(int j = 0; j < n;j++) {
-            if(nums[j] < 1) nums[j] = n + 1;
-        }
 
         while(i < nums.length) {
             int correct = nums[i] - 1;
 
-            if(nums[i] < nums.length && nums[i] != nums[correct]) {
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
                 int temp = nums[i];
                 nums[i] = nums[correct];
                 nums[correct] = temp;
             } else i++;
         }
 
-        if(nums[0] != 1) return 1;
-        if(nums[1] != 2) return 2;
-
         for(int j = 0; j < nums.length; j++) {
             if(nums[j] != j+1) return j + 1;
         }
 
-        return nums.length;
+        return nums.length + 1;
     }
 }
 
