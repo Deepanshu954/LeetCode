51b4ddfeb03fa9e7029d79697dc6f75cec86c757
Deepanshu954 <deepanshu95488@email.com>
2026-03-14 21:16:59 +0000

    ✅ 287. Find the Duplicate Number | Accepted | sub:1948412305

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,50 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int idx = 0;
 
-        while(idx < nums.length) {
-            int correctIdx = nums[idx] - 1;
+        int slow = nums[0];
+        int fast = nums[0];
 
-            if(nums[idx] != idx + 1) {
-                if(nums[idx] != nums[correctIdx]) {
-                    int temp = nums[idx];
-                    nums[idx] = nums[correctIdx];
-                    nums[correctIdx] = temp;
-                } else {
-                    return nums[idx];
-                }
+        // Phase 1: detect cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
 
-            } else idx++;
+        // Phase 2: find entry point
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return slow;
     }
 }
-
-
-// // This this as a linked list.
-// // Pointer is going to that particular index
-// // If number is same it get stuck
-
-// class Solution {
-//     public int findDuplicate(int[] nums) {
-
-//         int slow = nums[0];
-//         int fast = nums[0];
-
-//         // Phase 1: detect cycle
-//         do {
-//             slow = nums[slow];
-//             fast = nums[nums[fast]];
-//         } while (slow != fast);
-
-//         // Phase 2: find entry point
-//         slow = nums[0];
-//         while (slow != fast) {
-//             slow = nums[slow];
-//             fast = nums[fast];
-//         }
-
-//         return slow;
-//     }
-// }
