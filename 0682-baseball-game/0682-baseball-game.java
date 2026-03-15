5f3b4bb36267079b895b2a8175fbd15d3567c26c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-15 14:00:13 +0530

    Compile Error: Baseball Game

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int calPoints(String[] op) {
+        int res = 0;
+
+        for(int i = 0; i < op; i++) {
+            if(op[i] == "+") {
+                res += op[i - 1] + op[i - 2];
+            }
+
+            if(op == "D") {
+                res += 2 * 
+            }
+        }
+        
+    }
+}
