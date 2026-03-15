10d9f464084ae299671443f3d7e5fe0cd2ded2d1
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-15 22:22:25 +0530

    Accepted: Trapping Rain Water

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,48 +1,29 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = 0;
-        int rightMax = 0;
-        int ans = 0;
+        int n = height.length;
 
-        while(left < right){
-            if(height[left] < height[right])
-            {
-                if(height[left] >= leftMax) leftMax = height[left];
-                else ans += leftMax - height[left];
-                left++;
-            }
-            else
-            {
-                if(height[right] >= rightMax) rightMax = height[right];
-                else ans += rightMax - height[right];
-                right--;
+        int[] l = new int[n];
+        int[] r = new int[n];
+
+        l[0] = height[0];
+        for(int i = 1; i < n; i++) {
+            l[i] = Math.max(l[i-1], height[i]);
+        }
+
+        r[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            r[i] = Math.max(r[i+1], height[i]);
+        }
+
+        int res = 0;
+
+        for(int i = 0; i < n; i++) {
+            int calc = Math.min(l[i], r[i]);
+            if(calc > height[i]) {
+                res += calc - height[i];
             }
         }
 
-        return ans;
-        
-
-        // while (n > 0 && height[n - 1] > height[n]) n--;
-        // while (left < n && height[left] > height[left + 1]) left++;
-        // if (left == n) return 0;
-
-
-        // for(int right = left; right < n; right++){
-
-        //     rightMax = height[right];
-        //     int minHeight = Math.min(leftMax, rightMax);
-
-        //     if(rightMax < leftMax)
-            
-        //     while(left != right){
-        //         ans = minHeight - height[left];
-        //         left++;
-        //     }
-        // }
-
-        
-        
+        return res;
     }
 }
