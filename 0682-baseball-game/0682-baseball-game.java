edd496ec8c80976cd7f7d602602ce003ed32a4ea
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-15 14:04:51 +0530

    Accepted: Baseball Game

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int calPoints(String[] op) {
-        int res = 0;
+    public int calPoints(String[] operations) {
+        List<Integer> arr = new ArrayList<>();
 
-        for(int i = 0; i < op; i++) {
-            if(op[i] == "+") {
-                res += op[i - 1] + op[i - 2];
-            }
+        for (String s : operations) {
+            int n = arr.size();
 
-            if(op == "D") {
-                res += 2 * 
+            if (s.equals("+")) {
+                arr.add(arr.get(n - 1) + arr.get(n - 2));
+            } else if (s.equals("C")) {
+                arr.remove(n - 1);
+            } else if (s.equals("D")) {
+                arr.add(arr.get(n - 1) * 2);
+            } else {
+                arr.add(Integer.parseInt(s));
             }
         }
-        
+
+        int ans = 0;
+        for (int score : arr) {
+            ans += score;
+        }
+
+        return ans;
     }
 }
