1d0c0dfc6cc9326e9e51abb25f9165671d2c6251
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-17 02:39:23 +0530

    Accepted: Maximum Gap

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+
+        int gap = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+            gap = Math.max(gap, nums[i] - nums[i-1]);
+        }
+
+        return gap;
+    }
+}
