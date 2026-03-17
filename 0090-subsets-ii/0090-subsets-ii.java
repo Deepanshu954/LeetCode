4905d2f020ab49efd2277427a8767fe6c70ea11f
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-17 23:03:13 +0530

    Accepted: Subsets II

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
 
+        List<List<Integer>> result = new ArrayList<>();
         helper(nums, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void helper(int[] nums, int index, List<Integer> unp, List<List<Integer>> result) {
-        result.add(new ArrayList<>(unp));
+    private void helper(int[] nums, int index, List<Integer> curr, List<List<Integer>> result) {
+        result.add(new ArrayList<>(curr));
 
         for(int i = index; i < nums.length; i++) {
             if(i > index && nums[i] == nums[i-1]) continue;
 
-            unp.add(nums[i]);
-            helper(nums, i + 1, unp, result);
-            unp.remove(unp.size() - 1);
+            curr.add(nums[i]);
+            helper(nums, i + 1, curr, result);
+            curr.remove(curr.size() - 1);
         }
     }
 }
