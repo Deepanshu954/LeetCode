f6eccdc09e14f2956a53308cf51d28424324ab3f
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-19 02:11:23 +0530

    Accepted: Generate Parentheses

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,28 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
 
-        helper(0 , 0, n, "", res);
+        helper(0 , 0, n, sb, res);
         return res;
     }
 
-    private void helper(int open, int close, int n,String str, List<String> res) {
+    private void helper(int open, int close, int n,StringBuilder str, List<String> res) {
         if(open == n && close == n) {
-            res.add(str);
+            res.add(str.toString());
             return;
         }
 
         if(open < n) {
-            helper(open + 1, close, n, str + "(", res);
+            str.append("(");
+            helper(open + 1, close, n, str, res);
+            str.deleteCharAt(str.length() - 1);
         }
 
         if(close < open) {
-            helper(open, close + 1, n, str + ")", res);
+            str.append(")");
+            helper(open, close + 1, n, str, res);
+            str.deleteCharAt(str.length() - 1);
         }
     }
 }
