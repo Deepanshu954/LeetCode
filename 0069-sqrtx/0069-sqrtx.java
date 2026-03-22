5d8fcd87fc2841e22a70e1f5d8b1a72b36026a6b
Deepanshu954 <deepanshu95488@email.com>
2026-03-22 06:12:12 +0000

    🛠️ 69. Sqrt(x) | Compile Error | sub:1955467262

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        long low = 1;
+        long high = x;
+
+        long val = x;
+        long calc = 0;
+
+        while(low < high) {
+            long mid = low + (high - low)/2;
+
+            calc = mid * mid;
+
+            if(l) 
+        }
+    }
+}
