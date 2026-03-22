050654bb5ed1ac4f7c31ef552e2cbb2bd91000a2
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-22 11:43:31 +0530

    Accepted: Sqrt(x)

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,17 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        long low = 1;
-        long high = x;
+        int low = 0;
+        int high = x;
+        int ans = -1;
 
-        long val = x;
-        long calc = 0;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long midsq = mid * mid;
 
-        while(low < high) {
-            long mid = low + (high - low)/2;
-
-            calc = mid * mid;
-
-            if(l) 
+            if (midsq == x)
+                return (int)mid;
+            else if (midsq > x)
+                high = (int)mid - 1;
+            else {
+                ans = (int)mid;
+                low = (int)mid + 1;
+            }
         }
+
+        return ans;
     }
 }
