6457a9df36158f497b08caa28ce61282d24dcc59
Deepanshu954 <deepanshu95488@email.com>
2026-03-24 08:53:40 +0000

    ✅ 37. Sudoku Solver | Accepted | sub:1957580999

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,65 +1,66 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        if(solve( board)) return;
+        if( solve(board) ) return;
     }
 
-    public boolean solve(char[][] board) {
+    private boolean solve(char[][] board) {
         int row = -1;
         int col = -1;
 
-        // Checking Does any postion is empty or not
-        boolean emptyLeft = true;
+        boolean empty = true;
 
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(board[i][j] == '.') {
                     row = i;
                     col = j;
-                    emptyLeft = false;
+                    empty = false;
                     break;
                 }
             }
-
-            if(!emptyLeft) break;
+            if(!empty) break;
         }
 
-        // if there is no empty place reamin then return
-        if(emptyLeft) return true;;
+        if(empty) return true;
 
         for(char i = '1'; i <= '9'; i++) {
             if(isSafe(board, row, col, i)) {
                 board[row][col] = i;
                 if(solve(board)) return true;
-                else board[row][col] = '.';
+                board[row][col] = '.';
             }
         }
-        
+
         return false;
     }
 
-    private boolean isSafe(char[][] board, int row, int col, char num) {
-
-        // checking row
+    private boolean isSafe(char[][] board, int row, int col, char val) {
+        // row Check
         for(int i = 0; i < 9; i++) {
-            if(board[i][col] == num) return false;
+            if(board[i][col] == val) return false;
         }
 
-        // checking col
+        // col check
         for(int i = 0; i < 9; i++) {
-            if(board[row][i] == num) return false;
+            if(board[row][i] == val) return false;
         }
 
-        // Checking sub-box
+        // 3 x 3 grid check
         int r = row - row % 3;
         int c = col - col % 3;
 
+        // for(int i = 0; i < 3; i++) {
+        //     for(int j = 0; j < 3; j++) {
+        //         if(board[r + i][c + j] == val) return false;
+        //     }
+        // }
+
         for(int i = r; i < r + 3; i++) {
             for(int j = c; j < c + 3; j++) {
-                if(board[i][j] == num) return false;
+                if(board[i][j] == val) return false;
             }
         }
 
         return true;
     }
 }
-
