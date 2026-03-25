4a9de597380428ac6eadf4ccbc6fc13265213810
Deepanshu954 <deepanshu95488@email.com>
2026-03-25 23:58:49 +0000

    ✅ 547. Number of Provinces | Accepted | sub:1959381745

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] grid) {
         boolean[] vis = new boolean[grid.length];
-
-        int res = 0;
+        int cnt = 0;
 
         for(int i = 0; i < grid.length; i++) {
             if(!vis[i]) {
                 dfs(grid, i, vis);
-                res++;
+                cnt++;
             }
         }
 
-        return res;
+        return cnt;
     }
 
     private void dfs(int[][] grid, int node, boolean[] vis) {
         vis[node] = true;
 
-        for(int i = 0; i < grid.length; i++) {
-            if(grid[node][i] == 1 && !vis[i]) {
-                dfs(grid, i, vis);
-            }
+        for(int i = 0; i < vis.length; i++) {
+            if(grid[node][i] == 1 && !vis[i]) dfs(grid, i, vis);
         }
     }
 }
