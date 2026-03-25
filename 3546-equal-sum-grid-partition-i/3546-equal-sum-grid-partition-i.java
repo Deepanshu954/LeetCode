be2fc8346e41a28f66386dadf8feb0213086b2a6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-25 08:19:54 +0530

    Accepted: Equal Sum Grid Partition I

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -4,32 +4,28 @@ class Solution {
         long total = 0;
 
         long[] row = new long[m];
+        long[] col = new long[n];
 
-        // Calculate total + row sums
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 total += grid[i][j];
                 row[i] += grid[i][j];
+                col[j] += grid[i][j];
             }
         }
 
         if (total % 2 != 0) return false;
         long target = total / 2;
 
-        // Check horizontal cut
         long sum = 0;
         for (int i = 0; i < m - 1; i++) {
             sum += row[i];
             if (sum == target) return true;
         }
 
-        // Check vertical cut (compute on the fly)
+        sum = 0;
         for (int j = 0; j < n - 1; j++) {
-            long colSum = 0;
-            for (int i = 0; i < m; i++) {
-                colSum += grid[i][j];
-            }
-            sum += colSum;
+            sum += col[j];
             if (sum == target) return true;
         }
 
