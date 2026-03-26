d99e17eb9ed73dcb76eeb70b63b1f985d321c679
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-26 15:44:27 +0530

    Wrong Answer: Rotting Oranges

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,21 +1,23 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
         int fresh = 0;
 
-        Queue<int[]> rq = new ArrayDeque<>();
+        Queue<int[]> q = new ArrayDeque<>();
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) fresh++;
-                else if(grid[i][j] == 2) rq.offer(new int[]{i,j});
+                if(grid[i][j] == 2) q.offer(new int[] {i,j});
+                else if(grid[i][j] == 1) fresh++;
             }
         }
 
         if(fresh == 0) return 0;
+        if(q.isEmpty()) return -1;
 
         int min = 0;
+
         int[][] dir = {
             { 1, 0},
             {-1, 0},
@@ -23,22 +25,22 @@ class Solution {
             { 0,-1}
         };
 
-        while(!rq.isEmpty()) {
-            int size = rq.size();
+        while(!q.isEmpty()) {
+            int size = q.size();
 
             for(int i = 0; i < size; i++) {
-                int[] curr = rq.poll();
+                int[] curr = q.poll();
                 int x = curr[0];
                 int y = curr[1];
 
-                for(int[] d: dir) {
+                for(int[] d : dir) {
                     int nx = x + d[0];
                     int ny = y + d[1];
 
                     if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1) {
                         grid[nx][ny] = 2;
                         fresh--;
-                        rq.offer(new int[]{nx, ny});
+                        q.offer(new int[]{nx, ny});
                     }
                 }
             }
@@ -46,6 +48,6 @@ class Solution {
             min++;
         }
 
-        return fresh == 0 ? min - 1 : -1;
+        return fresh == 0 ? min - 1: -1;
     }
 }
