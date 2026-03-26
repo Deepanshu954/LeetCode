96f6eba8902e441911c410aff0e67eda83419768
Deepanshu954 <deepanshu95488@email.com>
2026-03-26 09:54:12 +0000

    ✅ 547. Number of Provinces | Accepted | sub:1959773381

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int findCircleNum(int[][] grid) {
-        boolean[] vis = new boolean[grid.length];
-        int cnt = 0;
+    public int findCircleNum(int[][] graph) {
+        boolean[] vis = new boolean[graph.length];
 
-        for(int i = 0; i < grid.length; i++) {
+        int prov = 0;
+
+        for(int i = 0; i < graph.length; i++) {
             if(!vis[i]) {
-                dfs(grid, i, vis);
-                cnt++;
+                dfs(graph, i, vis);
+                prov++;
             }
         }
 
-        return cnt;
+        return prov;
     }
 
-    private void dfs(int[][] grid, int node, boolean[] vis) {
+    private void dfs(int[][] graph, int node, boolean[] vis) {
         vis[node] = true;
 
-        for(int i = 0; i < vis.length; i++) {
-            if(grid[node][i] == 1 && !vis[i]) dfs(grid, i, vis);
+        for(int i = 0; i < graph.length; i++) {
+            if(graph[node][i] == 1 && !vis[i]) {
+                dfs(graph, i, vis);
+            }
         }
     }
 }
