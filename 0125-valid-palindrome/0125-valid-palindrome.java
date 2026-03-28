37af6897a986b88c485f0cdae946bc982112e9b5
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-28 07:02:14 +0530

    Wrong Answer: Valid Palindrome

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,41 +1,26 @@
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         int i = 0;
-//         int j = s.length() - 1;
-
-//         if(s.equals(" ")) return true;
-
-//         while(i < j){
-//             while(i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
-//             while(i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
-
-//             if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
-//             i++;
-//             j--;
-//         }
-
-//         return true;
-//     }
-// }
-
-
-
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
+        String str = s.toLowerCase();
 
-        return helper(s, 0, s.length() - 1);
-    }
+        int left = 0;
+        int right = s.length() - 1;
 
-    private boolean helper(String s, int left, int right) {
-        if(left >= right) return true;
+        while(left < right) {
+            if(str.charAt(left) < 'a' || str.charAt(left) > 'z') {
+                left++;
+                continue;
+            }
+            if(str.charAt(right) < 'a' || str.charAt(right) > 'z') {
+                right--;
+                continue;
+            }
 
-        if(!Character.isLetterOrDigit(s.charAt(left)))  return helper(s, left +1, right);
-        else if(!Character.isLetterOrDigit(s.charAt(right))) return helper(s, left, right - 1);
-        else if(s.charAt(left) != s.charAt(right)) return false;
+            if(str.charAt(left) != str.charAt(right)) return false;
 
-        return helper(s, left + 1, right - 1);
+            left++;
+            right--;
+        }
 
-        //return true;
+        return true;
     }
 }
