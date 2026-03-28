e7a35c23ea2c18d0570ea2829b41feb2f8364ea6
Deepanshu954 <deepanshu95488@email.com>
2026-03-28 01:08:58 +0000

    ✅ 11. Container With Most Water | Accepted | sub:1961342291

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,55 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
-        int max = 0;
-        int min = 0;
-
         int left = 0;
         int right = height.length - 1;
 
-        while(left < right) {
-            int localMax = 0;
+        int maxArea = 0;
+        int h = 0;
 
+        while(left <= right) {
             if(height[left] <= height[right]) {
-                min = height[left++];
-            } else {
-                min = height[right--];
+                h = height[left];
+                left++;
+            }
+            else {
+                h = height[right];
+                right--;
             }
 
-            localMax = min * (right - left + 1);
-
-            if(max < localMax) max = localMax;
+            maxArea = Math.max(maxArea, h * (right - left + 1));
         }
 
-        return max;
+        return maxArea;
     }
 }
-
-/*
-Brute Force
-
-class Solution {
-    public int maxArea(int[] height) {
-        
-        int max = 0;
-        int min = 0;
-
-        for(int i = 0; i < height.length; i++) {
-
-            int localMax = 0;
-
-            for(int j = i + 1; j < height.length; j++) {
-
-                if(height[i] < height[j]) min = height[i];
-                else min = height[j];
-
-                localMax = min * (j - i);
-
-                if(localMax > max) max = localMax;
-            }
-        }
-
-        return max;
-    }
-}
-
-*/
