fab4a5dbfcfb943375bf582a12540b49540cbeb6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-31 08:48:11 +0530

    Wrong Answer: Rotting Oranges

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,14 +2,14 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int fresh = 0;
 
+        int fresh = 0;
         Queue<int[]> q = new ArrayDeque<>();
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 2) q.offer(new int[] {i,j});
-                else if(grid[i][j] == 1) fresh++;
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2) q.offer(new int[]{i,j});
             }
         }
 
@@ -18,36 +18,30 @@ class Solution {
 
         int min = 0;
 
-        int[][] dir = {
-            { 1, 0},
-            {-1, 0},
-            { 0, 1},
-            { 0,-1}
-        };
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while(!q.isEmpty()) {
-            int size = q.size();
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
 
-            for(int i = 0; i < size; i++) {
-                int[] curr = q.poll();
-                int x = curr[0];
-                int y = curr[1];
+            boolean rot = false;
 
-                for(int[] d : dir) {
-                    int nx = x + d[0];
-                    int ny = y + d[1];
+            for(int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                    if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;
-                        fresh--;
-                        q.offer(new int[]{nx, ny});
-                    }
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 1) {
+                    fresh--;
+                    grid[nr][nc] = 2;
+                    q.offer(new int[]{nr,nc});
+                    rot = true;
                 }
             }
 
-            min++;
+            if(rot) min++;
         }
 
-        return fresh == 0 ? min - 1: -1;
+        return fresh == 0 ? min -1 : -1;
     }
 }
