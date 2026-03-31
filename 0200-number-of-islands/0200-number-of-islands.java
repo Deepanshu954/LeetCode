add429178660e443458ff68d03822cf661797c23
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-03-31 08:59:00 +0530

    Accepted: Number of Islands

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -9,12 +9,39 @@ class Solution {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == '1') {
                     res++;
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                 } 
             }
         }
 
         return res;
+    }
+
+    private void bfs(char[][] grid, int sr, int sc) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{sr,sc});
+        grid[sr][sc] = '0';
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for(int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1') {
+                    grid[nr][nc] = '0';
+                    q.offer(new int[]{nr,nc});
+                }
+            }
+        }
     }
 
     private void dfs(char[][] grid, int r, int c) {
