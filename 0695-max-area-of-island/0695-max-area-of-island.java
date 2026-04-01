1daa9813d52cb2bdfe9022edc1f973c3e023b682
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-01 09:23:01 +0530

    Accepted: Max Area of Island

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,4 +1,41 @@
 class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int maxArea = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfs(grid,i,j));
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int r, int c) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == 0) return 0;
+        grid[r][c] = 0;
+
+        return 1 +
+        dfs(grid, r-1, c) +
+        dfs(grid, r+1, c) +
+        dfs(grid, r, c-1) +
+        dfs(grid, r, c+1);
+    }
+}
+
+
+
+/*
+
+class Solution {
     private int area = 0;
     private int maxArea = 0;
 
@@ -35,3 +72,5 @@ class Solution {
         dfs(grid, r, c+1);
     }
 }
+
+*/
