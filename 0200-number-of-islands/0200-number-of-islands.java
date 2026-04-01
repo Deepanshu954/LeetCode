6b259f6854b6a70b042d1e03349c4532c03993f2
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-01 08:54:15 +0530

    Accepted: Number of Islands

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,47 +1,18 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int comp = 0;
 
-        int res = 0;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == '1') {
-                    res++;
-                    bfs(grid,i,j);
-                } 
-            }
-        }
-
-        return res;
-    }
-
-    private void bfs(char[][] grid, int sr, int sc) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{sr,sc});
-        grid[sr][sc] = '0';
-
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            int r = curr[0];
-            int c = curr[1];
-
-            for(int[] d : dir) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1') {
-                    grid[nr][nc] = '0';
-                    q.offer(new int[]{nr,nc});
+                    comp++;
+                    dfs(grid, i, j);
                 }
             }
         }
+
+
+        return comp;
     }
 
     private void dfs(char[][] grid, int r, int c) {
@@ -52,9 +23,9 @@ class Solution {
 
         grid[r][c] = '0';
 
-        dfs(grid, r-1, c);
         dfs(grid, r+1, c);
-        dfs(grid, r, c-1);
+        dfs(grid, r-1, c);
         dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
     }
 }
