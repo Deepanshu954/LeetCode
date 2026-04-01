67707d995eb4dcaa67bd7aa017c4f76098fcba98
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-01 13:40:49 +0530

    Accepted: 01 Matrix

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -3,34 +3,30 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
-        Queue<int[]> q = new ArrayDeque<>();
+        Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(mat[i][j] == 0) q.offer(new int[]{i,j});
-                else mat[i][j] = -1; 
+                else mat[i][j] = -1;
             }
         }
 
-        int[][] dir = {
-            { 1, 0},
-            {-1, 0},
-            { 0, 1},
-            { 0,-1}
-        };
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
 
         while(!q.isEmpty()) {
+
             int[] curr = q.poll();
-            int x = curr[0];
-            int y = curr[1];
+            int r = curr[0];
+            int c = curr[1];
 
-            for(int[] d: dir) {
-                int nx = x + d[0];
-                int ny = y + d[1];
+            for(int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                if(nx >= 0 && nx < m && ny >= 0 && ny < n && mat[nx][ny] == -1) {
-                    mat[nx][ny] = mat[x][y] + 1;
-                    q.offer(new int[]{nx, ny});
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && mat[nr][nc] == -1) {
+                    mat[nr][nc] = mat[r][c] + 1;
+                    q.offer(new int[]{nr,nc});
                 }
             }
         }
