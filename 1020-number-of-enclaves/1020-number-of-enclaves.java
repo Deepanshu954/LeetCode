04daf27f750bdd4f76538ad960c7d99296387a5f
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-01 09:39:56 +0530

    Accepted: Number of Enclaves

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,33 +1,27 @@
 class Solution {
-    private int maxArea = 0;
-    private int area = 0;
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 
         for(int i = 0; i < m; i++) {
             if(grid[i][0] == 1) dfs(grid,i,0);
-            if(grid[m-1][0] == 1) dfs(grid,m-1,0);
+            if(grid[i][n-1] == 1) dfs(grid,i,n-1);
         }
 
-        for(int i = 0; i < n; i++) {
-            if(grid[0][i] == 1) dfs(grid,0,i);
-            if(grid[0][n-1] == 1) dfs(grid,0,n-1);
+        for(int j = 0; j < n; j++) {
+            if(grid[0][j] == 1) dfs(grid,0,j);
+            if(grid[m-1][j] == 1) dfs(grid,m-1,j);
         }
 
-        area = 0;
 
+        int count = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) {
-                    area = 0;
-                    dfs(grid,i,j);
-                }
-                maxArea = Math.max(maxArea, area);
+                if(grid[i][j] == 1) count++;
             }
         }
 
-        return maxArea;
+        return count;
     }
 
     private void dfs(int[][] grid, int r, int c) {
@@ -36,8 +30,6 @@ class Solution {
 
         if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == 0) return;
         grid[r][c] = 0;
-
-        area++;
 
         dfs(grid, r-1, c);
         dfs(grid, r+1, c);
