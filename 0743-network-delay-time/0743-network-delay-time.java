e6da04a04873cd8d00284d34cde3aa83fbee3f69
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-03 22:14:01 +0530

    Accepted: Network Delay Time

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,27 +1,15 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        // Adj List
-
-        List<int[]>[] adj = new ArrayList[n+1];
-        for(int i = 0;i <= n; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        for(int[] time : times) {
-            int u = time[0];
-            int v = time[1];
-            int w = time[2];
-
-            adj[u].add(new int[]{v, w});
-        }
-
-        // Dijkstra
+        List<int[]>[] adj = buildAdj(n, times);
 
         int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a,b) -> a[0] - b[0]
+        );
+
         pq.offer(new int[]{0, k});
 
         while(!pq.isEmpty()) {
@@ -29,27 +17,40 @@ class Solution {
             int d = curr[0];
             int u = curr[1];
 
-            if(d > dist[u]) continue;
+            if(d > dist[u]) continue; // 🔥 restore this
 
-            for(int[] edge : adj[u]) {
-                int v = edge[0];
-                int w = edge[1];
+            for(int[] e : adj[u]) {
+                int v = e[0];
+                int w = e[1];
 
-                if(d + w < dist[v]) {
-                    dist[v] = d + w;
+                if(dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
                     pq.offer(new int[]{dist[v], v});
                 }
             }
         }
 
-        int maxDist = 0;
+        int max = 0;
         for(int i = 1; i <= n; i++) {
             if(dist[i] == Integer.MAX_VALUE) return -1;
-
-            //maxDist = Math.max(maxDist, dist[i]);
-            if(maxDist < dist[i]) maxDist = dist[i];
+            max = Math.max(max, dist[i]);
         }
 
-        return maxDist;
+        return max;
+    }
+
+    private List<int[]>[] buildAdj(int V, int[][] edges) {
+        List<int[]>[] adj = new ArrayList[V+1];
+
+        for(int i = 0; i <= V; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            adj[u].add(new int[]{v, w}); // 🔥 directed only
+        }
+
+        return adj;
     }
 }
