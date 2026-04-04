a72e5849737217bf66986f922e3c17469f834ea0
Deepanshu954 <deepanshu95488@email.com>
2026-04-04 14:38:36 +0000

    ✅ 744. Network Delay Time | Accepted | sub:1968679382

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,27 +1,27 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        // Adj List
+        List<int[]>[] adj = new ArrayList[n + 1];
 
-        List<int[]>[] adj = new ArrayList[n+1];
-        for(int i = 0;i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
 
-        for(int[] time : times) {
-            int u = time[0];
-            int v = time[1];
-            int w = time[2];
+        for (int[] e : times) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
 
-            adj[u].add(new int[]{v, w});
+            adj[u].add(new int[] { v, w });
         }
-
-        // Dijkstra
 
         int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a,b) -> a[0] - b[0]
+        ); // { dist, node}
+
         pq.offer(new int[]{0, k});
 
         while(!pq.isEmpty()) {
@@ -31,25 +31,24 @@ class Solution {
 
             if(d > dist[u]) continue;
 
-            for(int[] edge : adj[u]) {
-                int v = edge[0];
-                int w = edge[1];
+            for(int[] e : adj[u]) {
+                int v = e[0];
+                int w = e[1];
 
-                if(d + w < dist[v]) {
-                    dist[v] = d + w;
-                    pq.offer(new int[]{dist[v], v});
+                if(dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.offer(new int[]{ dist[v], v});
                 }
             }
         }
 
-        int maxDist = 0;
+        int min = 0;
         for(int i = 1; i <= n; i++) {
             if(dist[i] == Integer.MAX_VALUE) return -1;
-
-            //maxDist = Math.max(maxDist, dist[i]);
-            if(maxDist < dist[i]) maxDist = dist[i];
+            min = Math.max(min, dist[i]);
         }
 
-        return maxDist;
+        return min;
+
     }
 }
