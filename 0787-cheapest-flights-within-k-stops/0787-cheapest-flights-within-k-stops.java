4b2858e0c6f94b1c0143606bddaf0db4e966da49
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-07 06:46:29 +0530

    Wrong Answer: Cheapest Flights Within K Stops

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -6,15 +6,11 @@ class Solution {
 
         for(int[] f : flights) adj[f[0]].add(new int[]{f[1], f[2]});
 
-        int[][] dist = new int[n][k+2];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        dist[src][0] = 0;
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) -> a[2] - b[2]
-        ); // {stops, node, dist}
+        Queue<int[]> pq = new ArrayDeque<>();
 
         pq.offer(new int[]{0, src, 0});
 
@@ -30,20 +26,14 @@ class Solution {
                 int v = e[0];
                 int w = e[1];
 
-                if(d + w < dist[v][s+1]) {
-                    dist[v][s+1] = d + w;
-                    pq.offer(new int[]{s+1, v, d + w});
+                if(dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.offer(new int[]{s+1, v, dist[v]});
                 }
             }
         }
 
-        int ans = Integer.MAX_VALUE;
-
-        for(int i = 0; i <= k + 1; i++) {
-            ans = Math.min(ans, dist[dst][i]);
-        }
-
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst]; 
 
     }
 }
