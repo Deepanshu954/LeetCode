926983868844fc597bfa81af9889ab436921bdf5
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-08 09:13:59 +0530

    Time: 5 ms (99.1%), Space: 48.6 MB (94.6%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int networkDelayTime(int[][] edges, int n, int k) {
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+
+        for(int i = 1; i < n; i++) {
+            boolean rel = false;
+            for(int[] e : edges) {
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
+
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    rel = true;
+                }
+            }
+
+            if(!rel) break;
+        }
+
+        int min = 0;
+        for(int i = 1; i <= n; i++) {
+            if(dist[i] == Integer.MAX_VALUE) return -1;
+            min = Math.max(min, dist[i]);
+        }
+
+        return min;
+    }
+}
