ecb78bfeefe11701e1489cf4462be8cdfb641c20
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-08 09:13:52 +0530

    Accepted: Network Delay Time

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,45 +1,23 @@
 class Solution {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        List<int[]>[] adj = new ArrayList[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        for (int[] e : times) {
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
-
-            adj[u].add(new int[] { v, w });
-        }
-
+    public int networkDelayTime(int[][] edges, int n, int k) {
         int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) -> a[0] - b[0]
-        ); // { dist, node}
+        for(int i = 1; i < n; i++) {
+            boolean rel = false;
+            for(int[] e : edges) {
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
 
-        pq.offer(new int[]{0, k});
-
-        while(!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int d = curr[0];
-            int u = curr[1];
-
-            if(d > dist[u]) continue;
-
-            for(int[] e : adj[u]) {
-                int v = e[0];
-                int w = e[1];
-
-                if(dist[u] + w < dist[v]) {
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) {
                     dist[v] = dist[u] + w;
-                    pq.offer(new int[]{ dist[v], v});
+                    rel = true;
                 }
             }
+
+            if(!rel) break;
         }
 
         int min = 0;
@@ -49,6 +27,5 @@ class Solution {
         }
 
         return min;
-
     }
 }
