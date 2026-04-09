7da9a22436e074f05b617c9cec40e802d9ff4f65
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-09 06:52:35 +0530

    Compile Error: XOR After Range Multiplication Queries II

--- 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.java
+++ 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.java
@@ -1,89 +1,104 @@
-class Solution {
-    static const int MOD = 1e9 + 7;
+typedef long long ll;
 
-    long long modpow(long long a, long long e) {
-        long long r = 1;
-        while (e) {
-            if (e & 1) r = r * a % MOD;
-            a = a * a % MOD;
+class Solution {
+public:
+    static constexpr long long MOD = (int)(1e9 + 7);
+
+    // fast modular exponentiation: a^e mod m
+    long long mod_pow(long long a, long long e, long long m = MOD) {
+        a %= m;
+        if (a < 0) a += m;
+        long long r = 1 % m;
+        while (e > 0) {
+            if (e & 1) r = (r * a) % m;
+            a = (a * a) % m;
             e >>= 1;
         }
         return r;
     }
 
-public:
-    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        int B = sqrt(n) + 1;
+    // modular inverse: a^{-1} mod m (m must be prime and a != 0 mod m)
+    long long mod_inv(long long a, long long m = MOD) {
+        a %= m;
+        if (a < 0) a += m;
+        return mod_pow(a, m - 2, m);
+    }
 
-        // events[k][rem] = list of (position, multiplier)
-        vector<vector<vector<pair<int,int>>>> events(B + 1);
-        for (int k = 1; k <= B; k++) {
-            events[k].resize(k);
-        }
+    bool same(vector<int>& q, int k, int lm)
+    {
+        return q[2] == k && ((q[0] % k) == lm); 
+    }
 
-        // Process queries
-        for (auto &q : queries) {
-            int l = q[0], r = q[1], k = q[2], v = q[3];
+    int xorAfterQueries(vector<int>& nums1, vector<vector<int>>& qs) {
+        sort(qs.begin(), qs.end(), [](const vector<int>& a, const vector<int>& b){
+            int ka = a[2];
+            int kb = b[2];
+            if (ka != kb) return ka < kb;
+            int la = a[0] % ka;
+            int lb = b[0] % kb;
+            return la < lb;
+        });
 
-            if (k > B) {
-                // direct update
-                for (int i = l; i <= r; i += k) {
-                    nums[i] = (long long)nums[i] * v % MOD;
+        int m = qs.size();
+        int i = 0;
+        int n = nums1.size();
+        vector<ll> nums(n);
+        for (int i = 0; i < n; i++) nums[i] = nums1[i];
+        vector<ll> dp(n, 1);
+        int nq = sqrt(n);
+
+        while (i < m)
+        {
+            auto& cur = qs[i];
+            int k = cur[2];
+            if (k > nq)
+            {
+                for (int j = cur[0]; j <= cur[1]; j += k)
+                {
+                    nums[j] = nums[j] * (ll)(cur[3]) % MOD;
                 }
-            } else {
-                int rem = l % k;
-                int start = (l - rem) / k;
-                int end   = (r - rem) / k;
-
-                events[k][rem].push_back({start, v});
-
-                // range end => use modular inverse
-                int maxT = (n - 1 - rem) / k;
-                if (end + 1 <= maxT) {
-                    int inv = modpow(v, MOD - 2);
-                    events[k][rem].push_back({end + 1, inv});
+                i++;
+                continue;
+            }
+            int lm = cur[0] % k;
+            int j = i + 1;
+           
+            while (j < m && same(qs[j], k, lm))
+            {
+                j++;
+            }
+            for (int x = i; x < j; x++)
+            {
+                auto& q = qs[x];
+                int l = q[0];
+                int r = q[1];
+                int v = q[3];
+                dp[l] = dp[l] * v % MOD;
+                int e = l + ((r - l)/k + 1) * k;
+                if (e < n)
+                {
+                    dp[e] = dp[e] * mod_inv(v) % MOD;
                 }
             }
-        }
+            ll pre = 1;
+            for (int x = lm; x < n; x += k)
+            {
+                dp[x] = dp[x] * pre % MOD;
+                pre = dp[x];
 
-        // Apply small k events
-        for (int k = 1; k <= B; k++) {
-            for (int rem = 0; rem < k; rem++) {
-                auto &ev = events[k][rem];
-                if (ev.empty()) continue;
-
-                // sort events
-                sort(ev.begin(), ev.end());
-
-                // compress same positions
-                vector<pair<int,int>> comp;
-                for (auto &p : ev) {
-                    if (!comp.empty() && comp.back().first == p.first) {
-                        comp.back().second = (long long)comp.back().second * p.second % MOD;
-                    } else {
-                        comp.push_back(p);
-                    }
-                }
-
-                // apply prefix multiplication
-                long long cur = 1;
-                int ptr = 0;
-
-                for (int t = 0, idx = rem; idx < n; t++, idx += k) {
-                    while (ptr < comp.size() && comp[ptr].first == t) {
-                        cur = cur * comp[ptr].second % MOD;
-                        ptr++;
-                    }
-                    nums[idx] = nums[idx] * cur % MOD;
-                }
+                nums[x] = nums[x] * dp[x] % MOD;
+                dp[x] = 1;
             }
+            i = j;
         }
 
-        // Compute XOR
-        int ans = 0;
-        for (int x : nums) ans ^= x;
+        ll cur = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            cur = cur ^ nums[i];
+        }
+        return cur;
 
-        return ans;
+        
     }
 };
