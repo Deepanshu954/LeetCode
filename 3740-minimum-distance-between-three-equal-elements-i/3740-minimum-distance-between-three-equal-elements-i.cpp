f4ce3f022b8349aa0279aed1e9c570ddaddd5ff4
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-10 05:58:02 +0530

    Accepted: Minimum Distance Between Three Equal Elements I

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        int n = nums.size();
-        int last2[100] = {};
-        int res = 101;
-
-        for (int i = 0; i < n; i++) {
-            int val = nums[i] - 1;
-            int pos = i + 1, pack = last2[val];
-            int old = pack & 255, cur = pack >> 8;
-
-            last2[val] = cur | (pos << 8);
-
-            if (old)
-                res = min(res, (pos - old) << 1);
+        if(nums.size() < 3) return -1;
+        int ans = INT_MAX;
+        for(int i = 0; i < nums.size() - 2; i++) {
+            for(int j = i + 1; j < nums.size() - 1; j++) {
+                if(nums[j] != nums[i]) continue;
+                for(int k = j + 1; k < nums.size(); k++) {
+                    if(nums[j] == nums[k]) {
+                        ans = min(ans , (j - i) + (k - j) + (k - i));
+                    }
+                }
+            }
         }
-
-        return -(res == 101) | res;
+        if(ans == INT_MAX) return -1;
+        return ans;
     }
 };
