17a8cfa512bb38505544a3baab09526ba91f342b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-11 23:54:01 +0530

    Accepted: Coloring A Border

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -1,23 +1,37 @@
 class Solution {
-    public int[][] colorBorder(int[][] grid, int r0, int c0, int color) {
-        if(grid == null || grid.length == 0)
-            return null;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        dfs(grid, visited, r0, c0, grid[r0][c0], color);
+    private int m, n;
+
+    public int[][] colorBorder(int[][] grid, int r, int c, int color) {
+        this.m = grid.length;
+        this.n = grid[0].length;
+
+        boolean[][] vis = new boolean[m][n];
+        dfs(grid, vis, r, c, grid[r][c], color);
         return grid;
     }
-    public void dfs(int[][] grid, boolean[][] visited, int i, int j, int oldColor, int newColor) {
-        if (i > grid.length - 1 || i < 0 || j > grid[0].length - 1 || j < 0 || grid[i][j] != oldColor || visited[i][j])
-            return;
-        visited[i][j] = true;
+
+    public void dfs(int[][] grid, boolean[][] vis, int r, int c, int oc, int nc) {
+        if (r < 0 || c < 0 || r >= m || c >= n || vis[r][c] || grid[r][c] != oc) return;
+
+        vis[r][c] = true;
         boolean border = false;
-        if(i == 0 || j == 0 || j == grid[0].length - 1 || i == grid.length - 1 || grid[i+1][j] != oldColor || grid[i-1][j] != oldColor || grid[i][j-1] != oldColor || grid[i][j+1] != oldColor)
+
+        // check boundary
+        if (r == 0 || c == 0 || r == m - 1 || c == n - 1) {
             border = true;
-        dfs(grid, visited, i+1, j, oldColor, newColor);
-        dfs(grid, visited, i-1, j, oldColor, newColor);
-        dfs(grid, visited, i, j+1, oldColor, newColor);
-        dfs(grid, visited, i, j-1, oldColor, newColor);
-        if(border) 
-            grid[i][j] = newColor;            
+        }
+
+        // check neighbors
+        if (r > 0 && grid[r - 1][c] != oc) border = true;
+        if (r < m - 1 && grid[r + 1][c] != oc) border = true;
+        if (c > 0 && grid[r][c - 1] != oc) border = true;
+        if (c < n - 1 && grid[r][c + 1] != oc) border = true;
+
+        dfs(grid, vis, r + 1, c, oc, nc);
+        dfs(grid, vis, r - 1, c, oc, nc);
+        dfs(grid, vis, r, c + 1, oc, nc);
+        dfs(grid, vis, r, c - 1, oc, nc);
+
+        if (border) grid[r][c] = nc;
     }
 }
