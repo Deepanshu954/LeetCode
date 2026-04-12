d41bd1e4418660091283f1b7dec08944585185d3
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-12 21:30:05 +0530

    Accepted: Pacific Atlantic Water Flow

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,6 +1,6 @@
 class Solution {
-    private int m,n;
-    private int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+    private int m, n;
+    private int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
     public List<List<Integer>> pacificAtlantic(int[][] grid) {
         m = grid.length;
@@ -9,36 +9,41 @@ class Solution {
         boolean[][] pac = new boolean[m][n];
         boolean[][] atl = new boolean[m][n];
 
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             dfs(grid, i, 0, pac);
-            dfs(grid, i, n-1, atl);
+            dfs(grid, i, n - 1, atl);
         }
 
-        for(int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             dfs(grid, 0, j, pac);
-            dfs(grid, m-1, j, atl);
+            dfs(grid, m - 1, j, atl);
         }
 
         List<List<Integer>> res = new ArrayList<>();
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(pac[i][j] && atl[i][j]) res.add(Arrays.asList(i,j));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pac[i][j] && atl[i][j])
+                    res.add(Arrays.asList(i, j));
             }
         }
+
         return res;
     }
 
     private void dfs(int[][] grid, int r, int c, boolean[][] vis) {
         vis[r][c] = true;
 
-        for(int[] d : dir) {
+        for (int[] d : dir) {
             int nr = r + d[0];
             int nc = c + d[1];
 
-            if(nr >= 0 && nc >= 0 && nr < m && nc < n && !vis[nr][nc] && grid[nr][nc] >= grid[r][c]) {
-                dfs(grid, nr, nc, vis);
-            }
+            if (nr < 0 || nr >= m || nc < 0 || nc >= n || vis[nr][nc])
+                continue;
+            if (grid[nr][nc] < grid[r][c])
+                continue;
+
+            dfs(grid, nr, nc, vis);
         }
     }
 }
