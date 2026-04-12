3fab846589d0d4be5e60eb4752410f0adad4a76c
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-12 19:46:02 +0530

    Time: 3 ms (86.1%), Space: 52.3 MB (51.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,60 +1,35 @@
 class Solution {
+    private int m, n;
+
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        m = grid.length;
+        n = grid[0].length;
 
-        int res = 0;
+        int comp = 0;
+        boolean[][] vis = new boolean[m][n];
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == '1') {
-                    res++;
-                    bfs(grid,i,j);
-                } 
-            }
-        }
-
-        return res;
-    }
-
-    private void bfs(char[][] grid, int sr, int sc) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{sr,sc});
-        grid[sr][sc] = '0';
-
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            int r = curr[0];
-            int c = curr[1];
-
-            for(int[] d : dir) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1') {
-                    grid[nr][nc] = '0';
-                    q.offer(new int[]{nr,nc});
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    dfs(grid, i, j, vis);
+                    comp++;
                 }
             }
         }
+
+        return comp;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
-        int m = grid.length;
-        int n = grid[0].length;
+    private void dfs(char[][] grid, int r, int c, boolean[][] vis) {
+        if (r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == '0' || vis[r][c])
+            return;
 
-        if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == '0') return;
-
+        vis[r][c] = true;
         grid[r][c] = '0';
 
-        dfs(grid, r-1, c);
-        dfs(grid, r+1, c);
-        dfs(grid, r, c-1);
-        dfs(grid, r, c+1);
+        dfs(grid, r - 1, c, vis);
+        dfs(grid, r + 1, c, vis);
+        dfs(grid, r, c - 1, vis);
+        dfs(grid, r, c + 1, vis);
     }
 }
