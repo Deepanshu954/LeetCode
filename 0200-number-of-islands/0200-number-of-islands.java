c0326ef94d989c17b5749955989d5a1164fc930e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-12 19:45:49 +0530

    Accepted: Number of Islands

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,35 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int comp = 0;
+    private int m, n;
 
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == '1') {
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+
+        int comp = 0;
+        boolean[][] vis = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    dfs(grid, i, j, vis);
                     comp++;
-                    dfs(grid, i, j);
                 }
             }
         }
 
-
         return comp;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
-        int m = grid.length;
-        int n = grid[0].length;
+    private void dfs(char[][] grid, int r, int c, boolean[][] vis) {
+        if (r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == '0' || vis[r][c])
+            return;
 
-        if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == '0') return;
-
+        vis[r][c] = true;
         grid[r][c] = '0';
 
-        dfs(grid, r+1, c);
-        dfs(grid, r-1, c);
-        dfs(grid, r, c+1);
-        dfs(grid, r, c-1);
+        dfs(grid, r - 1, c, vis);
+        dfs(grid, r + 1, c, vis);
+        dfs(grid, r, c - 1, vis);
+        dfs(grid, r, c + 1, vis);
     }
 }
