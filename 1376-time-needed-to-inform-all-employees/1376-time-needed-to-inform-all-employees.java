a58a99c7a55c8db11e9493c4a26e6462e012f8ca
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-16 13:33:25 +0530

    Accepted: Time Needed to Inform All Employees

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+    public int numOfMinutes(int n, int node, int[] manager, int[] info) {
         List<Integer>[] adj = new ArrayList[n];
         for(int i = 0; i < n; i++) adj[i] = new ArrayList<>();
 
-        // build tree
         for(int i = 0; i < n; i++) {
-            if(manager[i] != -1) {
-                adj[manager[i]].add(i);
-            }
+            if(manager[i] != -1) adj[manager[i]].add(i);
         }
 
-        return dfs(headID, adj, informTime);
+        return dfs(node, adj, info);
     }
 
-    private int dfs(int node, List<Integer>[] adj, int[] informTime) {
+    private int dfs(int node, List<Integer>[] adj, int[] info) {
         int max = 0;
 
         for(int child : adj[node]) {
-            max = Math.max(max, dfs(child, adj, informTime));
+            max = Math.max(max, dfs(child, adj, info));
         }
 
-        return informTime[node] + max;
+        return info[node] + max;
     }
 }
