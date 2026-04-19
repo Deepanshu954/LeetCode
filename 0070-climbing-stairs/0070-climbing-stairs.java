743b9bbe017bb06e1fb0553a2edb938492c2af9c
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-19 22:01:57 +0530

    Time: 0 ms (100%), Space: 42.1 MB (39.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,52 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        int[] arr = new int[n+2];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i = 2; i <= n + 1; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
         }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+
+        return arr[n+1];
     }
 }
+
+/*
+Brute Force: 
+Allowed steps are +1 or +2 
+so i can use bottom up approach where i come down from up
+so here i can create a formula like c(n-1) + c(n-2) this will give me answer 
+
+Code:
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n < 2) return 1;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+}
+
+
+
+Here i notice that i am calculating for same thing again and again so for that i can simply use a array for stroing things
+
+Code: 
+class Solution {
+
+    private int[] arr;
+    public int climbStairs(int n) {
+        arr = new int[n+1];
+        Arrays.fill(arr, -5);
+
+        return helper(n);
+    }
+
+    private int helper(int n) {
+        if(n < 2) return 1;
+        if(arr[n] != -5) return arr[n];
+        return helper(n-1) + helper(n-2);
+    }
+}
+
+*/
