51bdd3cb052f868c9ce5e5cd56a51807395710ba
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-21 20:02:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,14 +5,16 @@ class Solution {
 
         int[] dp = new int[n];
 
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int a = nums[0];
+        int b = Math.max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++) {
-            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+            int c = Math.max(nums[i] + a, b);
+            a = b;
+            b = c;
         }
 
-        return Math.max(dp[n-1], dp[n-2]);
+        return Math.max(a, b);
     }
 }
 
@@ -42,4 +44,25 @@ class Solution {
     }
 }
 
+*/
+
+/*
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        }
+
+        return Math.max(dp[n-1], dp[n-2]);
+    }
+}
 */
