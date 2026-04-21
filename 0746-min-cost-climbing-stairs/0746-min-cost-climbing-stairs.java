a88bcf56f01fc84a2fb51305ef966fb440af83f4
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-21 19:36:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,4 +1,24 @@
 class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        int a = cost[0];
+        int b = cost[1];
+
+        for(int i = 2; i < n; i++) {
+            int c = cost[i] + Math.min(a, b);
+
+            a = b;
+            b = c;
+        }
+
+        return Math.min(a,b);
+    }
+}
+
+/*
+
+class Solution {
     private int[] dp;
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
@@ -20,3 +40,25 @@ class Solution {
         return dp[n];
     }
 }
+
+*/
+
+/*
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+}
+
+*/
