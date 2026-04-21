9ae1c5eab3302b644dabe9e3022c3f041f9c6434
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-21 19:53:55 +0530

    Wrong Answer: House Robber

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,15 +2,16 @@ class Solution {
     private int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        dp = new int[n];
+        if(n == 1) return nums[0];
+        
+        dp = new int[n+1];
         Arrays.fill(dp, -1);
 
         return Math.max(rec(n-1, nums), rec(n-2, nums));
     }
 
     private int rec(int i, int[] nums) {
-        if(i == 0) return nums[0];
-        if(i == 1) return nums[1];
+        if(i < 2) return nums[i];
 
         if(dp[i] != -1) return dp[i];
 
