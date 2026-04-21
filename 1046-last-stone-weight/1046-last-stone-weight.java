1353df9c65f8557d4c96552539c4c357f03dfe5a
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-21 12:36:53 +0530

    Accepted: Last Stone Weight

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,8 +1,18 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a,b) -> b - a
+        );
 
         for(int s : stones) pq.offer(s);
+
+        while(pq.size() > 1) {
+            int s1 = pq.poll();
+            int s2 = pq.poll();
+
+            int abs = Math.abs(s1 - s2);
+            pq.offer(abs);
+        }
 
         return pq.poll();
     }
