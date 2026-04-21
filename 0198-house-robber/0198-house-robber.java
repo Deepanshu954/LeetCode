3e53a3ed37ca5e6308c9fc0314c5fa11504f6dc8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-21 19:50:17 +0530

    Runtime Error: House Robber

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,9 @@
 class Solution {
+    private int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
 
         return Math.max(rec(n-1, nums), rec(n-2, nums));
     }
@@ -9,6 +12,10 @@ class Solution {
         if(i == 0) return nums[0];
         if(i == 1) return nums[1];
 
-        return  Math.max(nums[i] + rec(i-2, nums), rec(i-1, nums));
+        if(dp[i] != -1) return dp[i];
+
+        dp[i] = Math.max(nums[i] + rec(i-2, nums), rec(i-1, nums));
+
+        return dp[i];
     }
 }
