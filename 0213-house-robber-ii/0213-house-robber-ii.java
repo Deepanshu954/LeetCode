64e4a9daa107df9a1a0833b10226630044786dfe
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-22 20:31:23 +0530

    Runtime Error: House Robber II

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,10 +1,16 @@
 class Solution {
+    private int[] dp;
     private int s;
     public int rob(int[] nums) {
         int n = nums.length;
 
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+
         s = 0;
         int res1 = Math.max(rec(n-2, nums), rec(n-3, nums));
+
+        Arrays.fill(dp, -1);
         s = 1;
         int res2 = Math.max(rec(n-1, nums), rec(n-2, nums));
 
@@ -15,6 +21,8 @@ class Solution {
         if(i == s) return nums[s];
         if(i == s+1) return nums[s+1];
 
-        return  Math.max(nums[i] + rec(i-2, nums), rec(i-1, nums));
+        if(dp[i] != -1) return dp[i];
+        dp[i] = Math.max(nums[i] + rec(i-2, nums), rec(i-1, nums));
+        return dp[i];
     }
 }
