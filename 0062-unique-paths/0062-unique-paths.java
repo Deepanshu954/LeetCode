0d281071fcfb90efebd6ac0da0d2481bc8fd9f58
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-25 14:28:00 +0530

    Time: 0 ms (100%), Space: 42 MB (82.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,42 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m +1][n +1];
-        return helper(m,n, dp);
-    }
+        int[][] dp = new int[m][n];
 
-    private int helper(int m, int n, int[][] dp) {
-        if(m == 1 || n == 1) return 1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || j == 0) dp[i][j] = 1;
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
 
-        if(dp[m][n] != 0) return dp[m][n];
-
-        dp[m][n] = helper(m - 1, n, dp) + helper(m, n - 1, dp);
-
-        return dp[m][n];
+        return dp[m-1][n-1];
     }
 }
+
+/*
+
+class Solution {
+    private int[][] dp;
+    public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        for(int[] d : dp) Arrays.fill(d, -1);
+        return helper(m-1,n-1);
+    }
+
+    private int helper(int m, int n) {
+        if(m == 0 || n == 0) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int left = helper(m - 1, n);
+        int right = helper(m, n - 1);
+
+        return dp[m][n] = left + right;
+    }
+}
+
+*/
