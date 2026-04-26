48e6e721b0f217f23354c65008a51e3f81c0d821
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 11:28:17 +0530

    Wrong Answer: Unique Paths II

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,25 +1,17 @@
 class Solution {
-    private int[][] dp;
+    private int m,n;
     public int uniquePathsWithObstacles(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        m = grid.length;
+        n = grid[0].length;
 
-        dp = new int[m][n];
-        for(int[] d : dp) Arrays.fill(d, -1);
-
-        return helper(m-1,n-1, grid);
+        return rec(m-1, n-1,grid);
     }
 
-    private int helper(int m, int n, int[][] grid) {
+    private int rec(int m, int n, int[][] grid) {
+        if(m == 0 && n == 0) return 1;
         if(m < 0 || n < 0) return 0;
         if(grid[m][n] == 1) return 0;
-        if(m == 0 && n == 0) return 1;
-        
-        if(dp[m][n] != -1) return dp[m][n];
 
-        int left = helper(m - 1, n, grid);
-        int right = helper(m, n - 1, grid);
-
-        return dp[m][n] = left + right;
+        return rec(m-1, n, grid) + rec(m, n-1, grid);
     }
 }
