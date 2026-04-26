548238bc3f717a60e4ccd7c629f7ecc9bcfa1a1c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 12:00:47 +0530

    Accepted: Minimum Path Sum

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,7 +1,11 @@
 class Solution {
+    private int[][] dp;
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
+        dp = new int[m][n];
+        for(int[] d : dp) Arrays.fill(d, -1);
 
         return rec(m-1, n-1, grid);
     }
@@ -9,7 +13,9 @@ class Solution {
     private int rec(int r, int c, int[][] grid) {
         if(r < 0 || c < 0) return (int)1e9;
         if(r == 0 && c == 0) return grid[r][c];
+        if(dp[r][c] != -1) return dp[r][c];
 
-        return grid[r][c] + Math.min(rec(r-1, c, grid), rec(r, c-1, grid));
+        dp[r][c] = grid[r][c] + Math.min(rec(r-1, c, grid), rec(r, c-1, grid));
+        return dp[r][c];
     }
 }
