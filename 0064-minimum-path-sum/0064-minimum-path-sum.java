27fae3e7c236fe779779a3a2da9e64341a2d4ec6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 12:10:24 +0530

    Wrong Answer: Minimum Path Sum

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,4 +1,37 @@
 class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        // Base Case
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++) {
+            dp[i][0] = grid[i-1][0] + grid[i][0];
+        }
+
+        for(int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+        // recursive part
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
+
+/*
+
+// memorization
+
+class Solution {
     private int[][] dp;
     public int minPathSum(int[][] grid) {
         int m = grid.length;
@@ -19,3 +52,5 @@ class Solution {
         return dp[r][c];
     }
 }
+
+*/
