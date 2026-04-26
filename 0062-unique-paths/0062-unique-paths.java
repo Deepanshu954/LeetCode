fcafbc46a62ad2a1608c116eb3b90f9c29d42d35
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 11:19:45 +0530

    Accepted: Unique Paths

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,3 +1,51 @@
+// space optimization
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+
+        // base case
+        Arrays.fill(dp , 1);
+
+        // recursive part
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j-1];
+            }
+        }
+        
+        return dp[n-1];
+    }
+}
+
+/*
+
+// memorization
+
+class Solution {
+    private int[][] dp;
+    public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        for(int[] d : dp) Arrays.fill(d, -1);
+        
+        return rec(m-1,n-1);
+    }
+
+    private int rec(int m, int n) {
+        if( m == 0 || n == 0) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+
+        dp[m][n] = rec(m-1, n) + rec(m, n-1);
+        return dp[m][n];
+    }
+}
+
+*/
+
+/*
+
+// tabular
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
@@ -17,26 +65,6 @@ class Solution {
         }
         
         return dp[m-1][n-1];
-    }
-}
-
-/*
-
-class Solution {
-    private int[][] dp;
-    public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        for(int[] d : dp) Arrays.fill(d, -1);
-        
-        return rec(m-1,n-1);
-    }
-
-    private int rec(int m, int n) {
-        if( m == 0 || n == 0) return 1;
-        if(dp[m][n] != -1) return dp[m][n];
-
-        dp[m][n] = rec(m-1, n) + rec(m, n-1);
-        return dp[m][n];
     }
 }
 
