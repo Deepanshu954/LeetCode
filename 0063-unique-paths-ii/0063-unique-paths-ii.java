fb2153dc2e51a3bbdbb9adbd5bb295ea42c26129
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 11:29:37 +0530

    Wrong Answer: Unique Paths II

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,13 +3,15 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
+        if(m == 1 && n == 1) return 0;
+
 
         return rec(m-1, n-1,grid);
     }
 
     private int rec(int m, int n, int[][] grid) {
-        if(m == 0 && n == 0) return 1;
         if(m < 0 || n < 0) return 0;
+        if(m == 0 && n == 0) return 1;
         if(grid[m][n] == 1) return 0;
 
         return rec(m-1, n, grid) + rec(m, n-1, grid);
