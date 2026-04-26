d151a07682149e21a39f49cc34f1f3747aa75421
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-26 11:51:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (81.43%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -9,20 +9,13 @@ class Solution {
         if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return 0;
 
         int[] dp = new int[n];
+        dp[0] = (grid[0][0] == 1) ? 0 : 1;
 
-        // Base Case
-        dp[0] = 1;
-        for(int j = 1; j < n; j++) {
-            if(grid[0][j] == 1) dp[j] = 0;
-            else dp[j] = dp[j-1];
-        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
 
-        // recursive part
-
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
                 if(grid[i][j] == 1) dp[j] = 0;
-                else {
+                else if(j > 0){
                     dp[j] = dp[j] + dp[j-1];
                 }
             }
