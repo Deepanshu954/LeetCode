27d0e62a86892a5bf6d42f6a056ad4edb43b9404
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-04-26 15:16:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,4 +1,42 @@
 class Solution {
+    public int minimumTotal(List<List<Integer>> tri) {
+        int n = tri.size();
+        int[][] dp = new int[n][n];
+
+        // Base Case
+        dp[0][0] = tri.get(0).get(0);
+
+        // recursive part
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                int val = tri.get(i).get(j);
+
+                if(j == 0) {
+                    dp[i][j] = val + dp[i-1][j];
+                } else if(j == i) {
+                    dp[i][j] = val + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = val + Math.min(dp[i-1][j], dp[i-1][j-1]);
+                }
+            }
+        }
+
+        int res = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            res = Math.min(res, dp[n-1][i]);
+        }
+
+        return res;
+    }
+}
+
+
+/*
+
+// Memorization
+
+class Solution {
     private int[][] dp;
     public int minimumTotal(List<List<Integer>> tri) {
         int n = tri.size();
@@ -22,3 +60,5 @@ class Solution {
         return dp[r][c];
     }
 }
+
+*/
