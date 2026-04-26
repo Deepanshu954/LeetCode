0d1296a8e642ee1bb64e464b0e8bc26acc1113d1
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 14:58:51 +0530

    Accepted: Triangle

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,6 +1,9 @@
 class Solution {
+    private int[][] dp;
     public int minimumTotal(List<List<Integer>> tri) {
         int n = tri.size();
+        dp = new int[n][n];
+        for(int[] d : dp) Arrays.fill(d, -1);
 
         int minPath = Integer.MAX_VALUE;
 
@@ -13,7 +16,9 @@ class Solution {
     private int rec(int r, int c, List<List<Integer>> tri) {
         if(r == 0) return tri.get(0).get(0);
         if(c < 0 || c > r) return (int)1e9;
+        if(dp[r][c] != -1) return dp[r][c];
 
-        return tri.get(r).get(c) + Math.min(rec(r-1, c, tri), rec(r-1, c-1, tri));
+        dp[r][c] = tri.get(r).get(c) + Math.min(rec(r-1, c, tri), rec(r-1, c-1, tri));
+        return dp[r][c];
     }
 }
