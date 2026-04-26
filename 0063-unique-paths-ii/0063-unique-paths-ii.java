b6cf1061b0bb102d2b85df89c22bef6e3ff88886
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-26 11:32:14 +0530

    Accepted: Unique Paths II

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,10 +1,14 @@
 class Solution {
     private int m,n;
+    private int[][] dp;
     public int uniquePathsWithObstacles(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
         if(grid[0][0] == 1) return 0;
 
+        dp = new int[m][n];
+        for(int[] d : dp) Arrays.fill(d, -1);
+ 
 
         return rec(m-1, n-1,grid);
     }
@@ -13,7 +17,9 @@ class Solution {
         if(m < 0 || n < 0) return 0;
         if(m == 0 && n == 0) return 1;
         if(grid[m][n] == 1) return 0;
+        if(dp[m][n] != -1) return dp[m][n];
 
-        return rec(m-1, n, grid) + rec(m, n-1, grid);
+        dp[m][n] = rec(m-1, n, grid) + rec(m, n-1, grid);
+        return dp[m][n];
     }
 }
