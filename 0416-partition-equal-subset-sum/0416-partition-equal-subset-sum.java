586135e7c00de2921b6e677f132e0d090aa1c492
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-04-27 23:50:24 +0530

    Accepted: Partition Equal Subset Sum

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,21 +1,67 @@
+// memorization
+
 class Solution {
+    private int[][] dp;
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
+
         int sum = 0;
         for(int x : nums) sum += x;
         if(sum % 2 == 1) return false;
 
-        int t = sum / 2;
+        int k = sum/2;
+        dp = new int[n][k+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
 
-        return rec(nums.length - 1, t, nums);
+        return rec(n - 1, k, nums);
     }
 
-    private boolean rec(int i, int target, int[] nums) {
-        if(target == 0) return true;
-        if(target < 0 || i == 0) return false;
+    private boolean rec(int i, int k, int[] nums) {
+        if(k == 0) return true;
+        if(i == 0) return nums[0] == k;
 
-        boolean take = rec(i-1, target - nums[i], nums);
-        boolean nottake = rec(i-1, target, nums);
+        if(dp[i][k] != -1) return dp[i][k] == 1;
 
-        return take || nottake;
+        // don't take
+        boolean notTake = rec(i -1, k, nums);
+
+        // take
+        boolean take = false;
+        if(nums[i] <= k) 
+            take = rec(i-1, k - nums[i], nums);
+        
+        dp[i][k] = (notTake || take) ? 1 : 0;
+        return dp[i][k] == 1;
     }
 }
+
+/*
+
+// Recursion
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int x : nums) sum += x;
+
+        if(sum % 2 == 1) return false;
+        return rec(nums.length - 1, sum/2, nums);
+    }
+
+    private boolean rec(int i, int k, int[] nums) {
+        if(k == 0) return true;
+        if(i == 0) return nums[0] == k;
+
+        // don't take
+        boolean notTake = rec(i -1, k, nums);
+
+        // take
+        boolean take = false;
+        if(nums[i] <= k) 
+            take = rec(i-1, k - nums[i], nums);
+        
+        return notTake || take;
+    }
+}
+
+*/
