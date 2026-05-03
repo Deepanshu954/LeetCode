ea4d55024d9addd6700c17ab0e76b7461d59e372
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-03 20:52:39 +0530

    Accepted: Coin Change II

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,29 +2,30 @@ class Solution {
     private int[][] dp;
     public int change(int k, int[] nums) {
         int n = nums.length;
-
         dp = new int[n][k+1];
-        for(int[] d : dp) Arrays.fill(d, -1);
 
-        return rec(n-1, k, nums);
-    }
-
-    private int rec(int i, int k, int[] nums) {
-        if (i == 0) {
-            return (k % nums[0] == 0) ? 1 : 0;
+        // Base Case
+        for(int j = 0; j <= k; j++) {
+            if(j % nums[0] == 0) {
+                dp[0][j] = 1;
+            } 
         }
 
+        // Rest
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= k; j++) {
+                int notTake = dp[i-1][j];
 
-        if(dp[i][k] != -1) return dp[i][k];
+                int take = 0;
+                if(nums[i] <= j) {
+                    take = dp[i][j - nums[i]];
+                }
 
-        int notTake = rec(i - 1, k, nums);
-
-        int take = 0;
-        if (nums[i] <= k) {
-            take = rec(i , k - nums[i], nums);
+                dp[i][j] = take + notTake;
+            }
         }
 
-        return dp[i][k] = take + notTake;
+        return dp[n-1][k];
     }
 }
 
@@ -52,6 +53,42 @@ class Solution {
         }
 
         return take + notTake;
+    }
+}
+
+*/
+
+/*
+
+// Memorization
+
+class Solution {
+    private int[][] dp;
+    public int change(int k, int[] nums) {
+        int n = nums.length;
+
+        dp = new int[n][k+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
+
+        return rec(n-1, k, nums);
+    }
+
+    private int rec(int i, int k, int[] nums) {
+        if (i == 0) {
+            return (k % nums[0] == 0) ? 1 : 0;
+        }
+
+
+        if(dp[i][k] != -1) return dp[i][k];
+
+        int notTake = rec(i - 1, k, nums);
+
+        int take = 0;
+        if (nums[i] <= k) {
+            take = rec(i , k - nums[i], nums);
+        }
+
+        return dp[i][k] = take + notTake;
     }
 }
 
