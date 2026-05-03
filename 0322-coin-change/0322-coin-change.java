4b7bb18647eff989075dc1d2a2f6f409e895f661
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-03 12:24:40 +0530

    Time Limit Exceeded: Coin Change

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,10 +1,14 @@
 class Solution {
     private int res;
+    private int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
         Arrays.sort(coins);
         res = (int)1e9;
+
+        dp = new int[n][amount+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
 
         rec(n-1,  amount, 0 ,coins);
         return (res == (int)1e9) ? -1 : res;
@@ -13,6 +17,12 @@ class Solution {
     private void rec(int i, int k, int s,int[] nums) {
         if(i < 0 || k < 0) return;
         if(k == 0) {
+            res = Math.min(res, s);
+            dp[i][k] = res;
+            return;
+        }
+
+        if(dp[i][k] != -1) {
             res = Math.min(res, s);
             return;
         }
