68ad6d2e8499717ae170691e15ec8ed3f8251f67
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-03 20:44:00 +0530

    Accepted: Coin Change II

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,4 +1,38 @@
 class Solution {
+    private int[][] dp;
+    public int change(int k, int[] nums) {
+        int n = nums.length;
+
+        dp = new int[n][k+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
+
+        return rec(n-1, k, nums);
+    }
+
+    private int rec(int i, int k, int[] nums) {
+        if (i == 0) {
+            return (k % nums[0] == 0) ? 1 : 0;
+        }
+
+
+        if(dp[i][k] != -1) return dp[i][k];
+
+        int notTake = rec(i - 1, k, nums);
+
+        int take = 0;
+        if (nums[i] <= k) {
+            take = rec(i , k - nums[i], nums);
+        }
+
+        return dp[i][k] = take + notTake;
+    }
+}
+
+/* 
+
+// Recursion
+
+class Solution {
     public int change(int k, int[] nums) {
         int n = nums.length;
 
@@ -20,3 +54,6 @@ class Solution {
         return take + notTake;
     }
 }
+
+*/
+
