916f20054588fdc2b6e95302d2555046a56ad425
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-03 20:38:53 +0530

    Time Limit Exceeded: Coin Change II

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,22 @@
 class Solution {
     public int change(int k, int[] nums) {
         int n = nums.length;
-        if(nums[0] == k && n == 1) return 1;
 
         return rec(n-1, k, nums);
     }
 
-    private int rec(int i, int k, int[] nums) {
-        if(i == 0) {
-            return (k == 0) ? 1 : 0;
+    private static int rec(int i, int k, int[] nums) {
+        if (i == 0) {
+            return (k % nums[0] == 0) ? 1 : 0;
         }
 
-        int notTake = rec(i-1, k, nums);
+        int notTake = rec(i - 1, k, nums);
 
         int take = 0;
-        if(nums[i] <= k) {
-            take = 1 + rec(i, k - nums[i], nums);
+        if (nums[i] <= k) {
+            take = rec(i , k - nums[i], nums);
         }
 
-        return notTake + take;
+        return take + notTake;
     }
 }
