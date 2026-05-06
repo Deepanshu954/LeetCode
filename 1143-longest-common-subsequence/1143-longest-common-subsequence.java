c2f4015cde6c9ef43f2c727cb97dfc2429e22f20
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-06 10:56:09 +0530

    Time: 100 ms (5.28%), Space: 53.8 MB (79.95%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,55 @@
+// Memorization
+
+class Solution {
+    private int[][] dp;
+    public int longestCommonSubsequence(String text1, String text2) {
+        int s1 = text1.length();
+        int s2 = text2.length();
+
+        dp = new int[s1+1][s2+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
+
+        return rec(s1 - 1, s2 - 1, text1, text2);
+    }
+    
+    private int rec(int s1, int s2, String text1, String text2) {
+        if(s1 < 0 || s2 < 0) return 0;
+
+        if(dp[s1][s2] != -1) return dp[s1][s2];
+
+        if(text1.charAt(s1) == text2.charAt(s2)) {
+            return dp[s1][s2] = 1 + rec(s1 - 1, s2 - 1, text1, text2);
+        }
+
+        return dp[s1][s2] = Math.max(
+            rec(s1, s2 - 1, text1, text2),
+            rec(s1 - 1, s2, text1, text2)
+        );
+    }
+}
+
+/*
+
+// Recursion
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        return rec(text1.length()-1, text2.length()-1, text1, text2);
+    }
+    
+    private int rec(int s1, int s2, String text1, String text2) {
+        if(s1 < 0 || s2 < 0) return 0;
+
+        if(text1.charAt(s1) == text2.charAt(s2)) {
+            return 1 + rec(s1 - 1, s2 - 1, text1, text2);
+        }
+
+        return Math.max(
+            rec(s1, s2 - 1, text1, text2),
+            rec(s1 - 1, s2, text1, text2)
+        );
+    }
+}
+
+*/
+
