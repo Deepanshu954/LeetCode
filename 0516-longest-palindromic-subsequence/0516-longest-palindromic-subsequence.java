9e6901207d8aab852d052738ffe6f6fb50604376
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-07 19:30:37 +0530

    Wrong Answer: Longest Palindromic Subsequence

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        return rec(0, s.length() - 1, s);
+    }
+
+    private int rec(int i, int j, String s) {
+        if(i > j) return 0;
+
+        if(s.charAt(i) == s.charAt(j)) {
+            return 2 + rec(i+1, j-1, s);
+        }
+
+        return Math.max(
+            rec(i+1, j, s),
+            rec(i, j-1, s)
+        );
+    }
+}
