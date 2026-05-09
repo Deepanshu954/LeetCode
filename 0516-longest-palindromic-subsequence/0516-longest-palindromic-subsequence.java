a4b71370c1d46beaaead9671ed3a2ac6357dd672
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-09 23:43:45 +0530

    Accepted: Longest Palindromic Subsequence

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,3 +1,56 @@
+// Tabular
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+
+        int[][] dp = new int[n][n];
+
+        for(int i = 0; i < n; i++) dp[i][i] = 1;
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i+1; j < n; j++) {
+
+                if(s.charAt(i) == s.charAt(j)) dp[i][j] = 2 + dp[i+1][j-1];
+                else dp[i][j] = Math.max(
+                    dp[i+1][j],
+                    dp[i][j-1]
+                );
+            }
+        }
+
+        return dp[0][n-1];
+    }
+}
+
+/*
+
+// Recursion
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        return rec(0, s.length() - 1, s);
+    }
+
+    private int rec(int i, int j, String s) {
+        if(i > j) return 0;
+        if(i == j) return 1;
+
+        if(s.charAt(i) == s.charAt(j)) {
+            return 2 + rec(i+1, j-1, s);
+        }
+
+        return Math.max(
+            rec(i+1, j, s),
+            rec(i, j-1, s)
+        );
+    }
+}
+
+*/
+
+/*
+
 // Memorization
 
 class Solution {
@@ -22,30 +75,6 @@ class Solution {
         }
 
         return dp[i][j] = Math.max(
-            rec(i+1, j, s),
-            rec(i, j-1, s)
-        );
-    }
-}
-
-/*
-
-// Recursion
-
-class Solution {
-    public int longestPalindromeSubseq(String s) {
-        return rec(0, s.length() - 1, s);
-    }
-
-    private int rec(int i, int j, String s) {
-        if(i > j) return 0;
-        if(i == j) return 1;
-
-        if(s.charAt(i) == s.charAt(j)) {
-            return 2 + rec(i+1, j-1, s);
-        }
-
-        return Math.max(
             rec(i+1, j, s),
             rec(i, j-1, s)
         );
