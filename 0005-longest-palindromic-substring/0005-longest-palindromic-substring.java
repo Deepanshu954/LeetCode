e0378569262bc4dbbf112c3a0705964dcb26134e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-10 10:29:44 +0530

    Wrong Answer: Longest Palindromic Substring

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,7 +1,6 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-
         boolean[][] dp = new boolean[n][n];
 
         int start = 0;
@@ -18,7 +17,7 @@ class Solution {
                         dp[i][j] = true;
 
                         if(j - i + 1 > maxLen) {
-                            maxLen = j-1+1;
+                            maxLen = j-i+1;
                             start = i;
                         }
                     }
