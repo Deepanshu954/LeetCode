a895b01193634dc0f9e7b25dd7c762d018c2cb07
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-10 16:48:49 +0530

    Accepted: Distinct Subsequences

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,3 +1,30 @@
+// Space Optimization
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = n; j > 0 ; j--) {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[j] = dp[j] + dp[j-1];
+            }
+        }
+
+        return dp[n];
+    }
+}
+
+
+/*
+
+// Tabular
+
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length();
@@ -20,3 +47,5 @@ class Solution {
         return dp[m][n];
     }
 }
+
+*/
