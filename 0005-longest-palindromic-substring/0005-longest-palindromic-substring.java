43ae071f8c66b43535d6e7a69a4326911921bb4b
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-10 10:30:40 +0530

    Time: 100 ms (28.34%), Space: 48.3 MB (7.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,30 @@
 class Solution {
-    private int start = 0;
-    private int maxLength = 0;
-
     public String longestPalindrome(String s) {
         int n = s.length();
-        if (n < 2) return s;
+        boolean[][] dp = new boolean[n][n];
 
-        for (int i = 0; i < n; i++) {
-            // Case 1: Odd length (Center is s[i])
-            expandAroundCenter(s, i, i);
+        int start = 0;
+        int maxLen = 1;
 
-            // Case 2: Even length (Center is s[i] and s[i+1])
-            expandAroundCenter(s, i, i + 1);
+        for(int i = 0; i < n; i++) dp[i][i] = true;
+
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i+1; j < n; j++) {
+
+                if(s.charAt(i) == s.charAt(j)) {
+                    if(j-i == 1 || dp[i+1][j-1]) {
+                        dp[i][j] = true;
+
+                        if(j - i + 1 > maxLen) {
+                            maxLen = j-i+1;
+                            start = i;
+                        }
+                    }
+                }
+            }
         }
 
-        return s.substring(start, start + maxLength);
-    }
-
-    private void expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-
-        // The current palindrome found is s.substring(left + 1, right)
-        // Its length is (right - 1) - (left + 1) + 1 = right - left - 1
-
-        int currentLength = right - left - 1;
-
-        if (currentLength > maxLength) {
-            maxLength = currentLength;
-            start = left + 1; // start index of the new longest palindrome
-        }
+        return s.substring(start, start + maxLen);
     }
 }
