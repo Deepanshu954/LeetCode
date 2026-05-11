fec989123225645d52c48287012c68e7e6463443
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-11 09:36:02 +0530

    Wrong Answer: Wildcard Matching

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,33 +1,22 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
-        int l = lcs(s,p);
+    public boolean isMatch(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
 
-        return s.length() == l;
+        return rec(m-1, n-1, s1, s2);
     }
 
-    public int lcs(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
+    private boolean rec(int i, int j, String s1, String s2) {
+        if(i < 0) return true;
+        if(j < 0) return false;
 
-        int[] dp = new int[m+1];
+        char ch1 = s1.charAt(i);
+        char ch2 = s2.charAt(j);
 
-        for(int i = 1; i <= n; i++) {
-            char ch1 = s1.charAt(i-1);
+        if(ch1 == ch2 || ch2 == '?') return rec(i-1, j-1, s1, s2);
 
-            int[] curr = new int[m+1];
+        if(ch2 == '*') return rec(i-1, j-1, s1, s2) || rec(i-1, j, s1, s2);
 
-            for(int j = 1; j <= m; j++) {
-                char ch2 = s2.charAt(j-1);
-                if(ch2 == '*') return n;
-
-                if(ch1 == ch2 || ch2 == '?') curr[j] = 1 + dp[j-1];
-                else curr[j] = Math.max(dp[j], curr[j-1]);
-            }
-
-            dp = curr;
-        }
-
-        return dp[m];
+        return false;
     }
-
 }
