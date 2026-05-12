9f9c1b44bb19987dec2bdc7ff17e1353dbba76cc
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-12 12:10:26 +0530

    Accepted: Edit Distance

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,3 +1,48 @@
+// memorization
+
+class Solution {
+    private int[][] dp;
+
+    public int minDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        dp = new int[m + 1][n + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        return rec(m - 1, n - 1, s1, s2);
+    }
+
+    private int rec(int i, int j, String s1, String s2) {
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            dp[i][j] = rec(i - 1, j - 1, s1, s2);
+        } else {
+            int ins = rec(i - 1, j, s1, s2);
+            int del = rec(i, j - 1, s1, s2);
+            int rep = rec(i - 1, j - 1, s1, s2);
+
+            dp[i][j] = 1 + Math.min(ins, Math.min(del, rep));
+        }
+
+        return dp[i][j];
+    }
+
+}
+
+/*
+
+
+// Recursion
+
 class Solution {
     public int minDistance(String s1, String s2) {
         int m = s1.length();
@@ -23,3 +68,5 @@ class Solution {
 
 
 }
+
+*/
