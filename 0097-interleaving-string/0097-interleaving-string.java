d5d87564156ce0f236b3555d27728a062bba4efc
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-12 18:52:42 +0530

    Time: 4 ms (54.52%), Space: 43.1 MB (81.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,4 +1,4 @@
-// Tabular
+// Space Optimization
 
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -8,31 +8,25 @@ class Solution {
 
         if(m + n != o) return false;
 
-        boolean[][] dp = new boolean[m+1][n+1];
-        dp[0][0] = true;
-
-        for(int i = 1; i <= m; i++) 
-            dp[i][0] = dp[i-1][0] && (s1.charAt(i-1) == s3.charAt(i-1));
-
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
 
         for(int j = 1; j <= n; j++)
-            dp[0][j] = dp[0][j-1] && (s2.charAt(j-1) == s3.charAt(j-1));
+            dp[j] = dp[j-1] && (s2.charAt(j-1) == s3.charAt(j-1));
 
 
         for(int i = 1; i <= m; i++) {
+
+            dp[0] = dp[0] && (s1.charAt(i-1) == s3.charAt(i-1));
             for(int j = 1; j <= n; j++) {
                 int k = i + j;
 
-                if(s1.charAt(i-1) == s3.charAt(k-1)) {
-                    dp[i][j] |= dp[i-1][j];
-                } 
-                if(s2.charAt(j-1) == s3.charAt(k-1)) {
-                    dp[i][j] |= dp[i][j-1];
-                }
+                dp[j] = (dp[j] && (s1.charAt(i-1) == s3.charAt(k-1))) ||
+                        (dp[j-1] && (s2.charAt(j-1) == s3.charAt(k-1)));
             }
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 }
 
@@ -153,5 +147,47 @@ class Solution {
 }
 
 
+
+*/
+
+/*
+
+// Tabular
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int o = s3.length();
+
+        if(m + n != o) return false;
+
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+
+        for(int i = 1; i <= m; i++) 
+            dp[i][0] = dp[i-1][0] && (s1.charAt(i-1) == s3.charAt(i-1));
+
+
+        for(int j = 1; j <= n; j++)
+            dp[0][j] = dp[0][j-1] && (s2.charAt(j-1) == s3.charAt(j-1));
+
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                int k = i + j;
+
+                if(s1.charAt(i-1) == s3.charAt(k-1)) {
+                    dp[i][j] |= dp[i-1][j];
+                } 
+                if(s2.charAt(j-1) == s3.charAt(k-1)) {
+                    dp[i][j] |= dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
 
 */
