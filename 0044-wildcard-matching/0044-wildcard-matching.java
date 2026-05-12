8b4681a6ff7d90c8b46e0744a9cb35b05ab672fb
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-12 14:11:57 +0530

    Time: 16 ms (64.91%), Space: 63.7 MB (10.36%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,75 +2,64 @@
 
 class Solution {
     private Boolean[][] dp;
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
 
-    public boolean isMatch(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+        dp = new Boolean[m+1][n+1];
 
-        dp = new Boolean[m][n];
-        return rec(m-1, n-1, s1, s2);
+        return rec(m-1, n-1, s, p);
     }
 
-    private boolean rec(int i, int j, String s1, String s2) {
-
+    private boolean rec(int i, int j, String s, String p) {
         if(i < 0 && j < 0) return true;
         if(j < 0) return false;
 
         if(i < 0) {
             for(int k = 0; k <= j; k++) {
-                if(s2.charAt(k) != '*') return false;
-            }
-            return true;
+                if(p.charAt(k) != '*') return false;
+            } return true;
         }
 
         if(dp[i][j] != null) return dp[i][j];
 
-        char ch1 = s1.charAt(i);
-        char ch2 = s2.charAt(j);
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+            dp[i][j] = rec(i-1, j-1, s, p);
+        } else if(p.charAt(j) == '*') {
+            dp[i][j] =  rec(i, j-1, s, p) || rec(i-1, j, s, p);
+        } else dp[i][j] = false;
 
-        if(ch1 == ch2 || ch2 == '?') {
-            return dp[i][j] = rec(i-1, j-1, s1, s2);
-        }
-        else if(ch2 == '*') {
-            return dp[i][j] = rec(i-1, j, s1, s2) || rec(i, j-1, s1, s2);
-        }
-
-        return dp[i][j] = false;
+        return dp[i][j];
     }
 }
-
 
 /*
 
 // Recursion
 
 class Solution {
-    public boolean isMatch(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
 
-        return rec(m-1, n-1, s1, s2);
+        return rec(m-1, n-1, s, p);
     }
 
-    private boolean rec(int i, int j, String s1, String s2) {
+    private boolean rec(int i, int j, String s, String p) {
         if(i < 0 && j < 0) return true;
         if(j < 0) return false;
 
         if(i < 0) {
             for(int k = 0; k <= j; k++) {
-                if(s2.charAt(k) != '*') return false;
-            }
-            return true;
+                if(p.charAt(k) != '*') return false;
+            } return true;
         }
 
-        char ch1 = s1.charAt(i);
-        char ch2 = s2.charAt(j);
-
-        if(ch1 == ch2 || ch2 == '?') return rec(i-1, j-1, s1, s2);
-
-        if(ch2 == '*') return rec(i-1, j, s1, s2) || rec(i, j-1, s1, s2);
-
-        return false;
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+            return rec(i-1, j-1, s, p);
+        } else if(p.charAt(j) == '*') {
+            return  rec(i, j-1, s, p) || rec(i-1, j, s, p);
+        } else return false;
     }
 }
 
