020c90450db1360426eeb113c65ad3f5800d1020
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-12 11:42:03 +0530

    Accepted: Distinct Subsequences

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,51 +1,58 @@
-// Space Optimization
+// Memorization
 
 class Solution {
+    private int[][] dp;
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
 
-        int[] dp = new int[n+1];
+        dp = new int[m+1][n+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
 
-        dp[0] = 1;
+        return rec(m-1, n-1, s, t);
+    }
 
-        for(int i = 1; i <= m; i++) {
-            for(int j = n; j > 0 ; j--) {
-                if(s.charAt(i-1) == t.charAt(j-1))
-                    dp[j] = dp[j] + dp[j-1];
-            }
+    private int rec(int i, int j, String s, String t) {
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s.charAt(i) == t.charAt(j)) {
+            dp[i][j] = rec(i-1, j-1, s, t) + rec(i-1, j, s, t);
+        } else {
+            dp[i][j] = rec(i-1, j, s, t);
         }
 
-        return dp[n];
+        return dp[i][j];
     }
+
 }
 
 
 /*
 
-// Tabular
+// Recursion
 
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
 
-        int[][] dp = new int[m+1][n+1];
+        return rec(m-1, n-1, s, t);
+    }
 
-        for(int i = 0; i <= m; i++)
-            dp[i][0] = 1;
+    private int rec(int i, int j, String s, String t) {
+        if(j < 0) return 1;
+        if(i < 0) return 0;
 
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i-1][j];
-
-                if(s.charAt(i-1) == t.charAt(j-1)) 
-                    dp[i][j] += dp[i-1][j-1];
-            }
+        if(s.charAt(i) == t.charAt(j)) {
+            return rec(i-1, j-1, s, t) + rec(i-1, j, s, t);
         }
 
-        return dp[m][n];
+        return rec(i-1, j, s, t);
     }
+
 }
 
 */
