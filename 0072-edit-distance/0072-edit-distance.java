8ccec2178db31e20392a94d112268613d466d2c4
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-12 12:10:30 +0530

    Time: 3 ms (98.54%), Space: 47.4 MB (5.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,100 +1,72 @@
-class Solution {
-    public int minDistance(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-
-        int[][] dp = new int[m+1][n+1];
-
-        for(int i = 0; i <= m; i++) dp[i][0] = i;
-        for(int j = 0; j <= n; j++) dp[0][j] = j;
-
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-
-                if(s1.charAt(i-1) == s2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1]; // ✅ no +1
-                } else {
-                    dp[i][j] = 1 + Math.min(
-                        dp[i-1][j],      // delete
-                        Math.min(
-                            dp[i][j-1],  // insert
-                            dp[i-1][j-1] // replace
-                        )
-                    );
-                }
-            }
-        }
-
-        return dp[m][n];
-    }
-}
-
-
-
-/*
-
-// recursion
-
-class Solution {
-    public int minDistance(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-
-        return rec(m-1, n-1, s1, s2);
-    }
-
-    private int rec(int i, int j, String s1, String s2) {
-        if(i < 0) return j+1; // insert all
-        if(j < 0) return i+1; // delete all
-
-        if(s1.charAt(i) == s2.charAt(j)) {
-            return rec(i-1, j-1, s1, s2);
-        }
-
-        int del = rec(i-1, j, s1, s2);
-        int ins = rec(i, j-1, s1, s2);
-        int rep = rec(i-1, j-1, s1, s2);
-
-        return 1 + Math.min(del, Math.min(ins,rep));
-    }
-}
-
-*/
-
-
-/*
-
-
 // memorization
 
 class Solution {
     private int[][] dp;
+
     public int minDistance(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
 
-        dp = new int[m+1][n+1];
-        for(int[] d : dp) Arrays.fill(d, -1);
+        dp = new int[m + 1][n + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        return rec(m - 1, n - 1, s1, s2);
+    }
+
+    private int rec(int i, int j, String s1, String s2) {
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            dp[i][j] = rec(i - 1, j - 1, s1, s2);
+        } else {
+            int ins = rec(i - 1, j, s1, s2);
+            int del = rec(i, j - 1, s1, s2);
+            int rep = rec(i - 1, j - 1, s1, s2);
+
+            dp[i][j] = 1 + Math.min(ins, Math.min(del, rep));
+        }
+
+        return dp[i][j];
+    }
+
+}
+
+/*
+
+
+// Recursion
+
+class Solution {
+    public int minDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
 
         return rec(m-1, n-1, s1, s2);
     }
 
     private int rec(int i, int j, String s1, String s2) {
-        if(i < 0) return j+1; // insert all
-        if(j < 0) return i+1; // delete all
-
-        if(dp[i][j] != -1) return dp[i][j];
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
 
         if(s1.charAt(i) == s2.charAt(j)) {
             return rec(i-1, j-1, s1, s2);
         }
 
-        int del = rec(i-1, j, s1, s2);
-        int ins = rec(i, j-1, s1, s2);
+        int ins = rec(i-1, j, s1, s2);
+        int del = rec(i, j-1, s1, s2);
         int rep = rec(i-1, j-1, s1, s2);
 
-        return dp[i][j] = 1 + Math.min(del, Math.min(ins,rep));
+        return 1 + Math.min(ins, Math.min(del, rep));
     }
+
+
 }
 
 */
