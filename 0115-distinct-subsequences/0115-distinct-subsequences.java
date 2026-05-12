55a184f1e648b470488292c11289b91be85be22c
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-12 12:00:12 +0530

    Accepted: Distinct Subsequences

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,25 +1,23 @@
-// Tabular
+// Space Optimization
 
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
 
-        int[][] dp = new int[m+1][n+1];
+        int[] dp = new int[n+1];
 
-        for(int i = 0; i <= m; i++) dp[i][0] = 1;
+        dp[0] = 1;
 
         for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i-1][j];
-
+            for(int j = n; j > 0; j--) {
                 if(s.charAt(i-1) == t.charAt(j-1)) {
-                    dp[i][j] += dp[i-1][j-1];
+                    dp[j] += dp[j-1];
                 }
             }
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 }
 
@@ -86,5 +84,36 @@ class Solution {
     }
 
 }
+
+*/
+
+
+/*
+
+// Tabular
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = 0; i <= m; i++) dp[i][0] = 1;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j];
+
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
+
 
 */
