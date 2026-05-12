43f5df3e66c0eeec4a40284dd43202ab7ac24644
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-12 12:17:38 +0530

    Time: 5 ms (68.7%), Space: 47.1 MB (51.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -17,7 +17,13 @@ class Solution {
                 if(s1.charAt(i-1) == s2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1];
                 } else {
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    dp[i][j] = 1 + Math.min(
+                        dp[i-1][j-1], 
+                        Math.min(
+                            dp[i-1][j], 
+                            dp[i][j-1]
+                        )
+                    );
                 }
             }
         }
