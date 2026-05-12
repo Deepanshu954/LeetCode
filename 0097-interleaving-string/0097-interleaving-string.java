8236f85a35f5354d8a69168aadf6b4c6448dc6b2
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-12 18:30:52 +0530

    Time Limit Exceeded: Interleaving String

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,3 +1,7 @@
+/*
+
+// Recursion
+
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         int m = s1.length();
@@ -25,5 +29,39 @@ class Solution {
         } else {
             return false;
         }
+    }
+}
+
+
+*/
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int o = s3.length();
+
+        if(m + n != o) return false;
+
+        return rec(m-1, n-1, o-1, s1, s2, s3);
+    }
+
+    private boolean rec(int i, int j, int k, String s1, String s2, String s3) {
+        if(i < 0 && j < 0) return true;
+
+        char ch1 = (i >= 0) ? s1.charAt(i) : 'X';
+        char ch2 = (j >= 0) ? s2.charAt(j) : 'X';
+        char ch3 = s3.charAt(k);
+
+        boolean res = false;
+
+        if(ch1 == ch3) {
+            res |= rec(i-1, j, k-1, s1, s2, s3);
+        } 
+        if(ch2 == ch3) {
+            res |= rec(i, j-1, k-1, s1, s2, s3);
+        }
+
+        return res;
     }
 }
