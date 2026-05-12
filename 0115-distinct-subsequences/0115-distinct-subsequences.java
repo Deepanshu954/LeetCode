1930e48dff13cee286adff6055eee64b88a5b02c
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-12 11:50:21 +0530

    Time: 22 ms (23.8%), Space: 54.6 MB (28.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,3 +1,61 @@
+// Tabular
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = 0; i <= m; i++) dp[i][0] = 1;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j];
+
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
+
+
+
+
+/*
+
+// Recursion
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        return rec(m-1, n-1, s, t);
+    }
+
+    private int rec(int i, int j, String s, String t) {
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+
+        if(s.charAt(i) == t.charAt(j)) {
+            return rec(i-1, j-1, s, t) + rec(i-1, j, s, t);
+        }
+
+        return rec(i-1, j, s, t);
+    }
+
+}
+
+*/
+
+/*
+
+
 // Memorization
 
 class Solution {
@@ -25,32 +83,6 @@ class Solution {
         }
 
         return dp[i][j];
-    }
-
-}
-
-
-/*
-
-// Recursion
-
-class Solution {
-    public int numDistinct(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-
-        return rec(m-1, n-1, s, t);
-    }
-
-    private int rec(int i, int j, String s, String t) {
-        if(j < 0) return 1;
-        if(i < 0) return 0;
-
-        if(s.charAt(i) == t.charAt(j)) {
-            return rec(i-1, j-1, s, t) + rec(i-1, j, s, t);
-        }
-
-        return rec(i-1, j, s, t);
     }
 
 }
