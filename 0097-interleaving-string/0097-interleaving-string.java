06c6a30ac45bcf53822e159479f355a6276ec070
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-12 18:38:20 +0530

    Time: 0 ms (100%), Space: 42.9 MB (89.74%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,3 +1,7 @@
+/*
+
+// Recursion
+
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         int m = s1.length();
@@ -25,5 +29,46 @@ class Solution {
         } else {
             return false;
         }
+    }
+}
+
+
+*/
+
+class Solution {
+    private Boolean[][] dp;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int o = s3.length();
+
+        if(m + n != o) return false;
+
+        dp = new Boolean[m+1][n+1];
+
+        return rec(m-1, n-1, o-1, s1, s2, s3);
+    }
+
+    private boolean rec(int i, int j, int k, String s1, String s2, String s3) {
+        if(i < 0 && j < 0) return true;
+
+        if(i >= 0 && j >= 0 && dp[i][j] != null) return dp[i][j];
+
+        char ch1 = (i >= 0) ? s1.charAt(i) : 'X';
+        char ch2 = (j >= 0) ? s2.charAt(j) : 'X';
+        char ch3 = s3.charAt(k);
+
+        boolean res = false;
+
+        if(ch1 == ch3) {
+            res |= rec(i-1, j, k-1, s1, s2, s3);
+        } 
+        if(ch2 == ch3) {
+            res |= rec(i, j-1, k-1, s1, s2, s3);
+        }
+
+        if(i >= 0 && j >= 0) dp[i][j] = res;
+
+        return res;
     }
 }
