b012214175bed559f8dfb360fe86bc82c8116f10
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-12 12:07:58 +0530

    Time Limit Exceeded: Edit Distance

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -7,17 +7,19 @@ class Solution {
     }
 
     private int rec(int i, int j, String s1, String s2) {
-        if(i < 0) return j+1; // insert all
-        if(j < 0) return i+1; // delete all
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
 
         if(s1.charAt(i) == s2.charAt(j)) {
             return rec(i-1, j-1, s1, s2);
         }
 
-        int del = rec(i-1, j, s1, s2);
-        int ins = rec(i, j-1, s1, s2);
+        int ins = rec(i-1, j, s1, s2);
+        int del = rec(i, j-1, s1, s2);
         int rep = rec(i-1, j-1, s1, s2);
 
-        return 1 + Math.min(del, Math.min(ins,rep));
+        return 1 + Math.min(ins, Math.min(del, rep));
     }
+
+
 }
