69e7fc14a653d03cb9054c39e268635856c8ddc6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-13 16:44:03 +0530

    Accepted: Best Time to Buy and Sell Stock IV

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,70 +1,46 @@
 class Solution {
     private Integer[][][] dp;
+
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
 
-        dp = new Integer[n+1][k+1][2];
-        return rec(n-1, k, 0, prices);
+        // optimization
+        if (k >= n / 2) {
+            int profit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1])
+                    profit += prices[i] - prices[i - 1];
+            }
+            return profit;
+        }
+
+        dp = new Integer[n][k + 1][2];
+        return rec(n - 1, k, 0, prices);
     }
 
-    private int rec(int i, int j, int k, int[] prices) {
-        if(i == 0) {
-            return (k == 0) ? 0 : -prices[0];
-        }
-        
-        if(j == 0) {
-            return (k == 0) ? 0 : (int)-1e9;
+    private int rec(int i, int j, int hold, int[] prices) {
+        if (i == 0) {
+            return (hold == 0) ? 0 : -prices[0];
         }
 
-        if(dp[i][j][k] != null) return dp[i][j][k];
+        if (j == 0) {
+            return (hold == 0) ? 0 : Integer.MIN_VALUE / 2;
+        }
 
-        if(k == 0) {
-            dp[i][j][k] = Math.max(
-                rec(i-1, j, 0, prices),
-                rec(i-1, j, 1, prices) + prices[i]
+        if (dp[i][j][hold] != null) return dp[i][j][hold];
+
+        if (hold == 0) {
+            dp[i][j][hold] = Math.max(
+                rec(i - 1, j, 0, prices),
+                rec(i - 1, j, 1, prices) + prices[i]
             );
         } else {
-            dp[i][j][k] = Math.max(
-                rec(i-1, j, 1, prices),
-                rec(i-1, j-1, 0, prices) - prices[i]
+            dp[i][j][hold] = Math.max(
+                rec(i - 1, j, 1, prices),
+                rec(i - 1, j - 1, 0, prices) - prices[i]
             );
         }
 
-        return dp[i][j][k];
+        return dp[i][j][hold];
     }
 }
-
-/*
-
-// Recursion
-
-class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-
-        return rec(n-1, 2, 0, prices);
-    }
-
-    private int rec(int i, int j, int k, int[] prices) {
-        if(i == 0) {
-            return (k == 0) ? 0 : -prices[0];
-        }
-        if(j == 0) {
-            return 0;
-        }
-
-        if(k == 0) {
-            return Math.max(
-                rec(i-1, j, 0, prices),
-                rec(i-1, j, 1, prices) + prices[i]
-            );
-        } else {
-            return Math.max(
-                rec(i-1, j, 1, prices),
-                rec(i-1, j-1, 0, prices) - prices[i]
-            );
-        }
-    }
-}
-
-*/
