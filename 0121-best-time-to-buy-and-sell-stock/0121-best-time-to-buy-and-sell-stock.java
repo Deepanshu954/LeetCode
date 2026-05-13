0d8818f4a72354218a04cbc8546d79380f5410f3
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-13 09:41:33 +0530

    Time Limit Exceeded: Best Time to Buy and Sell Stock

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int maxProfit(int[] nums) {
-        int prof = 0, maxProf = 0, min = nums[0];
+    public int maxProfit(int[] prices) {
+        return rec(0, 1, prices);
+    }
 
-        for(int num : nums){
-            if(min > num){
-                min = num;
-                prof = 0;
-            }
-            prof = num - min;
-            maxProf = Math.max(prof, maxProf);
+    private int rec(int i, int buy, int[] prices) {
+        if(i == prices.length) return 0;
+
+        if(buy == 1) {
+            int take = -prices[i] + rec(i+1, 0, prices); // buy
+            int skip = rec(i+1, 1, prices);              // skip
+            return Math.max(take, skip);
+        } else {
+            int sell = prices[i];        // sell → STOP (no future recursion)
+            int skip = rec(i+1, 0, prices);
+            return Math.max(sell, skip);
         }
-        return maxProf;
     }
 }
