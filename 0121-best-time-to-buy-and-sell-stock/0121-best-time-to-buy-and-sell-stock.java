534e43b2391dd97ae391fe28469e364a1e4ec559
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-13 10:04:08 +0530

    Accepted: Best Time to Buy and Sell Stock

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,3 +1,60 @@
+// Tabular
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+
+        for(int i = n-1; i >= 0; i--) {
+            dp[i][0] = Math.max(
+                dp[i+1][0],
+                dp[i+1][1] - prices[i]
+            );
+
+            dp[i][1] = Math.max(
+                dp[i+1][1],
+                prices[i]
+            );
+        }
+
+        return dp[0][0];
+    }
+}
+
+/*
+
+
+// Recursion
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        return rec(0, 1, prices);
+    }
+
+    private int rec(int i, int buy, int[] prices) {
+        if(i == prices.length) return 0;
+
+        if(buy == 1) {
+            int take = -prices[i] + rec(i+1, 0, prices); // buy
+            int skip = rec(i+1, 1, prices);              // skip
+            return Math.max(take, skip);
+        } else {
+            int sell = prices[i];        // sell → STOP (no future recursion)
+            int skip = rec(i+1, 0, prices);
+            return Math.max(sell, skip);
+        }
+    }
+}
+
+
+*/
+
+
+/*
+
+// Memorization
+
+
 class Solution {
     private Integer[][] dp;
     public int maxProfit(int[] prices) {
@@ -25,3 +82,5 @@ class Solution {
         return dp[i][hold];
     }
 }
+
+*/
