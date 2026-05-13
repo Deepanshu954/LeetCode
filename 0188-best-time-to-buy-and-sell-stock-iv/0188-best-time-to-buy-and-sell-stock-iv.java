963c69d98dff96892a92a1f3a77e06e9f3992754
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-13 16:43:54 +0530

    Time: 14 ms (7.61%), Space: 56.8 MB (5.03%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,70 @@
+class Solution {
+    private Integer[][][] dp;
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+
+        dp = new Integer[n+1][k+1][2];
+        return rec(n-1, k, 0, prices);
+    }
+
+    private int rec(int i, int j, int k, int[] prices) {
+        if(i == 0) {
+            return (k == 0) ? 0 : -prices[0];
+        }
+        
+        if(j == 0) {
+            return (k == 0) ? 0 : (int)-1e9;
+        }
+
+        if(dp[i][j][k] != null) return dp[i][j][k];
+
+        if(k == 0) {
+            dp[i][j][k] = Math.max(
+                rec(i-1, j, 0, prices),
+                rec(i-1, j, 1, prices) + prices[i]
+            );
+        } else {
+            dp[i][j][k] = Math.max(
+                rec(i-1, j, 1, prices),
+                rec(i-1, j-1, 0, prices) - prices[i]
+            );
+        }
+
+        return dp[i][j][k];
+    }
+}
+
+/*
+
+// Recursion
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        return rec(n-1, 2, 0, prices);
+    }
+
+    private int rec(int i, int j, int k, int[] prices) {
+        if(i == 0) {
+            return (k == 0) ? 0 : -prices[0];
+        }
+        if(j == 0) {
+            return 0;
+        }
+
+        if(k == 0) {
+            return Math.max(
+                rec(i-1, j, 0, prices),
+                rec(i-1, j, 1, prices) + prices[i]
+            );
+        } else {
+            return Math.max(
+                rec(i-1, j, 1, prices),
+                rec(i-1, j-1, 0, prices) - prices[i]
+            );
+        }
+    }
+}
+
+*/
