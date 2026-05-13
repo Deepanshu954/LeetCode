9dda846b7cb633d01cc057bab67846dec5c25514
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-13 11:04:49 +0530

    Accepted: Best Time to Buy and Sell Stock II

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int totalProfit = 0;
-        
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                totalProfit += prices[i] - prices[i - 1];
+        int res = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i-1] < prices[i]) {
+                res += prices[i] - prices[i-1];
             }
         }
-        
-        return totalProfit;
+
+        return res;
     }
 }
