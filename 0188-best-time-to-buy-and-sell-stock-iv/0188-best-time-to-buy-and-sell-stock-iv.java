879997a0625759a16c30407f9bb7ffd5dc47baf5
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-14 09:05:09 +0530

    Time: 7 ms (38.87%), Space: 47.9 MB (12.6%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,46 +1,39 @@
 class Solution {
-    private Integer[][][] dp;
-
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
+        int[][][] dp = new int[n + 1][k+1][2];
 
-        // optimization
-        if (k >= n / 2) {
-            int profit = 0;
-            for (int i = 1; i < n; i++) {
-                if (prices[i] > prices[i - 1])
-                    profit += prices[i] - prices[i - 1];
+        // Base Case
+        for(int j = 0; j <= k; j++) {
+            dp[0][j][0] = 0;
+
+            dp[0][j][1] = (j > 0) ? -prices[0] : (int)-1e5;
+        }
+
+        // Rest
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= k; j++) {
+                
+                // Not Holding
+                dp[i][j][0] = Math.max( 
+                    dp[i-1][j][0],
+                    dp[i-1][j][1] + prices[i]
+                );
+
+                // Holding
+
+                if (j > 0) {
+                    dp[i][j][1] = Math.max( 
+                        dp[i-1][j][1],
+                        dp[i-1][j-1][0] - prices[i]
+                    );
+                } else {
+                    dp[i][j][1] = dp[i-1][j][1];
+                }
+
             }
-            return profit;
         }
 
-        dp = new Integer[n][k + 1][2];
-        return rec(n - 1, k, 0, prices);
-    }
-
-    private int rec(int i, int j, int hold, int[] prices) {
-        if (i == 0) {
-            return (hold == 0) ? 0 : -prices[0];
-        }
-
-        if (j == 0) {
-            return (hold == 0) ? 0 : Integer.MIN_VALUE / 2;
-        }
-
-        if (dp[i][j][hold] != null) return dp[i][j][hold];
-
-        if (hold == 0) {
-            dp[i][j][hold] = Math.max(
-                rec(i - 1, j, 0, prices),
-                rec(i - 1, j, 1, prices) + prices[i]
-            );
-        } else {
-            dp[i][j][hold] = Math.max(
-                rec(i - 1, j, 1, prices),
-                rec(i - 1, j - 1, 0, prices) - prices[i]
-            );
-        }
-
-        return dp[i][j][hold];
+        return dp[n-1][k][0];
     }
 }
