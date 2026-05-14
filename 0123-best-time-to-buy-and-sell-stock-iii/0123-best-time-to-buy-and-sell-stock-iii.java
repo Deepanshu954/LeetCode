185f838ea6c9bb89b3120f8c64f867a193a4f74b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-14 09:02:41 +0530

    Accepted: Best Time to Buy and Sell Stock III

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,39 @@
 class Solution {
-    private Integer[][][] dp;
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        dp = new Integer[n+1][3][2];
+        int[][][] dp = new int[n + 1][3][2];
 
-        return rec(n-1, 2, 0, prices);
-    }
+        // Base Case
+        for(int j = 0; j < 3; j++) {
+            dp[0][j][0] = 0;
 
-    private int rec(int i, int j, int k, int[] prices) {
-        if(i == 0)
-            return (k == 0) ? 0 : -prices[0];
-        if(j == 0) return 0;
-
-        if(dp[i][j][k] != null) return dp[i][j][k];
-
-        if(k == 0) {
-            dp[i][j][k] = Math.max(
-                rec(i-1, j, 0, prices),
-                rec(i-1, j, 1, prices) + prices[i]
-            );
-        } else {
-            dp[i][j][k] = Math.max(
-                rec(i-1, j, 1, prices),
-                rec(i-1, j-1, 0, prices) - prices[i]
-            );
+            dp[0][j][1] = (j > 0) ? -prices[0] : (int)-1e5;
         }
 
-        return dp[i][j][k];
+        // Rest
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < 3; j++) {
+                
+                // Not Holding
+                dp[i][j][0] = Math.max( 
+                    dp[i-1][j][0],
+                    dp[i-1][j][1] + prices[i]
+                );
+
+                // Holding
+
+                if (j > 0) {
+                    dp[i][j][1] = Math.max( 
+                        dp[i-1][j][1],
+                        dp[i-1][j-1][0] - prices[i]
+                    );
+                } else {
+                    dp[i][j][1] = dp[i-1][j][1];
+                }
+
+            }
+        }
+
+        return dp[n-1][2][0];
     }
 }
