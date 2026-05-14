fa6977fdf35d39b675d81c08d52a83050cce1fcf
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-14 19:05:18 +0530

    Wrong Answer: Fibonacci Number

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,45 +1,14 @@
-// // Brute Force
-
-// class Solution {
-//     public int fib(int n) {
-//         if(n < 2) return n;
-//         return fib(n-1) + fib(n-2);
-//     }
-// }
-
-// // Optimal Approach : Using HashMap
-
-// class Solution {
-//     private Map<Integer, Integer> map = new HashMap<>();
-
-//     public int fib(int n) {
-//         if(n < 2) return n;
-        
-//         if(map.containsKey(n)) return map.get(n);
-
-//         int result = fib(n-1) + fib(n-2);
-//         map.put(n, result);
-
-//         return result;
-//     }
-
-// }
-
-// Optimal Approach : Using Array
-
 class Solution {
     public int fib(int n) {
-        int[] arr = new int[n+1];
-        Arrays.fill(arr, -1);
-        return helper(arr, n);
-    }
+        int a = 0;
+        int b = 1;
 
-    private int helper(int[] arr, int n) {
-        if( n < 2) return n;
+        for(int i = 1; i < n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
 
-        if(arr[n] != -1) return arr[n];
-
-        arr[n] = helper(arr, n - 1) + helper(arr, n - 2);
-        return arr[n];
+        return b;
     }
 }
