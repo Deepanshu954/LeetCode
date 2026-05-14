796d04eede2cc2c46050765b7e6ff0daf9688058
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-14 16:25:32 +0530

    Accepted: Longest Increasing Subsequence

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,60 @@
+// Tabular
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i-1; j >= -1; j--) {
+
+                int notTake = dp[i+1][j+1];
+
+                int take = 0;
+                if(j == -1 || nums[i] > nums[j]) {
+                    take = 1 + dp[i+1][i+1];
+                }
+
+                dp[i][j+1] = Math.max(notTake, take);
+
+            }
+        }
+
+        return dp[0][0];
+    }
+}
+
+/*
+
+// Recursion
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        return rec(n-1, -1,nums);
+    }
+
+    private int rec(int i, int prev,int[] nums) {
+        if(i < 0) return 0;
+
+        // not take
+        int notTake = rec(i-1, prev, nums);
+
+        // take
+        int take = 0;
+        if(prev == -1 || nums[i] < nums[prev]) {
+            take = 1 + rec(i-1, i, nums);
+        }
+
+        return Math.max(take, notTake);
+    }
+}
+
+*/
+
+/*
+
 // Memorization
 
 class Solution {
@@ -28,33 +85,6 @@ class Solution {
             return Math.max(take, notTake);
         else
             return dp[i][prev] = Math.max(take, notTake);
-    }
-}
-
-/*
-
-// Recursion
-
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-
-        return rec(n-1, -1,nums);
-    }
-
-    private int rec(int i, int prev,int[] nums) {
-        if(i < 0) return 0;
-
-        // not take
-        int notTake = rec(i-1, prev, nums);
-
-        // take
-        int take = 0;
-        if(prev == -1 || nums[i] < nums[prev]) {
-            take = 1 + rec(i-1, i, nums);
-        }
-
-        return Math.max(take, notTake);
     }
 }
 
