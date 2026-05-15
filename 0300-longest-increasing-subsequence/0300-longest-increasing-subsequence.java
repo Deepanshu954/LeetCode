d75d9fecf6a9770d2c8d078bcc460edfebbdbfa9
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-05-15 08:30:31 +0530

    Time: 6 ms (86.47%), Space: 46.4 MB (30.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,95 +1,23 @@
-// Tabular
+import java.util.*;
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n+1];
 
-        for(int i = n-1; i >= 0; i--) {
+        List<Integer> temp = new ArrayList<>();
 
-            int[] curr = new int[n+1];
-            for(int j = i-1; j >= -1; j--) {
+        for(int num : nums) {
 
-                int notTake = dp[j+1];
+            int idx = Collections.binarySearch(temp, num);
 
-                int take = 0;
-                if(j == -1 || nums[i] > nums[j]) {
-                    take = 1 + dp[i+1];
-                }
+            if(idx < 0) idx = -(idx + 1); // lower_bound
 
-                curr[j+1] = Math.max(notTake, take);
-
+            if(idx == temp.size()) {
+                temp.add(num);
+            } else {
+                temp.set(idx, num);
             }
-
-            dp = curr;
         }
 
-        return dp[0];
+        return temp.size();
     }
 }
-
-/*
-
-// Recursion
-
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-
-        return rec(n-1, -1,nums);
-    }
-
-    private int rec(int i, int prev,int[] nums) {
-        if(i < 0) return 0;
-
-        // not take
-        int notTake = rec(i-1, prev, nums);
-
-        // take
-        int take = 0;
-        if(prev == -1 || nums[i] < nums[prev]) {
-            take = 1 + rec(i-1, i, nums);
-        }
-
-        return Math.max(take, notTake);
-    }
-}
-
-*/
-
-/*
-
-// Memorization
-
-class Solution {
-    private Integer[][] dp;
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-
-        dp = new Integer[n+1][n+1];
-
-        return rec(n-1, -1,nums);
-    }
-
-    private int rec(int i, int prev,int[] nums) {
-        if(i < 0) return 0;
-
-        if(prev != -1 && dp[i][prev] != null) return dp[i][prev];
-
-        // not take
-        int notTake = rec(i-1, prev, nums);
-
-        // take
-        int take = 0;
-        if(prev == -1 || nums[i] < nums[prev]) {
-            take = 1 + rec(i-1, i, nums);
-        }
-
-        if(prev == -1) 
-            return Math.max(take, notTake);
-        else
-            return dp[i][prev] = Math.max(take, notTake);
-    }
-}
-
-*/
