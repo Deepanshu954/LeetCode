6329c982c7e49df6cc82c258f72abd7f04abbae9
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-20 05:50:57 +0530

    Accepted: Find the Prefix Common Array of Two Arrays

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,21 +1,16 @@
 class Solution {
-public    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        unordered_map<int, int> m1, m2;
-        for(int i = 0; i < n; i++) {
-            m1[A[i]] = i;
-            m2[B[i]] = i;
+        vector<int> freq(n + 1, 0);
+        vector<int> ans;
+        int common = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (++freq[A[i]] == 2) common++;
+            if (++freq[B[i]] == 2) common++;
+            ans.push_back(common);
         }
-        vector<int> c(n);
-        for(int i = 0; i < n; i++) {
-            int cnt = 0;
-            for(int j = 0; j <= i; j++) {
-                if(m1[A[j]] <= i && m2[A[j]] <= i) {
-                    cnt++;
-                }
-            }
-            c[i] = cnt;
-        }
-        return c;
+        return ans;
     }
 };
