eb52c323be34923f0d6aba220d5df0077e93c8fe
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-21 06:21:51 +0530

    Accepted: Find the Length of the Longest Common Prefix

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,388 @@
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <cassert>
+#include <concepts>
+#include <cstdint>
+#include <functional>
+#include <numeric>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+
+
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+
+
+
+template <typename To>
+inline static constexpr auto cast =
+    []<typename From> [[nodiscard, gnu::always_inline]] (From&& v) noexcept
+{
+    return static_cast<To>(std::forward<From>(v));
+};
+
+
+template <std::integral T>
+[[nodiscard, gnu::always_inline]] static constexpr T ceil_div(
+    T a,
+    std::type_identity_t<T> b) noexcept
+{
+    return (a + (b - 1)) / b;
+}
+
+struct EmptyType
+{
+};
+
+inline static constexpr EmptyType kEmpty{};
+
+struct FullType
+{
+};
+
+inline static constexpr FullType kFull{};
+
+
+
+template <u64 value>
+using UintForValue = std::conditional_t < value < (1 << 8),
+      u8,
+      std::conditional_t <
+          value<
+              (1 << 16),
+              u16,
+              std::conditional_t<value<(1UL << 32), u32, u64>>>;
+
+template <size_t capacity, typename Word = u64>
+class PyramidBitset
+{
+public:
+    using ValueType = UintForValue<capacity - 1>;
+    static constexpr size_t kWordSize = sizeof(Word) * 8;
+    static constexpr u8 kMask = kWordSize - 1;
+    static constexpr int kShift = std::bit_width(kMask);
+    static constexpr size_t kNumLayers =
+        ceil_div(std::bit_width(capacity), kShift);
+    static constexpr auto kLayersSizes = []
+    {
+        std::array<size_t, kNumLayers> sizes{};
+        for (size_t i = 0, x = capacity; i != sizes.size(); ++i)
+        {
+            sizes[i] = x;
+            x = ceil_div(x, kWordSize);
+        }
+        return sizes;
+    }();
+    static constexpr size_t kTotalWords =
+        std::accumulate(kLayersSizes.begin(), kLayersSizes.end(), 0UZ);
+    std::array<Word, kTotalWords> words;
+
+    template <u8 i>
+    struct LyrIdx
+    {
+        // NOLINTNEXTLINE
+        [[nodiscard]] consteval operator u8() const noexcept { return i; }
+    };
+
+    static constexpr auto offsets = []
+    {
+        std::array<size_t, kNumLayers> offsets;  // NOLINT
+        std::exclusive_scan(
+            kLayersSizes.begin(),
+            kLayersSizes.end(),
+            offsets.begin(),
+            size_t{});
+        return offsets;
+    }();
+
+    constexpr PyramidBitset() = default;
+
+    // NOLINTNEXTLINE
+    constexpr PyramidBitset(FullType, size_t size = capacity) noexcept
+    {
+        assert(size <= capacity);
+        initialize(kFull, size);
+    }
+
+    // NOLINTNEXTLINE
+    constexpr PyramidBitset(EmptyType, size_t size = capacity) noexcept
+    {
+        assert(size <= capacity);
+        initialize(kEmpty, size);
+    }
+
+    template <bool v>
+    [[gnu::always_inline]] constexpr void set(ValueType idx) noexcept
+    {
+        if constexpr (v)
+        {
+            add(idx);
+        }
+        else
+        {
+            remove(idx);
+        }
+    }
+
+    [[gnu::always_inline]] constexpr void set(ValueType idx, bool v) noexcept
+    {
+        if (v)
+        {
+            add(idx);
+        }
+        else
+        {
+            remove(idx);
+        }
+    }
+
+    [[gnu::always_inline]] constexpr void add(ValueType v) noexcept
+    {
+        for_each_layer(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                ValueType wi = v >> kShift;
+                words[offsets[layer] + wi] |= Word{1} << (v & kMask);
+                v = wi;
+            });
+    }
+
+    // Returns true if element did not exist previously
+    [[nodiscard, gnu::always_inline]] constexpr bool add_ex(
+        ValueType v) noexcept
+    {
+        bool added = false;
+        for_each_layer(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                ValueType wi = v >> kShift;
+                auto& word = words[offsets[layer] + wi];
+                Word m = Word{1} << (v & kMask);
+                if constexpr (layer == 0)
+                {
+                    added = (word & m) == 0;
+                }
+                word |= m;
+                v = wi;
+            });
+        return added;
+    }
+
+    [[gnu::always_inline]] constexpr void remove(ValueType v) noexcept
+    {
+        bool prev_empty = true;
+        for_each_layer(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                ValueType wi = v >> kShift;
+                auto& w = words[offsets[layer] + wi];
+                w &= ~(Word{prev_empty} << (v & kMask));
+                v = wi;
+                prev_empty = !w;
+            });
+    }
+
+    [[nodiscard, gnu::always_inline]] constexpr bool remove_ex(
+        ValueType v) noexcept
+    {
+        bool prev_empty = true, removed = false;
+        for_each_layer(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                ValueType wi = v >> kShift;
+                auto& w = words[offsets[layer] + wi];
+                if constexpr (layer == 0)
+                {
+                    removed = (w & (Word{1} << (v & kMask)));
+                }
+
+                w &= ~(Word{prev_empty} << (v & kMask));
+                v = wi;
+                prev_empty = !w;
+            });
+
+        return removed;
+    }
+
+    [[nodiscard, gnu::always_inline]] constexpr ValueType min() const noexcept
+    {
+        ValueType wi = 0;
+        for_each_layer_reversed(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                u8 bi = std::countr_zero(words[offsets[layer] + wi]) & 0xFF;
+                ValueType x =
+                    cast<ValueType>(wi << kShift) | cast<ValueType>(bi);
+                wi = x;
+            });
+        return wi;
+    }
+
+    [[nodiscard, gnu::always_inline]] constexpr ValueType max() const noexcept
+    {
+        ValueType wi = 0;
+        for_each_layer_reversed(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                u8 bi =
+                    kMask - std::countl_zero(words[offsets[layer] + wi]) & 0xFF;
+                ValueType x =
+                    cast<ValueType>(wi << kShift) | cast<ValueType>(bi);
+                wi = x;
+            });
+        return wi;
+    }
+
+    [[nodiscard, gnu::always_inline]] constexpr bool get(
+        ValueType v) const noexcept
+    {
+        return words[offsets[0] + (v >> kShift)] & (Word{1} << (v & kMask));
+    }
+
+    [[nodiscard, gnu::always_inline]] static constexpr size_t
+    get_capacity() noexcept
+    {
+        return capacity;
+    }
+
+    [[nodiscard, gnu::always_inline]] constexpr bool is_empty() const noexcept
+    {
+        return words[offsets[kNumLayers - 1]] == 0;
+    }
+
+    [[gnu::always_inline]] constexpr void initialize(
+        FullType,
+        size_t end = capacity) noexcept
+    {
+        initialize<true>(end);
+    }
+
+    [[gnu::always_inline]] constexpr void initialize(
+        EmptyType,
+        size_t end = capacity) noexcept
+    {
+        initialize<false>(end);
+    }
+
+    template <bool value>
+    [[gnu::always_inline]] constexpr void initialize(
+        size_t end = capacity) noexcept
+    {
+        constexpr auto m = value ? ~Word{} : Word{};
+        for_each_layer(
+            [&] [[gnu::always_inline]] (auto layer) noexcept
+            {
+                end = ceil_div<size_t>(end, kWordSize);
+                std::fill_n(std::next(words.begin(), offsets[layer]), end, m);
+            });
+    }
+
+private:
+    template <typename F>
+    [[gnu::always_inline]] constexpr static void for_each_layer(F&& f) noexcept
+    {
+        [&]<size_t... layer> [[gnu::always_inline]] (
+            std::index_sequence<layer...>) noexcept
+        {
+            (f(LyrIdx<layer>{}), ...);
+        }(std::make_index_sequence<kNumLayers>());
+    }
+
+    template <typename F>
+    [[gnu::always_inline]] constexpr static void for_each_layer_reversed(
+        F&& f) noexcept
+    {
+        for_each_layer([&] [[gnu::always_inline]] (auto layer) noexcept
+                       { f(LyrIdx<kNumLayers - (layer + 1)>{}); });
+    }
+};
+
+
+
+
+inline static constexpr auto max2 =
+    []<typename T> [[gnu::always_inline, nodiscard]] (
+        const T& a,
+        const T& b) noexcept -> const T&
+{
+    return std::max(a, b);
+};
+
+
+inline static constexpr auto upd =
+    []<typename T, typename F> [[gnu::always_inline]] (
+        T & x,
+        const std::type_identity_t<T>& another,
+        F&& f) noexcept -> T&
+{
+    return x = std::forward<F>(f)(x, another);
+};
+
+inline static constexpr auto upd_max =
+    std::bind(upd, std::placeholders::_1, std::placeholders::_2, max2);
+
+class Solution
+{
+public:
+    u8 longestCommonPrefix(const std::vector<int>& a, const std::vector<int>& b)
+        const noexcept
+    {
+        auto pa = &a, pb = &b;
+        if (a.size() > b.size()) std::swap(pa, pb);
+        return impl(
+            reinterpret_cast<const std::vector<u32>&>(*pa),
+            reinterpret_cast<const std::vector<u32>&>(*pb));
+    }
+    static u8 impl(
+        const std::vector<u32>& a,
+        const std::vector<u32>& b) noexcept
+    {
+        static PyramidBitset<100'000'001> bs;
+        u8 max_digits = 0;
+        u32 num_unique = 0;
+        for (u32 v : a)
+        {
+            u8 digits = 0;
+            while (v)
+            {
+                num_unique += bs.add_ex(v);
+                v /= 10;
+                ++digits;
+            }
+            upd_max(max_digits, digits);
+        }
+        u8 r = 0;
+        for (u32 v : b)
+        {
+            while (v && !bs.get(v)) v /= 10;
+
+            u8 l = 0;
+            while (v)
+            {
+                num_unique -= bs.remove_ex(v);
+                ++l;
+                v /= 10;
+            }
+            upd_max(r, l);
+            if (r == max_digits || num_unique == 0) break;
+        }
+
+        // cleanup static bitset
+        while (num_unique--) bs.remove(bs.min());
+
+        return r;
+    }
+};
+
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
