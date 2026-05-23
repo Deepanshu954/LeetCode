f8758ca5de6a9b934f4a244f9510c405d506ac47
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-05-23 08:32:19 +0530

    Accepted: Maximum Depth of Binary Tree

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,15 +15,15 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        return helper(root);
+        return rec(root);
     }
 
-    private int helper(TreeNode root) {
+    private int rec(TreeNode root) {
         if(root == null) return 0;
 
-        int left = helper(root.left);
-        int right = helper(root.right);
+        int left = rec(root.left);
+        int right = rec(root.right);
 
-        return Math.max(left, right) + 1;
+        return 1 + Math.max(left, right);
     }
 }
