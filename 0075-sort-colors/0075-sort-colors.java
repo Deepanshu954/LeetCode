fbfaf310ed2c5e3b2c863482b47b59c267093e68
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-01 17:45:14 +0530

    Accepted: Sort Colors

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,49 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
 
-        for(int num : nums)
-        {
+        for(int num : nums) {
             if(num == 0) cnt0++;
             else if(num == 1) cnt1++;
             else cnt2++;
         }
 
-        int index = 0;
-
-        while(cnt0 > 0) {nums[index++] = 0; cnt0--;}
-        while(cnt1 > 0) {nums[index++] = 1; cnt1--;}
-        while(cnt2 > 0) {nums[index++] = 2; cnt2--;}
-        
-        return;
+        int idx = 0;
+        while(cnt0-->0) nums[idx++] = 0;
+        while(cnt1-->0) nums[idx++] = 1;
+        while(cnt2-->0) nums[idx++] = 2;
     }
 }
-
-
-
-// class Solution {
-//     public void sortColors(int[] nums) {
-//         int low = 0, mid = 0, high = nums.length - 1;
-
-//         while (mid <= high) {
-//             if (nums[mid] == 0) {
-//                 swap(nums, low, mid);
-//                 low++;
-//                 mid++;
-//             } else if (nums[mid] == 1) {
-//                 mid++;
-//             } else {
-//                 swap(nums, mid, high);
-//                 high--;
-//             }
-//         }
-//     }
-
-//     private void swap(int[] nums, int i, int j) {
-//         int temp = nums[i];
-//         nums[i] = nums[j];
-//         nums[j] = temp;
-//     }
-// }
