db9e31ff10a9f240ec8c72d2384bdd9989cdd234
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-01 09:11:12 +0530

    Accepted: Minimum Cost of Buying Candies With Discount

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -6,13 +6,11 @@ class Solution {
         Arrays.sort(nums);
         int res = 0;
         
-        // Count from right to left, skipping every 3rd candy
         for (int i = n - 1; i >= 0; i -= 3) {
-            res += nums[i];          // Buy the most expensive remaining candy
+            res += nums[i]; 
             if (i - 1 >= 0) {
-                res += nums[i - 1];  // Buy the 2nd most expensive remaining candy
+                res += nums[i - 1];
             }
-            // The 3rd one at (i - 2) is free, so we automatically skip it by doing i -= 3
         }
         return res;
     }
