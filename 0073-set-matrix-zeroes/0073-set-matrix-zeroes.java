e56d52eb63d1ba8568717a6c8f1e02ed713790b0
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-06 22:09:16 +0530

    Accepted: Set Matrix Zeroes

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,33 +1,47 @@
 class Solution {
-    public void setZeroes(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
+  public void setZeroes(int[][] mat) {
+    int m = mat.length;
+    int n = mat[0].length;
 
-        boolean col0 = false;
+    boolean flagR = false;
+    boolean flagC = false;
 
-        // Mark Row And Col
-        for(int i = 0; i < m; i++) {
-            if(mat[i][0] == 0) col0 = true;
-
-            for(int j = 1; j < n; j++) {
-                if(mat[i][j] == 0) {
-                    mat[i][0] = 0;
-                    mat[0][j] = 0;
-                }
-            }
-        }
-
-        // Apply marker
-        for(int i = m-1; i >= 0; i--) {
-            for(int j = n-1; j >= 1; j--) {
-                if(mat[i][0] == 0 || mat[0][j] == 0) {
-                    mat[i][j] = 0;
-                }
-            }
-
-            if(col0) mat[i][0] = 0;
-        }
-
-
+    // Mark Row
+    for(int i = 0; i < m; i++) {
+      if(mat[i][0] == 0) flagC = true;
     }
+
+    // Mark Col
+    for(int j = 0; j < n; j++) {
+      if(mat[0][j] == 0) flagR = true;
+    }
+
+    // Use Markers
+    for(int i = 1; i < m; i++) {
+      for(int j = 1; j < n; j++) {
+        if(mat[i][j] == 0) {
+            mat[i][0] = 0;
+            mat[0][j] = 0;
+        }
+      }
+    }
+
+    // Set Zero Based On Marker
+    for(int i = 1; i < m; i++) {
+      for(int j = 1; j < n; j++) {
+        if(mat[i][0] == 0 || mat[0][j] == 0) {
+          mat[i][j] = 0;
+        }
+      }
+    }
+
+    if(flagC) {
+      for(int i = 0; i < m; i++) mat[i][0] = 0;
+    }
+
+    if(flagR) {
+      for(int j = 0; j < n; j++) mat[0][j] = 0;
+    }
+
+  }
 }
