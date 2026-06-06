0d7e92cb1955f52dcdfdc060dcc43bb3268c1682
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-06 21:41:23 +0530

    Accepted: Set Matrix Zeroes

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,31 +3,28 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
-        HashSet<Integer> set_i = new HashSet<>();
-        HashSet<Integer> set_j = new HashSet<>();
+        Set<Integer> sr = new HashSet<>();
+        Set<Integer> sc = new HashSet<>();
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(mat[i][j] == 0) {
-                    set_i.add(i);
-                    set_j.add(j);
+                    sr.add(i);
+                    sc.add(j);
                 }
             }
         }
 
-        // row
-        for(int s : set_i) {
+        for(int s : sr) {
             for(int j = 0; j < n; j++) {
                 mat[s][j] = 0;
             }
         }
 
-        // col
         for(int i = 0; i < m; i++) {
-            for(int s : set_j) {
+            for(int s : sc) {
                 mat[i][s] = 0;
             }
         }
-
     }
 }
