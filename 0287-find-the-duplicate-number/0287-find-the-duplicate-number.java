1ceae26e8f74233ad9c4f161634204770230089b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-07 18:52:55 +0530

    Accepted: Find the Duplicate Number

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,11 +1,21 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        int slow = nums[0];
+        int fast = nums[0];
 
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i-1] == nums[i]) return nums[i];
+        // Detect Cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        // find entry
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return slow;
     }
 }
