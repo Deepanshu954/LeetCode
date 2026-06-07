b1ae9d502287912eefd1dd7c6f1dc11b6fd0a69e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-07 18:22:18 +0530

    Accepted: Merge Intervals

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,27 +1,23 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-
-        if (intervals.length == 0) return new int[0][];
+    public int[][] merge(int[][] nums) {
+        if (nums.length == 0) return new int[0][];
 
         // Sort by start time
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(nums, (a, b) -> a[0] - b[0]);
 
-        int index = 0;
-
-        for (int i = 1; i < intervals.length; i++) {
+        int idx = 0;
+        for (int[] curr : nums) {
 
             // Overlap check: last end >= current start
-            if (intervals[index][1] >= intervals[i][0]) {
-                intervals[index][1] =
-                        Math.max(intervals[index][1], intervals[i][1]);
-            }
-            // No overlap → move forward
-            else {
-                index++;
-                intervals[index] = intervals[i];
+            if (nums[idx][1] >= curr[0]) {
+                nums[idx][1] =
+                        Math.max(nums[idx][1], curr[1]);
+            } else {
+                idx++;
+                nums[idx] = curr;
             }
         }
 
-        return Arrays.copyOfRange(intervals, 0, index + 1);
+        return Arrays.copyOfRange(nums, 0, idx + 1);
     }
 }
