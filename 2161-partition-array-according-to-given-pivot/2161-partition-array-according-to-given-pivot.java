d5cadc79ba0ca471db13018e50be325cd956210e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-08 05:56:50 +0530

    Accepted: Partition Array According to Given Pivot

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,40 +1,28 @@
 class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-        int length = nums.length;
-        
-        int countLessThan = 0;
-        int countEqualTo = 0;
-        
-        // Count frequencies to establish our pointer starting zones
-        for (int num : nums) {
-            if (num < pivot) {
-                countLessThan++;
-            } else if (num == pivot) {
-                countEqualTo++;
+    public int[] pivotArray(int[] nums, int p) {
+        int n = nums.length;
+
+        int[] arr = new int[n];
+
+        int idx = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] < p) {
+                arr[idx++] = nums[i];
             }
         }
-        
-        // Establish the 3 pointers mapping to the start of each segment
-        int lessThanPointer = 0;
-        int equalToPointer = countLessThan;
-        int greaterThanPointer = countLessThan + countEqualTo;
-        
-        // Temporary storage array to map elements correctly in a single pass
-        int[] auxiliaryArray = new int[length];
-        
-        for (int i = 0; i < length; i++) {
-            if (nums[i] < pivot) {
-                auxiliaryArray[lessThanPointer++] = nums[i];
-            } else if (nums[i] == pivot) {
-                auxiliaryArray[equalToPointer++] = nums[i];
-            } else {
-                auxiliaryArray[greaterThanPointer++] = nums[i];
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == p) {
+                arr[idx++] = nums[i];
             }
         }
-        
-        // Copy back to nums to satisfy the in-place modification concept
-        System.arraycopy(auxiliaryArray, 0, nums, 0, length);
-        
-        return nums;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > p) {
+                arr[idx++] = nums[i];
+            }
+        }
+
+        return arr;
     }
 }
