ebb3dc035e5363b75f4a87e2a7a1aa0becd6f347
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-09 05:56:53 +0530

    Accepted: Maximum Total Subarray Value I

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -1,29 +1,15 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
-        int n  = nums.length;
-        int cnt = 0;
-        int sum = 0;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
-
-        for(int i = 0; i < n; i++) {
-            int min = (int)1e9;
-            int max = -1;
-            for(int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-            }
-
-            int diff = max - min;
-            pq.add(diff);
-            
+        int n = nums.length;
+        int min = (int)1e9;
+        int max = -1;
+        for(int j = 0; j < n; j++) {
+            min = Math.min(min, nums[j]);
+            max = Math.max(max, nums[j]);
         }
 
-        long res = 0;
-        while(k--> 0 && !pq.isEmpty()) {
-            res += pq.peek();
-        }
+        long diff = max - min;
 
-        return res;
+        return k * diff; 
     }
 }
