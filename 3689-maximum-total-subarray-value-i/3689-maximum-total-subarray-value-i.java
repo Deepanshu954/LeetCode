75f258f056caf9fa3a6e078b670d4544b3d15a23
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-09 05:57:18 +0530

    Accepted: Maximum Total Subarray Value I

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -3,6 +3,7 @@ class Solution {
         int n = nums.length;
         int min = (int)1e9;
         int max = -1;
+
         for(int j = 0; j < n; j++) {
             min = Math.min(min, nums[j]);
             max = Math.max(max, nums[j]);
