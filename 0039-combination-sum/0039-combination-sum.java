49e118633d027c2990ff599d42ce8d244571f288
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-10 06:49:54 +0530

    Accepted: Combination Sum

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,20 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        helper(candidates, 0, new ArrayList<>(), target, result);
-        return result;
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        helper(0, nums, target, new ArrayList<>(), res);
+        return res;
     }
 
-    private void helper(int[] nums, int index, List<Integer> curr, int target, List<List<Integer>> result) {
+    private void helper(int idx, int[] nums, int k, List<Integer> curr, List<List<Integer>> res) {
+        if(k == 0) res.add(new ArrayList<>(curr));
+        if(idx < 0) return;
 
-        if(index == nums.length) {
-            if(target == 0) result.add(new ArrayList<>(curr));
-            return;
-        }
-
-        if(nums[index] <= target) {
-            curr.add(nums[index]);
-            helper(nums, index, curr, target - nums[index], result); 
+        for(int i = idx; i < nums.length; i++) {
+            if(nums[i] > k) continue;
+            curr.add(nums[i]);
+            helper(i, nums, k - nums[i], curr, res);
             curr.remove(curr.size() - 1);
         }
-
-        helper(nums, index + 1, curr, target, result); 
-
     }
 }
