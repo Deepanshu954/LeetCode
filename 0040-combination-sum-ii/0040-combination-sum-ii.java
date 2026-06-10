8824c2cbafe98c9b746bb1f5ea34317e22e8e680
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-10 06:57:09 +0530

    Accepted: Combination Sum II

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,18 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
 
         helper(0, nums, target, new ArrayList<>(), res);
-        List<List<Integer>> result = new ArrayList<>(res);
-        return result;
+        return res;
     }
 
-    private void helper(int idx, int[] nums, int k, List<Integer> curr, Set<List<Integer>> res) {
-        if(k == 0) res.add(new ArrayList<>(curr));
+    private void helper(int idx, int[] nums, int k, List<Integer> curr, List<List<Integer>> res) {
+        if(k == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
         if(idx < 0) return;
 
         for(int i = idx; i < nums.length; i++) {
+            if(i > idx && nums[i-1] == nums[i]) continue;
+            if(nums[i] > k) continue;
+
             curr.add(nums[i]);
             helper(i+1, nums, k - nums[i], curr, res);
             curr.remove(curr.size() - 1);
