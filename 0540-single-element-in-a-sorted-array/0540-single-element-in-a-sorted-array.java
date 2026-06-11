1235a9caebadccbf508bd09b7e79b8c0d2945230
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-11 08:26:16 +0530

    Accepted: Single Element in a Sorted Array

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,18 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int res = 0;
+        int left=0, right=nums.length-1;
+        while(left<right){
+            int mid= (left+right)/2;
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]==nums[mid+1]){
+                left=mid+2;
+            }else{
+                right=mid;
+            }
 
-        for(int num : nums) res ^= num;
-
-        return res;
+        }
+        return nums[right];
     }
 }
