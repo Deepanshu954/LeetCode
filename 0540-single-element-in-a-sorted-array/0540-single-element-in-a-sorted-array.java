f86e934cdb2ffcbc07476589c9aad9508d689759
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-11 08:14:32 +0530

    Accepted: Single Element in a Sorted Array

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,28 +1,9 @@
-public class Solution {
-    public int singleNonDuplicate(int[] arr) {
-        int n = arr.length;
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int res = 0;
 
-        if (n == 1) return arr[0];
-        if (arr[0] != arr[1]) return arr[0];
-        if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+        for(int num : nums) res ^= num;
 
-        int low = 1, high = n - 2;
-
-        while (low <= high) {
-            int mid = low + (high - low)/2;
-            if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) return arr[mid];
-
-            // If mid is in the left half (pairing is valid)
-            if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) ||
-                (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
-                // Move to the right half
-                low = mid + 1;
-            }
-            // If mid is in the right half (pairing broken earlier)
-            else {
-                high = mid - 1;
-            }
-        }
-        return -1;
+        return res;
     }
 }
