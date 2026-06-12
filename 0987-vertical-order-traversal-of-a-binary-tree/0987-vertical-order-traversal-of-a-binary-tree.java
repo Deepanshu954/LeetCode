4df58d54cc3c49882cd100e4cd13520cd92f1828
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-12 15:54:14 +0530

    Accepted: Vertical Order Traversal of a Binary Tree

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,55 +1,31 @@
-class Tuple {
-    TreeNode node;
-    int x;
-    int y;
-
-    public Tuple(TreeNode node, int x, int y) {
-        this.node = node;
-        this.x = x;
-        this.y = y;
-    }
-}
-
 class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-
-        Queue<Tuple> dq = new ArrayDeque<>();
-        dq.offer(new Tuple(root, 0, 0));
-
-        while(!dq.isEmpty()) {
-            Tuple t = dq.poll();
-
-            TreeNode node = t.node;
-            int x = t.x;
-            int y = t.y;
-
-            map.putIfAbsent(x, new TreeMap<>());
-            map.get(x).putIfAbsent(y, new PriorityQueue<>());
-            map.get(x).get(y).offer(node.val);
-
-            if(node.left != null) {
-                dq.offer(new Tuple(node.left, x-1, y+1));
-            }
-            if(node.right != null) {
-                dq.offer(new Tuple(node.right, x+1, y+1));
-            }
-        }
-
-        List<List<Integer>> res = new ArrayList<>();
-
-        for(TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
-            List<Integer> col = new ArrayList<>();
-
-            for(PriorityQueue<Integer> pq : ys.values()) {
-                while(!pq.isEmpty()) {
-                    col.add(pq.poll());
+        rowcolumn(0, 0, root);
+        for (TreeMap<Integer, PriorityQueue<Integer>> row : map.values()) {
+            List<Integer> colist = new ArrayList<>();
+            for (PriorityQueue<Integer> pq : row.values()) {
+                while (!pq.isEmpty()) {
+                    colist.add(pq.poll());
                 }
             }
-
-            res.add(col);
+            ans.add(colist);
         }
-
-        return res;
+        return ans;
     }
+
+    void rowcolumn(int row, int col, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        map.putIfAbsent(col, new TreeMap<>());
+        map.get(col).putIfAbsent(row, new PriorityQueue<>());
+        map.get(col).get(row).offer(root.val);
+        rowcolumn(row + 1, col - 1, root.left);
+        rowcolumn(row + 1, col + 1, root.right);
+
+    }
+
 }
