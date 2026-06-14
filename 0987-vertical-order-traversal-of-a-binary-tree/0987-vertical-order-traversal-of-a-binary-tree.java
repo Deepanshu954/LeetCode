9eef24559f0250ce8c9b4fb4543ca40dd13f0c54
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 08:20:56 +0530

    Accepted: Vertical Order Traversal of a Binary Tree

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,17 +1,17 @@
 class Solution {
-    private List<List<Integer>> res;
     private TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map;
+    private List<List<Integer>> res;
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        res = new ArrayList<>();
         map = new TreeMap<>();
+        res = new ArrayList<>();
 
         helper(root, 0, 0);
 
-        for(TreeMap<Integer, PriorityQueue<Integer>> row : map.values()) {
+        for(TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
             List<Integer> list = new ArrayList<>();
 
-            for(PriorityQueue<Integer> pq : row.values()) {
+            for(PriorityQueue<Integer> pq : rows.values()) {
                 while(!pq.isEmpty()) {
                     list.add(pq.poll());
                 }
@@ -19,18 +19,19 @@ class Solution {
 
             res.add(list);
         }
-        
+
         return res;
     }
 
     private void helper(TreeNode root, int row, int col) {
-        if(root == null) return;
+        if (root == null)
+            return;
 
         map.putIfAbsent(col, new TreeMap<>());
         map.get(col).putIfAbsent(row, new PriorityQueue<>());
         map.get(col).get(row).offer(root.val);
 
-        helper(root.left,  row+1, col-1);
-        helper(root.right, row+1, col+1);
+        helper(root.left,  row + 1, col - 1);
+        helper(root.right, row + 1, col + 1);
     }
 }
