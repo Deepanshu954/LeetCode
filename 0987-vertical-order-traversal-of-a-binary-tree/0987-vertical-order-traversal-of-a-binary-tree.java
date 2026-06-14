686860983dfd6433b846dedb78f4b7ba40e95642
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 08:21:48 +0530

    Accepted: Vertical Order Traversal of a Binary Tree

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,37 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map;
-    private List<List<Integer>> res;
+    private static class NodeInfo {
+        int row;
+        int col;
+        int val;
 
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        map = new TreeMap<>();
-        res = new ArrayList<>();
-
-        helper(root, 0, 0);
-
-        for(TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
-            List<Integer> list = new ArrayList<>();
-
-            for(PriorityQueue<Integer> pq : rows.values()) {
-                while(!pq.isEmpty()) {
-                    list.add(pq.poll());
-                }
-            }
-
-            res.add(list);
+        NodeInfo(int row, int col, int val) {
+            this.row = row;
+            this.col = col;
+            this.val = val;
         }
-
-        return res;
     }
 
-    private void helper(TreeNode root, int row, int col) {
-        if (root == null)
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<NodeInfo> nodes = new ArrayList<>();
+        inorder(root, 0, 0, nodes);
+        Collections.sort(nodes, (a, b) -> {
+            if (a.col != b.col) {
+                return Integer.compare(a.col, b.col); 
+            }
+            if (a.row != b.row) {
+                return Integer.compare(a.row, b.row); 
+            }
+            return Integer.compare(a.val, b.val);     
+        });
+
+        List<List<Integer>> result = new ArrayList<>();
+        if (nodes.isEmpty()) {
+            return result;
+        }
+
+        int currentCol = nodes.get(0).col;
+        List<Integer> currentColumnList = new ArrayList<>();
+
+        for (NodeInfo node : nodes) {
+            if (node.col != currentCol) {
+                result.add(currentColumnList);
+                currentColumnList = new ArrayList<>();
+                currentCol = node.col;
+            }
+            currentColumnList.add(node.val);
+        }
+        result.add(currentColumnList); 
+
+        return result;
+    } // Fixed: Removed the duplicate closing brace that was right below this line
+
+    private void inorder(TreeNode node, int row, int col, List<NodeInfo> nodes) {
+        if (node == null) {
             return;
-
-        map.putIfAbsent(col, new TreeMap<>());
-        map.get(col).putIfAbsent(row, new PriorityQueue<>());
-        map.get(col).get(row).offer(root.val);
-
-        helper(root.left,  row + 1, col - 1);
-        helper(root.right, row + 1, col + 1);
+        }
+        inorder(node.left, row + 1, col - 1, nodes);
+        nodes.add(new NodeInfo(row, col, node.val));
+        inorder(node.right, row + 1, col + 1, nodes);
     }
 }
