5d889cc01ae6eba34ef80f3d6f6814528c81b5cb
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 16:24:40 +0530

    Accepted: Path Sum

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,27 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return helper(root, sum);
+    }
+
+    private boolean helper(TreeNode root, int sum) {
         if(root == null) return false;
 
         if(root.left == null && root.right == null) {
-            return targetSum == root.val;
+            return root.val == sum;
         }
 
-        return hasPathSum(root.left, targetSum - root.val) || 
-                hasPathSum(root.right, targetSum - root.val);
+
+        return helper(root.left, sum - root.val) || helper(root.right, sum - root.val);
     }
 }
