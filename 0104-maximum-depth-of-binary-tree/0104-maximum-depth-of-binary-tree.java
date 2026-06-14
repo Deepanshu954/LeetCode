1acb651374404785fd86beea8c05481087362912
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 08:44:25 +0530

    Compile Error: Maximum Depth of Binary Tree

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,11 +1,8 @@
 class Solution {
     public int maxDepth(TreeNode root) {
-        return helper(root);
     }
 
     private int helper(TreeNode root) {
-        if(root == null) return 0;
-
-        return 1 + Math.max(helper(root.left), helper(root.right));
+        
     }
 }
