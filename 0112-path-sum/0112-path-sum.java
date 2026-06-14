8ad072d08aa609a46dd041d89cfa8ea0bc4edbf8
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 16:26:46 +0530

    Accepted: Path Sum

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-        return helper(root, sum);
-    }
-
-    private boolean helper(TreeNode root, int sum) {
         if(root == null) return false;
-
         if(root.left == null && root.right == null) {
             return root.val == sum;
         }
 
-
-        return helper(root.left, sum - root.val) || helper(root.right, sum - root.val);
+        return hasPathSum(root.left, sum - root.val) 
+            || hasPathSum(root.right, sum - root.val);
     }
 }
