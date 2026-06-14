c53807d3e7b1767573130286e0d60bf12b1c861b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 09:01:24 +0530

    Accepted: Minimum Depth of Binary Tree

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -1,61 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-        Deque<TreeNode> dq = new LinkedList<>();
-        dq.offerFirst(root);
 
-        int depth = 0;
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        dq.offer(root);
 
-        while (!dq.isEmpty()) {
+        int cnt = 1;
+        while(!dq.isEmpty()) {
             int size = dq.size();
-            depth++;
 
-            for (int i = 0; i < size; i++) {
-                TreeNode node = dq.pollLast();
+            for(int i = 0; i < size; i++) {
+                TreeNode curr = dq.poll();
 
-                if(node.left == null && node.right == null) return depth;
-
-                if (node.left != null) dq.offerFirst(node.left);
-                if (node.right != null) dq.offerFirst(node.right);
+                if(curr.left == null && curr.right == null) return cnt;
+                if(curr.left != null) dq.offer(curr.left);
+                if(curr.right != null) dq.offer(curr.right);
             }
+
+            cnt++;
         }
-        return -1;
+
+        return cnt;
     }
 }
-
-
-/*
-
-class Solution {
-    public int minDepth(TreeNode root) {
-        return helper(root);
-    }
-
-    private int helper(TreeNode node) {
-        if(node == null) return 0;
-
-        int leftH = helper(node.left);
-        int rightH = helper(node.right);
-
-        if(leftH == 0 || rightH == 0) return Math.max(leftH, rightH) + 1;
-
-        return Math.min(leftH, rightH) + 1;
-    }
-}
-
-*/
