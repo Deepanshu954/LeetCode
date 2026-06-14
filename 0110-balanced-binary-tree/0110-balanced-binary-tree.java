c1448c8e8c499ca8b93331bb6d4ef4a1b69fda20
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-14 08:54:34 +0530

    Accepted: Balanced Binary Tree

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,19 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
@@ -31,32 +15,6 @@ class Solution {
 
         if(Math.abs(l - r) > 1) return -1;
 
-        return Math.max(l,r) + 1;
+        return 1 + Math.max(l, r);
     }
 }
-
-/*
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-
-        int l = check(root.left);
-        int r = check(root.right);
-
-        if(Math.abs(l - r) > 1) return false;
-
-        return isBalanced(root.left) && isBalanced(root.right);
-
-        
-    }
-
-    private int check(TreeNode root) {
-        if(root == null) return 0;
-
-        int l = check(root.left);
-        int r = check(root.right);
-
-        return Math.max(l,r) + 1;
-    }
-}
-*/
