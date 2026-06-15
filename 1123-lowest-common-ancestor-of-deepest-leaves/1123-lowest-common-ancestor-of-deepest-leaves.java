bb168a2363cd30f3d1b3b5fe17cfefe3ac992d34
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-15 12:17:43 +0530

    Accepted: Lowest Common Ancestor of Deepest Leaves

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -1,39 +1,31 @@
 class Solution {
+
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        int height = height(root);
-
-        Deque<TreeNode> dq = new ArrayDeque<>();
-        dq.offer(root);
-
-        int cnt = 0;
-        while(!dq.isEmpty() && cnt < (height - 2)) {
-            int size = dq.size();
-
-            for(int i = 0; i < size; i++) {
-                TreeNode curr = dq.poll();
-
-                if(curr.left != null) dq.offer(curr.left);
-                if(curr.right != null) dq.offer(curr.right);
-
-            }
-            cnt++;
-        }
-
-        while(!dq.isEmpty()) {
-            TreeNode curr = dq.poll();
-
-            if(curr.left != null && curr.right != null) return curr;
-            if(curr.left == null && curr.right != null) return curr.right;
-            if(curr.left != null && curr.right == null) return curr.left;
-        }
-
-        return root;
+        return dfs(root).node;
     }
 
-    private int height(TreeNode root) {
-        if(root == null) return 0;
+    private Pair dfs(TreeNode root) {
+        if (root == null) return new Pair(null, 0);
 
-        return 1 + Math.max(height(root.left), height(root.right));
+        Pair left = dfs(root.left);
+        Pair right = dfs(root.right);
+
+        if (left.depth == right.depth) {
+            return new Pair(root, left.depth + 1);
+        } else if (left.depth > right.depth) {
+            return new Pair(left.node, left.depth + 1);
+        } else {
+            return new Pair(right.node, right.depth + 1);
+        }
+    }
+
+    class Pair {
+        TreeNode node;
+        int depth;
+
+        Pair(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
     }
 }
-
