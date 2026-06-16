8bd7c209ed8168565d82b046ae32ed1c4a7f2459
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-16 06:23:15 +0530

    Runtime Error: Process String with Special Operations I

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '*') {
+                sb.deleteCharAt(sb.length() - 1);
+            } else if(ch == '#') {
+                sb.append(sb.toString());
+            } else if(ch == '%') {
+                sb.reverse();
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
+/*
+a
+aa
+aab
+baa
+ba
+
+*/
