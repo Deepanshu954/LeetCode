10cba4da1778c87979910ce1de7319957b8aeb5e
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-19 06:47:03 +0530

    Accepted: Search in a Binary Search Tree

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -1,29 +1,10 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        return dfs(root, val);
-    }
-
-    private TreeNode dfs(TreeNode node, int val) {
-        if(node == null) return null;
-        if(node.val == val) return node;
-        else if(node.val > val) return dfs(node.left, val);
-        else if(node.val < val) return dfs(node.right, val);
-
+        while (root != null) {
+            if (root.val == val) return root;
+            else if (val < root.val) root = root.left;
+            else root = root.right;
+        }
         return null;
     }
 }
