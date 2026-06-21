37f9b2e6048930c21046c01b18ba5552ec7b9c0b
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-21 06:11:22 +0530

    Accepted: Maximum Ice Cream Bars

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -2,13 +2,13 @@ class Solution {
     public int maxIceCream(int[] costs, int coins) {
         Arrays.sort(costs);
 
-        int count = 0;
-        while (count < costs.length && coins >= costs[count]) {
-            coins -= costs[count];
-            count++;
+        int cnt = 0;
+        while (cnt < costs.length && coins >= costs[cnt]) {
+            coins -= costs[cnt++];
+            //cnt++;
         }
 
-        return count;
+        return cnt++;
 
     }
 }
