a01fc012a9d7c2cc44ebddcd69cce03ec21140ad
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-21 06:30:31 +0530

    Accepted: Maximum Ice Cream Bars

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,14 +1,44 @@
-class Solution {
-    public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+import java.util.Map;
+import java.util.TreeMap;
 
-        int cnt = 0;
-        while (cnt < costs.length && coins >= costs[cnt]) {
-            coins -= costs[cnt++];
-            //cnt++;
+class Solution {
+    public int maxIceCream(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= k) map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        return cnt++;
+        int cnt = 0;
 
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int price = entry.getKey();
+            int ava = entry.getValue();
+
+            int canAfford = k / price;
+            int bought = Math.min(ava, canAfford);
+
+            cnt += bought;
+            k -= (price * bought);
+
+            if (k < price) break;
+        }
+
+        return cnt;
     }
 }
+
+
+// class Solution {
+//     public int maxIceCream(int[] costs, int coins) {
+//         Arrays.sort(costs);
+
+//         int cnt = 0;
+//         while (cnt < costs.length && coins >= costs[cnt]) {
+//             coins -= costs[cnt++];
+//         }
+
+//         return cnt++;
+//     }
+// }
