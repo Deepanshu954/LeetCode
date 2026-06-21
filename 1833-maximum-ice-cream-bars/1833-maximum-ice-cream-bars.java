323b8180866ed93dabe3a0810d452d1d4c15d142
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-21 06:33:56 +0530

    Accepted: Maximum Ice Cream Bars

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,44 +1,31 @@
-import java.util.Map;
-import java.util.TreeMap;
-
 class Solution {
-    public int maxIceCream(int[] nums, int k) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new TreeMap<>();
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= k) map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    public int maxIceCream(int[] costs, int coins) {
+        int maxCost = 0;
+        for (int cost : costs) {
+            if (cost > maxCost) {
+                maxCost = cost;
+            }
         }
 
-        int cnt = 0;
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int price = entry.getKey();
-            int ava = entry.getValue();
-
-            int canAfford = k / price;
-            int bought = Math.min(ava, canAfford);
-
-            cnt += bought;
-            k -= (price * bought);
-
-            if (k < price) break;
+        int[] frequency = new int[maxCost + 1];
+        for (int cost : costs) {
+            frequency[cost]++;
         }
 
-        return cnt;
+        int count = 0;
+
+        for (int price = 1; price <= maxCost; price++) {
+            if (frequency[price] == 0) continue;
+            if (coins < price) break;
+            
+            int canBuy = Math.min(frequency[price], coins / price);
+
+            count += canBuy;
+            coins -= canBuy * price;
+
+            if (coins == 0) break;
+        }
+
+        return count;
     }
 }
-
-
-// class Solution {
-//     public int maxIceCream(int[] costs, int coins) {
-//         Arrays.sort(costs);
-
-//         int cnt = 0;
-//         while (cnt < costs.length && coins >= costs[cnt]) {
-//             coins -= costs[cnt++];
-//         }
-
-//         return cnt++;
-//     }
-// }
