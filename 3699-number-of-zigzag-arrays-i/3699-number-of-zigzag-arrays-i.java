457fb40c33810d72bfb0d97336b4254f1b25cbf0
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-23 05:56:42 +0530

    Time Limit Exceeded: Number of ZigZag Arrays I

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -1,13 +1,49 @@
-// Recursive
+// // Recursive
+// class Solution {
+//     int m;
+//     int MOD = 1000000007;
+
+//     public int zigZagArrays(int n, int l, int r) {
+//         m = r - l + 1;
+//         if (n == 1) return m;
+
+//         long totalCount = 0;
+//         for (int v = 0; v < m; v++) {
+//             totalCount = (totalCount + calculateWays(n - 1, v, 2)) % MOD;
+//         }
+//         return (int) totalCount;
+//     }
+
+//     private long calculateWays(int remainingLength, int lastValue, int requiredDirection) {
+//         if (remainingLength == 0) return 1;
+
+//         long ways = 0;
+//         for (int v = 0; v < m; v++) {
+//             if (v == lastValue) continue;
+//             if (requiredDirection == 0 && v <= lastValue) continue;
+//             if (requiredDirection == 1 && v >= lastValue) continue;
+
+//             int nextDirection = (v > lastValue) ? 1 : 0;
+//             ways = (ways + calculateWays(remainingLength - 1, v, nextDirection)) % MOD;
+//         }
+//         return ways;
+//     }
+// }
+
+// Memorization
+
 class Solution {
     int m;
     int MOD = 1000000007;
+    Long[][][] memo;
 
     public int zigZagArrays(int n, int l, int r) {
         m = r - l + 1;
         if (n == 1) return m;
 
+        memo = new Long[n][m][3];
         long totalCount = 0;
+        
         for (int v = 0; v < m; v++) {
             totalCount = (totalCount + calculateWays(n - 1, v, 2)) % MOD;
         }
@@ -16,6 +52,9 @@ class Solution {
 
     private long calculateWays(int remainingLength, int lastValue, int requiredDirection) {
         if (remainingLength == 0) return 1;
+        if (memo[remainingLength][lastValue][requiredDirection] != null) {
+            return memo[remainingLength][lastValue][requiredDirection];
+        }
 
         long ways = 0;
         for (int v = 0; v < m; v++) {
@@ -26,6 +65,7 @@ class Solution {
             int nextDirection = (v > lastValue) ? 1 : 0;
             ways = (ways + calculateWays(remainingLength - 1, v, nextDirection)) % MOD;
         }
-        return ways;
+        
+        return memo[remainingLength][lastValue][requiredDirection] = ways;
     }
 }
