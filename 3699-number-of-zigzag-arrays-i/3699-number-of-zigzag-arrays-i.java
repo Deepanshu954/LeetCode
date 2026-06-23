ef8516423cada5c3f85992d416d932b580b5dc12
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-23 05:57:33 +0530

    Accepted: Number of ZigZag Arrays I

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -1,13 +1,49 @@
-// // // Recursive
+// // // // Recursive
+// // // class Solution {
+// // //     int m;
+// // //     int MOD = 1000000007;
+
+// // //     public int zigZagArrays(int n, int l, int r) {
+// // //         m = r - l + 1;
+// // //         if (n == 1) return m;
+
+// // //         long totalCount = 0;
+// // //         for (int v = 0; v < m; v++) {
+// // //             totalCount = (totalCount + calculateWays(n - 1, v, 2)) % MOD;
+// // //         }
+// // //         return (int) totalCount;
+// // //     }
+
+// // //     private long calculateWays(int remainingLength, int lastValue, int requiredDirection) {
+// // //         if (remainingLength == 0) return 1;
+
+// // //         long ways = 0;
+// // //         for (int v = 0; v < m; v++) {
+// // //             if (v == lastValue) continue;
+// // //             if (requiredDirection == 0 && v <= lastValue) continue;
+// // //             if (requiredDirection == 1 && v >= lastValue) continue;
+
+// // //             int nextDirection = (v > lastValue) ? 1 : 0;
+// // //             ways = (ways + calculateWays(remainingLength - 1, v, nextDirection)) % MOD;
+// // //         }
+// // //         return ways;
+// // //     }
+// // // }
+
+// // // Memorization
+
 // // class Solution {
 // //     int m;
 // //     int MOD = 1000000007;
+// //     Long[][][] memo;
 
 // //     public int zigZagArrays(int n, int l, int r) {
 // //         m = r - l + 1;
 // //         if (n == 1) return m;
 
+// //         memo = new Long[n][m][3];
 // //         long totalCount = 0;
+        
 // //         for (int v = 0; v < m; v++) {
 // //             totalCount = (totalCount + calculateWays(n - 1, v, 2)) % MOD;
 // //         }
@@ -16,6 +52,9 @@
 
 // //     private long calculateWays(int remainingLength, int lastValue, int requiredDirection) {
 // //         if (remainingLength == 0) return 1;
+// //         if (memo[remainingLength][lastValue][requiredDirection] != null) {
+// //             return memo[remainingLength][lastValue][requiredDirection];
+// //         }
 
 // //         long ways = 0;
 // //         for (int v = 0; v < m; v++) {
@@ -26,52 +65,50 @@
 // //             int nextDirection = (v > lastValue) ? 1 : 0;
 // //             ways = (ways + calculateWays(remainingLength - 1, v, nextDirection)) % MOD;
 // //         }
-// //         return ways;
+        
+// //         return memo[remainingLength][lastValue][requiredDirection] = ways;
 // //     }
 // // }
 
-// // Memorization
+
+// // Tabular
 
 // class Solution {
-//     int m;
-//     int MOD = 1000000007;
-//     Long[][][] memo;
-
 //     public int zigZagArrays(int n, int l, int r) {
-//         m = r - l + 1;
+//         int m = r - l + 1;
+//         int MOD = 1000000007;
+
 //         if (n == 1) return m;
 
-//         memo = new Long[n][m][3];
+//         long[][][] dp = new long[n + 1][m][2];
+
+//         for (int v = 0; v < m; v++) {
+//             dp[2][v][1] = v;
+//             dp[2][v][0] = m - 1 - v;
+//         }
+
+//         for (int i = 3; i <= n; i++) {
+//             for (int v = 0; v < m; v++) {
+//                 for (int k = 0; k < v; k++) {
+//                     dp[i][v][1] = (dp[i][v][1] + dp[i - 1][k][0]) % MOD;
+//                 }
+//                 for (int k = v + 1; k < m; k++) {
+//                     dp[i][v][0] = (dp[i][v][0] + dp[i - 1][k][1]) % MOD;
+//                 }
+//             }
+//         }
+
 //         long totalCount = 0;
-        
 //         for (int v = 0; v < m; v++) {
-//             totalCount = (totalCount + calculateWays(n - 1, v, 2)) % MOD;
+//             totalCount = (totalCount + dp[n][v][0] + dp[n][v][1]) % MOD;
 //         }
+
 //         return (int) totalCount;
-//     }
-
-//     private long calculateWays(int remainingLength, int lastValue, int requiredDirection) {
-//         if (remainingLength == 0) return 1;
-//         if (memo[remainingLength][lastValue][requiredDirection] != null) {
-//             return memo[remainingLength][lastValue][requiredDirection];
-//         }
-
-//         long ways = 0;
-//         for (int v = 0; v < m; v++) {
-//             if (v == lastValue) continue;
-//             if (requiredDirection == 0 && v <= lastValue) continue;
-//             if (requiredDirection == 1 && v >= lastValue) continue;
-
-//             int nextDirection = (v > lastValue) ? 1 : 0;
-//             ways = (ways + calculateWays(remainingLength - 1, v, nextDirection)) % MOD;
-//         }
-        
-//         return memo[remainingLength][lastValue][requiredDirection] = ways;
 //     }
 // }
 
 
-// Tabular
+// Space Optimization
 
 class Solution {
     public int zigZagArrays(int n, int l, int r) {
@@ -80,27 +117,37 @@ class Solution {
 
         if (n == 1) return m;
 
-        long[][][] dp = new long[n + 1][m][2];
+        long[] up = new long[m];
+        long[] down = new long[m];
 
         for (int v = 0; v < m; v++) {
-            dp[2][v][1] = v;
-            dp[2][v][0] = m - 1 - v;
+            up[v] = v;
+            down[v] = m - 1 - v;
         }
 
         for (int i = 3; i <= n; i++) {
+            long[] nextUp = new long[m];
+            long[] nextDown = new long[m];
+
+            long sumDown = 0;
             for (int v = 0; v < m; v++) {
-                for (int k = 0; k < v; k++) {
-                    dp[i][v][1] = (dp[i][v][1] + dp[i - 1][k][0]) % MOD;
-                }
-                for (int k = v + 1; k < m; k++) {
-                    dp[i][v][0] = (dp[i][v][0] + dp[i - 1][k][1]) % MOD;
-                }
+                nextUp[v] = sumDown;
+                sumDown = (sumDown + down[v]) % MOD;
             }
+
+            long sumUp = 0;
+            for (int v = m - 1; v >= 0; v--) {
+                nextDown[v] = sumUp;
+                sumUp = (sumUp + up[v]) % MOD;
+            }
+
+            up = nextUp;
+            down = nextDown;
         }
 
         long totalCount = 0;
         for (int v = 0; v < m; v++) {
-            totalCount = (totalCount + dp[n][v][0] + dp[n][v][1]) % MOD;
+            totalCount = (totalCount + up[v] + down[v]) % MOD;
         }
 
         return (int) totalCount;
