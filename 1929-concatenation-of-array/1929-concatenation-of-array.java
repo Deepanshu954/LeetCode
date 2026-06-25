c2f203e5b83a65506881dc34adb724001ed54ef6
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-25 08:27:41 +0530

    Accepted: Concatenation of Array

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,12 +1,15 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[n + n];
+        int size = 2 * n;
 
-        for(int i = 0; i < n; i++){
+        int[] arr = new int[size];
+
+        for(int i = 0; i < n; i++) {
             arr[i] = nums[i];
-            arr[n + i] = nums[i];
+            arr[n+i] = nums[i];
         }
+
         return arr;
     }
 }
