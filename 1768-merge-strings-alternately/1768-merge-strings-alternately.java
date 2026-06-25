d2f4fc99be7feb7aaae3c4f944ed7da84fba411a
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-25 10:36:20 +0530

    Accepted: Merge Strings Alternately

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 0;
+        while(idx < word1.length() && idx < word2.length()) {
+            sb.append(word1.charAt(idx));
+            sb.append(word2.charAt(idx));
+            idx++;
+        }
+
+        while(idx < word2.length()) {
+            sb.append(word2.charAt(idx));
+            idx++;
+        }
+
+        while(idx < word1.length()) {
+            sb.append(word1.charAt(idx));
+            idx++;
+        }
+
+        return sb.toString();
+
+    }
+}
