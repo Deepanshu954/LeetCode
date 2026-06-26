c32875dcfb7da22df6fb87688a9b3ccedd0fbfd1
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-26 05:54:06 +0530

    Wrong Answer: Count Subarrays With Majority Element II

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -1,20 +1,50 @@
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-        // int[] rangeSum = new int[n];
-
-        int cnt = 0;
         int res = 0;
-        for(int i = 0; i < n; i++) {
-            cnt = 0;
-            for(int j = i; j < n; j++) {
-                if(nums[j] == target) cnt++;
-
-                int len = j - i + 1;
-                if(len < 2*cnt) res++;
-            }
+        int currentPrefixSum = 0;
+        
+        // Offset by n + 1 to handle negative prefix sums (array indices must be > 0)
+        int offset = n + 1; 
+        FenwickTree bit = new FenwickTree(2 * n + 2);
+        
+        // Add the initial prefix sum of 0 (with offset)
+        bit.update(0 + offset, 1);
+        
+        for (int num : nums) {
+            currentPrefixSum += (num == target) ? 1 : -1;
+            
+            // We want to find how many previous prefix sums are strictly less than currentPrefixSum
+            // So we query the BIT for everything up to currentPrefixSum - 1
+            res += bit.query(currentPrefixSum - 1 + offset);
+            
+            // Add the current prefix sum to our BIT
+            bit.update(currentPrefixSum + offset, 1);
         }
-
+        
         return res;
+    }
+}
+
+// OOPS concept: Encapsulating the BIT logic
+class FenwickTree {
+    private int[] tree;
+    
+    public FenwickTree(int size) {
+        tree = new int[size];
+    }
+    
+    public void update(int index, int delta) {
+        for (; index < tree.length; index += index & (-index)) {
+            tree[index] += delta;
+        }
+    }
+    
+    public int query(int index) {
+        int sum = 0;
+        for (; index > 0; index -= index & (-index)) {
+            sum += tree[index];
+        }
+        return sum;
     }
 }
