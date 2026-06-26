863dea518294074844df6c0bb84cdf537a0c08d1
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-26 13:37:43 +0530

    Accepted: Rotate Array

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,26 +1,29 @@
 class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
 
-    void reverseArr(int[] nums, int i, int j) {
-        while(i < j)
-        {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
+        if(k == 0) return;
+
+        rev(nums, 0, n-1);
+        rev(nums, 0, k-1);
+        rev(nums, k, n-1);
     }
 
-    public void rotate(int[] nums, int k){
+    private void rev(int[] nums, int l, int r) {
+        while(l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
 
-        int n = nums.length;
-        //if(n == 0) return;
-        k = k % n;
-        //if(k == 0) return;
+            l++;
+            r--;
+        }
 
-        reverseArr(nums , 0 , n-1);
-        reverseArr(nums , 0 , k-1);
-        reverseArr(nums , k , n-1);
-        
+        return;
     }
 }
+
+// 1 2 3 4      5 6 7
+// 7 6 5        4 3 2 1
+// 5 6 7        1 2 3 4
