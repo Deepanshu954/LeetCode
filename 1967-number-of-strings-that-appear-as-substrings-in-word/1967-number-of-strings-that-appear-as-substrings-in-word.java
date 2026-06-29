cee92d0ae1b6b3dafd2939d6764b8f3efa58e98d
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-29 06:20:29 +0530

    Accepted: Number of Strings That Appear as Substrings in Word

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -3,8 +3,14 @@ class Solution {
         int cnt = 0;
         
         for (int i = 0; i < patterns.length; i++) {
-            if (word.contains(patterns[i])) {
-                cnt++;
+            String pat = patterns[i];
+            
+            // Check every possible starting position in 'word' for this pattern
+            for (int idx = 0; idx <= word.length() - pat.length(); idx++) {
+                if (word.startsWith(pat, idx)) {
+                    cnt++;
+                    break; // Found it! Move to the next pattern.
+                }
             }
         }
         
