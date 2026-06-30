f70f8da303fd46e514e00ee127ca9a920a3210bd
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-06-30 07:25:49 +0530

    Accepted: Number of Substrings Containing All Three Characters

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,26 +1,35 @@
-class Solution {
-    public int numberOfSubstrings(String s) {
-        String str = "aaaabbbabbbbabbbabbaabaababbbbbbbabaabaabaabbbbbbababbbbabbbbababbaaab";
-        if(s.startsWith(str)) return 49998;
-        int n = s.length();
-
-        int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            int cntA = 0;
-            int cntB = 0;
-            int cntC = 0;
-
-            for(int j = i; j < n; j++) {
-                if(s.charAt(j) == 'a') cntA++;
-                if(s.charAt(j) == 'b') cntB++;
-                if(s.charAt(j) == 'c') cntC++;
-                if(cntA != 0 && cntB != 0 && cntC != 0) {
-                    cnt += (n-j);
-                    break;
-                }
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int count = 0;
+//         for(int i=0;i<s.length();i++){
+//             int array[] = new int[3];
+//             for(int j=i;j<s.length();j++){
+//                 char ch = s.charAt(j);
+//                 if(ch == 'a' || ch == 'b' || ch == 'c'){
+//                     array[ch-'a']++;
+//                 }
+//                 if(array[0]!=0 && array[1]!=0 && array[2]!=0) count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+class Solution{
+    public int numberOfSubstrings(String s){
+        int a = -1,b=-1,c=-1;
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == 'a'){
+                a = i;
+            }
+            else if(ch == 'b') b = i;
+            else c = i;
+            int min = Math.min(a,Math.min(b,c));
+            if(min!=-1){
+                count+=min+1;
             }
         }
-        
-        return cnt;
+        return count;
     }
 }
