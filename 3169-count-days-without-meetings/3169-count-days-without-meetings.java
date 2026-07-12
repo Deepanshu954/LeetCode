a885986f74f44c0fd605ee64d955760aafde8420
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-07-12 18:59:52 +0530

    Accepted: Count Days Without Meetings

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,37 +1,17 @@
+import java.util.*;
+
 class Solution {
-    public int countDays(int days, int[][] meet) {
-        int n = meet.length;
+    public int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+        int prevEnd = 0;
 
-        int[] start = new int[n];
-        int[] end = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            start[i] = meet[i][0];
-            end[i] = meet[i][1];
+        for (int[] meeting : meetings) {
+            int start = Math.max(meeting[0], prevEnd + 1);
+            int length = meeting[1] - start + 1;
+            days -= Math.max(length, 0);
+            prevEnd = Math.max(prevEnd, meeting[1]);
         }
 
-        // Arrays.sort(meet, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        Arrays.sort(start);
-        Arrays.sort(end);
-
-        int cnt = 0;
-        int ans = 0;
-
-        int i = 0;
-        int j = 0;
-
-        while(i < n && j < n) {
-            if(start[i] < end[j]) {
-                cnt++;
-                i++;
-            } else {
-                cnt--;
-                j++;
-            }
-
-            if(cnt == 0) ans++;
-        }
-
-        return ans + (days - end[n-1]) + start[0] - 1;
+        return days;
     }
 }
