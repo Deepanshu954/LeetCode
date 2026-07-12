1a31dcb2ea5948b0773dd02ba126426d75eea506
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-07-12 21:19:47 +0530

    Accepted: Count Days Without Meetings

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,25 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int countDays(int days, int[][] meetings) {
         Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
 
         int free = 0;
-        int prevEnd = 0;
+        int prev = 0;
 
-        for (int[] meeting : meetings) {
-            int start = meeting[0];
-            int end = meeting[1];
-
-            if (start > prevEnd + 1) {
-                free += start - prevEnd - 1;
+        for (int[] meet : meetings) {
+            if (meet[0] > prev + 1) {
+                free += meet[0] - prev - 1;
             }
 
-            prevEnd = Math.max(prevEnd, end);
+            prev = Math.max(prev, meet[1]);
         }
 
-        free += days - prevEnd;
-
-        return free;
+        return free + days - prev;
     }
 }
