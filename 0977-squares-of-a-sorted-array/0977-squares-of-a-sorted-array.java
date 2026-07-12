949f95411994ccc3e429471f20c54b5be078fad4
Deepanshu9548 <deepanshuchauhan971919@gmail.com>
2026-07-12 07:59:14 +0530

    Accepted: Squares of a Sorted Array

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1; // Start filling result from the largest index
+        
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            
+            if (leftSquare > rightSquare) {
+                result[index] = leftSquare;
+                left++;
+            } else {
+                result[index] = rightSquare;
+                right--;
+            }
+            index--;
+        }
+        
+        return result;
+    }
+}
