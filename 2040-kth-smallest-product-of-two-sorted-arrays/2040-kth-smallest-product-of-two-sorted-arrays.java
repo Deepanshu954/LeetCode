7bcedf67646dffdc9b6d903e155573cff72727e8
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-07-17 08:11:32 +0530

    Time: 69 ms (94.25%), Space: 62.7 MB (58.05%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,59 +1,106 @@
-class Solution {
-    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        List<Integer> A1 = new ArrayList<>();
-        List<Integer> A2 = new ArrayList<>();
-        List<Integer> B1 = new ArrayList<>();
-        List<Integer> B2 = new ArrayList<>();
-
-        separate(nums1, A1, A2);
-        separate(nums2, B1, B2);
-
-        long negCount = 1L * A1.size() * B2.size() + 1L * A2.size() * B1.size();
-        int sign = 1;
-
-        if (k > negCount) {
-            k -= negCount;
-        } else {
-            k = negCount - k + 1;
-            sign = -1;
-            List<Integer> temp = B1;
-            B1 = B2;
-            B2 = temp;
+class Solution 
+{
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) 
+    {
+        // Step 1: Swap to ensure nums1 is the shorter array (optimization)
+        if (nums1.length > nums2.length) 
+        {
+            return kthSmallestProduct(nums2, nums1, k);
         }
 
-        long l = 0, r = (long) 1e10;
-        while (l < r) {
-            long m = (l + r) / 2;
-            if (numProductNoGreaterThan(A1, B1, m) + numProductNoGreaterThan(A2, B2, m) >= k) {
-                r = m;
-            } else {
-                l = m + 1;
+        // Step 2: Set binary search bounds for product space
+        long lo = -1_0000_0000_00L;
+        long hi = 1_0000_0000_00L;
+
+        // Step 3: Binary search for smallest number with at least k products <= it
+        while (lo < hi) 
+        {
+            long mid = lo + (hi - lo) / 2;
+            long cnt = countLE(mid, nums1, nums2); // count of products <= mid
+
+            if (cnt < k) 
+            {
+                lo = mid + 1; // need bigger product
+            } 
+            else 
+            {
+                hi = mid; // try smaller (mid might be answer)
             }
         }
 
-        return sign * l;
+        return lo; // kth smallest product
     }
 
-    private void separate(int[] nums, List<Integer> A1, List<Integer> A2) {
-        for (int a : nums) {
-            if (a < 0) {
-                A1.add(-a);
-            } else {
-                A2.add(a);
-            }
-        }
-        Collections.reverse(A1);
-    }
-
-    private long numProductNoGreaterThan(List<Integer> A, List<Integer> B, long m) {
+    // Step 4: Count number of products <= x
+    public long countLE(long x, int[] A, int[] B) 
+    {
         long count = 0;
-        int j = B.size() - 1;
-        for (int a : A) {
-            while (j >= 0 && 1L * a * B.get(j) > m) {
-                j--;
+        int n = B.length;
+
+        if (x >= 0) 
+        {
+            // Case 1: x is positive
+            int jPos = n - 1;
+            int jNeg = n - 1;
+
+            for (int a : A) 
+            {
+                if (a > 0) 
+                {
+                    while (jPos >= 0 && a * 1L * B[jPos] > x)
+                    {
+                        jPos--;
+                    }
+                     
+                    count += jPos + 1;
+                } 
+                else if (a == 0) 
+                {
+                    count += n; // All zero products <= x (x >= 0)
+                } 
+                else 
+                {
+                    while (jNeg >= 0 && a * 1L * B[jNeg] <= x)
+                    {
+                        jNeg--;
+                    }
+
+                    count += n - 1 - jNeg;
+                }
             }
-            count += j + 1;
+        } else 
+        {
+            // Case 2: x is negative
+            int jPos = 0;
+            int jNeg = 0;
+
+            for (int a : A) 
+            {
+                if (a > 0) 
+                {
+                    while (jPos < n && a * 1L * B[jPos] <= x)
+                    {
+                        jPos++;
+                    }
+
+                    count += jPos;
+                } 
+                else if (a == 0) 
+                {
+                    // All zero products > negative x
+                } 
+                else 
+                {
+                    while (jNeg < n && a * 1L * B[jNeg] > x)
+                    {
+                        jNeg++;
+                    }
+
+                    count += n - jNeg;
+                }
+            }
         }
+
         return count;
     }
 }
