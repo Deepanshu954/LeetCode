8ae3957f308b2b5ee5cc5e06d048424b89a91400
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-07-18 18:17:13 +0530

    Time: 12 ms (93.51%), Space: 74.1 MB (37.02%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,12 +1,29 @@
 class Solution {
-        public long maxRunTime(int n, int[] A) {
-        Arrays.sort(A);
+    public long maxRunTime(int n, int[] nums) {
         long sum = 0;
-        for (int a: A)
-            sum += a;
-        int k = 0, na = A.length;
-        while (A[na - 1 - k] > sum / (n - k))
-            sum -= A[na - 1 - k++];
-        return sum / (n - k);
+        for(int num : nums) sum += num;
+
+        long left = 0;
+        long right = sum/n;
+
+        while(left < right) {
+            long mid = left + (right - left + 1)/2;
+
+            if(isValid(nums, mid, n)) {
+                left = mid;
+            } else right = mid-1;
+        }
+
+        return left;
     }
+
+    private boolean isValid(int[] batteries, long time, int n) {
+    long total = 0;
+
+    for (int battery : batteries) {
+        total += Math.min((long) battery, time);
+    }
+
+    return total >= time * n;
+}
 }
