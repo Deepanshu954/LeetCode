8aa65e75c652fdaee134e2226cca40a593a0236e
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-07-20 18:27:47 +0530

    Time: 782 ms (33.7%), Space: 64.2 MB (19.59%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,26 +1,71 @@
 class Solution {
-    private int res = (int)1e8;
-    private int sum = 0;
+
     public int minimumDifference(int[] nums) {
         int n = nums.length;
-        
-        if(n == 2) return Math.abs(nums[0] - nums[1]);
+        int m = n / 2;
 
-        for(int x : nums) sum += x;
-        int k = sum;
+        int total = 0;
+        for (int x : nums) total += x;
 
-        rec(n-1, k, nums);
-        return res;
-    }
+        List<Integer>[] left = new ArrayList[m + 1];
+        List<Integer>[] right = new ArrayList[m + 1];
 
-    private void rec(int i, int k, int[] nums) {
-        if(i < 0) return;
+        for (int i = 0; i <= m; i++) {
+            left[i] = new ArrayList<>();
+            right[i] = new ArrayList<>();
+        }
 
-        int abs = Math.abs(k - (sum - k));
-        res = Math.min(res, abs);
+        // Generate subset sums
+        for (int mask = 0; mask < (1 << m); mask++) {
+            int cnt = 0;
+            int sumL = 0;
+            int sumR = 0;
 
-        rec(i-1, k-nums[i], nums);
-        rec(i-1, k, nums);
-        return;
+            for (int j = 0; j < m; j++) {
+                if ((mask & (1 << j)) != 0) {
+                    cnt++;
+                    sumL += nums[j];
+                    sumR += nums[j + m];
+                }
+            }
+
+            left[cnt].add(sumL);
+            right[cnt].add(sumR);
+        }
+
+        // Sort right sums
+        for (int i = 0; i <= m; i++) {
+            Collections.sort(right[i]);
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int k = 0; k <= m; k++) {
+
+            List<Integer> leftList = left[k];
+            List<Integer> rightList = right[m - k];
+
+            for (int s1 : leftList) {
+
+                // Want s1 + s2 as close as possible to total/2
+                double target = total / 2.0 - s1;
+
+                int idx = Collections.binarySearch(rightList, (int)Math.ceil(target));
+
+                if (idx < 0) idx = -idx - 1;
+
+                if (idx < rightList.size()) {
+                    int subset = s1 + rightList.get(idx);
+                    ans = Math.min(ans, Math.abs(total - 2 * subset));
+                }
+
+                if (idx > 0) {
+                    int subset = s1 + rightList.get(idx - 1);
+                    ans = Math.min(ans, Math.abs(total - 2 * subset));
+                }
+            }
+        }
+
+        return ans;
     }
 }
