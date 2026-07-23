ffe7b124f74544aeb3a92f14a7a8cbf5590745c4
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-07-23 20:56:15 +0530

    Time: 2 ms (86.68%), Space: 42.6 MB (82.41%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] freq = new int[26];
+
+        for(int i = 0; i < tiles.length(); i++) {
+            freq[tiles.charAt(i) - 'A']++;
+        }
+
+        return dfs(freq);
+    }
+
+    private int dfs(int[] freq) {
+        int cnt = 0;
+
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] == 0) continue;
+
+            cnt++;
+            freq[i]--;
+            cnt += dfs(freq);
+            freq[i]++;
+        }
+
+        return cnt;
+    }
+}
