6b64bde3add9ae299c17c69c63ce5551cbefc2aa
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-07-28 07:25:20 +0530

    Time: 75 ms (12.75%), Space: 158.5 MB (7.15%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,47 +1,23 @@
-// class Solution {
-//     public int[] maxSlidingWindow(int[] nums, int k) {
-//         if (nums == null || k <= 0) return new int[0];
-//         int[] ans = new int[nums.length - k + 1];
-
-//         for(int i = 0; i < nums.length - k + 1; i++){
-//             int max = Integer.MIN_VALUE;
-
-//             for(int j = i; j < i+k; j++){
-//                 max = Math.max(max, nums[j]);
-//             }
-//             ans[i] = max;
-//         }
-//         return ans;
-//     }
-// }
-
-
-
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || k <= 0) return new int[0];
-        
         int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        int ri = 0;
-        
-        Deque<Integer> q = new ArrayDeque<>();
-        
+        int[] arr = new int[n - k + 1];
+
+        // Max Heap {value, index}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
         for (int i = 0; i < n; i++) {
-            if (!q.isEmpty() && q.peek() == i - k) q.poll();
-            
-            // 2. Remove elements smaller than the current element from the back
-            // We maintain a decreasing order in the deque
-            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) q.pollLast();
-            
-            // 3. Add current element's index
-            q.offer(i);
-            
-            // 4. Add the max to result (only if window is fully formed)
-            if (i >= k - 1) ans[ri++] = nums[q.peek()];
+            pq.offer(new int[]{nums[i], i});
+
+            if (i >= k - 1) {
+                while (pq.peek()[1] <= i - k) {
+                    pq.poll();
+                }
+
+                arr[i - k + 1] = pq.peek()[0];
+            }
         }
-        
-        return ans;
+
+        return arr;
     }
 }
