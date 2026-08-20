6edcb9f5aaef3f519aa2f0862abde71cca3197eb
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-08-20 10:31:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -3,16 +3,13 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
 
         for(int s : stones) pq.add(s);
-        int ans = 0;
 
         while(pq.size() > 1) {
             int s1 = pq.poll();
             int s2 = pq.poll();
 
-            if(s1 == s2) continue;
-            
-            ans = Math.abs(s1 - s2);
-            pq.offer(ans);
+            int abs = Math.abs(s1 - s2);
+            pq.offer(abs);
         }
 
         return pq.poll();
