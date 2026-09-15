61898d6fd97a6671d3ea36112e2a5bb5aefb36ac
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-09-15 21:51:31 +0530

    Time: 232 ms (80.61%), Space: 8.9 MB (60.14%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,60 +1,58 @@
 class Solution {
-public:
-    bool solve(vector<vector<char>>& board, int row, int col) {
-        if (row == 9) return true;
-        
+private:
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num)
+                return false;
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num)
+                return false;
+        }
+
+        // Check 3x3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool backtrack(vector<vector<char>>& board, int row, int col) {
+        if (row == 9)
+            return true;
+
         if (col == 9)
-            return solve(board, row + 1, 0);
-        
+            return backtrack(board, row + 1, 0);
+
         if (board[row][col] != '.')
-            return solve(board, row, col + 1);
-        
+            return backtrack(board, row, col + 1);
+
         for (char num = '1'; num <= '9'; num++) {
-            bool valid = true;
-            
-            // Check row
-            for (int j = 0; j < 9; j++) {
-                if (board[row][j] == num) {
-                    valid = false;
-                    break;
-                }
-            }
-            
-            // Check column
-            for (int i = 0; i < 9 && valid; i++) {
-                if (board[i][col] == num) {
-                    valid = false;
-                    break;
-                }
-            }
-            
-            // Check 3x3 box
-            int sr = (row / 3) * 3;
-            int sc = (col / 3) * 3;
-            
-            for (int i = sr; i < sr + 3 && valid; i++) {
-                for (int j = sc; j < sc + 3; j++) {
-                    if (board[i][j] == num) {
-                        valid = false;
-                        break;
-                    }
-                }
-            }
-            
-            if (valid) {
+            if (isValid(board, row, col, num)) {
                 board[row][col] = num;
-                
-                if (solve(board, row, col + 1))
+
+                if (backtrack(board, row, col + 1))
                     return true;
-                
+
                 board[row][col] = '.';
             }
         }
-        
+
         return false;
     }
 
+public:
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board, 0, 0);
+        backtrack(board, 0, 0);
     }
 };
