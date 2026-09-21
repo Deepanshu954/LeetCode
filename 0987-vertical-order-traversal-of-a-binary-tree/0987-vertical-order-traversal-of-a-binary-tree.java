a988ecdf8ead20649bfbc0b2d8a66af069814edc
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-09-21 10:33:45 +0530

    Time: 5 ms (12.56%), Space: 43.7 MB (92.31%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,49 +14,46 @@ class Solution {
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
 
-        if (root == null) return res;
-
-        // col -> row -> values
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-
+        TreeMap<Integer, List<int[]>> map = new TreeMap<>();
         Queue<TreeInfo> q = new LinkedList<>();
         q.offer(new TreeInfo(root, 0, 0));
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             TreeInfo curr = q.poll();
 
             TreeNode node = curr.node;
             int row = curr.row;
             int col = curr.col;
 
-            map
-                .computeIfAbsent(col, k -> new TreeMap<>())
-                .computeIfAbsent(row, k -> new PriorityQueue<>())
-                .offer(node.val);
+            map.computeIfAbsent(col, k -> new ArrayList<>()).add(new int[] {row, node.val});
 
             // left
-            if (node.left != null) {
+            if(node.left != null) {
                 q.offer(new TreeInfo(node.left, row + 1, col - 1));
             }
 
+
             // right
-            if (node.right != null) {
+            if(node.right != null) {
                 q.offer(new TreeInfo(node.right, row + 1, col + 1));
             }
         }
 
-        for (TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
-            List<Integer> column = new ArrayList<>();
+        for(List<int[]> nodes : map.values()) {
+            nodes.sort(
+                (a,b) -> (( a[0] == b[0] ) ? a[1] - b[1] : a[0] - b[0])
+            );
 
-            for (PriorityQueue<Integer> pq : rows.values()) {
-                while (!pq.isEmpty()) {
-                    column.add(pq.poll());
-                }
+            List<Integer> columnValues = new ArrayList<>();
+            for(int[] node : nodes) {
+                columnValues.add(node[1]);
             }
 
-            res.add(column);
+            res.add(columnValues);
         }
+
 
         return res;
     }
