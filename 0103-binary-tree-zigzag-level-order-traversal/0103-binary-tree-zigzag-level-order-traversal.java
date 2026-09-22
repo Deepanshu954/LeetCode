045085929e70f07bb6bda2b9f5d77e77103b3a9a
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-09-22 08:51:47 +0530

    Time: 1 ms (82.84%), Space: 43.9 MB (25.17%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -14,13 +14,13 @@ class Solution {
 
             for(int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                curr.add(node.val);
+
+                if(odd) curr.addFirst(node.val);
+                else curr.add(node.val);
 
                 if(node.left != null) q.offer(node.left);
                 if(node.right != null) q.offer(node.right);
             }
-
-            if(odd) Collections.reverse(curr);
 
             res.add(curr); 
 
@@ -30,3 +30,37 @@ class Solution {
         return res;
     }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if(root == null) return res;
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         boolean odd = false;
+
+//         while(!q.isEmpty()) {
+//             int size = q.size();
+//             List<Integer> curr = new ArrayList<>();
+
+//             for(int i = 0; i < size; i++) {
+//                 TreeNode node = q.poll();
+//                 curr.add(node.val);
+
+//                 if(node.left != null) q.offer(node.left);
+//                 if(node.right != null) q.offer(node.right);
+//             }
+
+//             if(odd) Collections.reverse(curr);
+
+//             res.add(curr); 
+
+//             odd = !odd;
+//         }
+
+//         return res;
+//     }
+// }
